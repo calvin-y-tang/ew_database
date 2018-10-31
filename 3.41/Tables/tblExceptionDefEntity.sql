c7bf8e2f906ff220c53ce6786ee076114910f8fa
David Rose <david.rose@examworks.com>
2018-10-31 10:41:50 -0400

    DB Release 3.41

---
+++ 3.41/Tables/tblExceptionDefEntity.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[tblExceptionDefEntity] (
+    [ExceptionDefID] INT NOT NULL,
+    [IMECentricCode] INT NOT NULL,
+    [Entity] VARCHAR(10) NULL, 
+    CONSTRAINT [PK_tblExceptionDefEntity] PRIMARY KEY CLUSTERED ([ExceptionDefID] ASC, [IMECentricCode] ASC)
+);
+
