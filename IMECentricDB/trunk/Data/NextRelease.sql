d60d4cd4753b92e1b47709823cef394430495e34
Terri Lyde <terri.lyde@examworks.com>
2018-11-09 15:47:17 -0500

    Issue 7804 - added database names back in

--- IMECentricDB/trunk/Data/NextRelease.sql
+++ IMECentricDB/trunk/Data/NextRelease.sql
@@ -11,11 +11,11 @@ GO
 -- Issue 7804 Data patch for Market in GPInvoice table
 UPDATE [IMECentricMaster].[dbo].[GPInvoice]
   SET 
-    GPInvoice.Market = REPLACE(fnReturnValueFromKeyInString(CD.Param, 'LibertyMarket'), '"', '')
-  FROM tblCase
-       INNER JOIN tblAcctHeader AS AH ON AH.CaseNbr = tblCase.CaseNbr
-       INNER JOIN tblEWFacility AS EWfac ON EWfac.EWFacilityID = AH.EWFacilityID
-       INNER JOIN tblCustomerData  AS CD ON CD.TableType = 'tblCase' AND CD.TableKey = tblCase.CaseNbr
+    GPInvoice.Market = REPLACE([IMECentricEW].[dbo].fnReturnValueFromKeyInString(CD.Param, 'LibertyMarket'), '"', '')
+  FROM [IMECentricEW].[dbo].tblCase
+       INNER JOIN [IMECentricEW].[dbo].tblAcctHeader AS AH ON AH.CaseNbr = tblCase.CaseNbr
+       INNER JOIN [IMECentricEW].[dbo].tblEWFacility AS EWfac ON EWfac.EWFacilityID = AH.EWFacilityID
+       INNER JOIN [IMECentricEW].[dbo].tblCustomerData  AS CD ON CD.TableType = 'tblCase' AND CD.TableKey = tblCase.CaseNbr
   WHERE GPInvoice.DocumentNo = CAST(AH.DocumentNbr AS VARCHAR(15))
     AND GPInvoice.GPFacilityID = EWfac.GPFacility
 
@@ -27,9 +27,9 @@ UPDATE [IMECentricMaster].[dbo].[GPInvoice]
        CaseDocID = IIF(GPInvoice.CaseDocID IS NULL, AH.CaseDocID, GPInvoice.CaseDocID),
        Marketer = IIF(GPInvoice.Marketer IS NULL, C.MarketerCode, GPInvoice.Marketer)
   FROM
-       tblCase AS C
-       INNER JOIN tblAcctHeader AS AH ON AH.CaseNbr = C.CaseNbr
-       INNER JOIN tblEWFacility AS EWfac ON EWfac.EWFacilityID = AH.EWFacilityID
+       [IMECentricEW].[dbo].tblCase AS C
+       INNER JOIN [IMECentricEW].[dbo].tblAcctHeader AS AH ON AH.CaseNbr = C.CaseNbr
+       INNER JOIN [IMECentricEW].[dbo].tblEWFacility AS EWfac ON EWfac.EWFacilityID = AH.EWFacilityID
   WHERE
        GPInvoice.DocumentNo = CAST(AH.DocumentNbr AS VARCHAR(15))
        AND GPInvoice.GPFacilityID = EWfac.GPFacility
