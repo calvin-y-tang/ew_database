27c2bac93671b4fa19d5d03396d8abe0f792e74e
Terri Lyde <terri.lyde@examworks.com>
2018-11-09 15:30:29 -0500

    Issue 7804 - added queries for data patches for issue 7804

--- IMECentricDB/trunk/Data/NextRelease.sql
+++ IMECentricDB/trunk/Data/NextRelease.sql
@@ -7,3 +7,31 @@ GO
 INSERT INTO tblSetting (Name, Value)
 VALUES('UseNewGenDocFormOffices', ';;')
 GO 
+
+-- Issue 7804 Data patch for Market in GPInvoice table
+UPDATE [IMECentricMaster].[dbo].[GPInvoice]
+  SET 
+    GPInvoice.Market = REPLACE([IMECentricEW].[dbo].fnReturnValueFromKeyInString(CD.Param, 'LibertyMarket'), '"', '')
+  FROM [IMECentricEW].[dbo].tblCase
+       INNER JOIN [IMECentricEW].[dbo].tblAcctHeader AS AH ON AH.CaseNbr = tblCase.CaseNbr
+       INNER JOIN [IMECentricEW].[dbo].tblEWFacility AS EWfac ON EWfac.EWFacilityID = AH.EWFacilityID
+       INNER JOIN [IMECentricEW].[dbo].tblCustomerData  AS CD ON CD.TableType = 'tblCase' AND CD.TableKey = tblCase.CaseNbr
+  WHERE GPInvoice.DocumentNo = CAST(AH.DocumentNbr AS VARCHAR(15))
+    AND GPInvoice.GPFacilityID = EWfac.GPFacility
+
+
+
+-- Issue 7804 Data Patch for Marketer and CaseDocID
+  UPDATE [IMECentricMaster].[dbo].[GPInvoice]
+  SET
+       CaseDocID = IIF(GPInvoice.CaseDocID IS NULL, AH.CaseDocID, GPInvoice.CaseDocID),
+       Marketer = IIF(GPInvoice.Marketer IS NULL, C.MarketerCode, GPInvoice.Marketer)
+  FROM
+       [IMECentricEW].[dbo].tblCase AS C
+       INNER JOIN [IMECentricEW].[dbo].tblAcctHeader AS AH ON AH.CaseNbr = C.CaseNbr
+       INNER JOIN [IMECentricEW].[dbo].tblEWFacility AS EWfac ON EWfac.EWFacilityID = AH.EWFacilityID
+  WHERE
+       GPInvoice.DocumentNo = CAST(AH.DocumentNbr AS VARCHAR(15))
+       AND GPInvoice.GPFacilityID = EWfac.GPFacility
+
+
