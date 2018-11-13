011cac05ebe2e42a91cf0311175630d410951e07
David Rose <david.rose@examworks.com>
2018-11-13 11:56:48 -0500

    DB Release 3.42

---
+++ 3.42/Stored Procedures/proc_GetReferralSummarySinceLastLoginDateCount.sql
@@ -0,0 +1,24 @@
+﻿CREATE PROCEDURE [dbo].[proc_GetReferralSummarySinceLastLoginDateCount]
+
+@LastLoginDate datetime,
+@UserCode int,
+@UserType char(2)
+
+AS
+BEGIN
+
+	SET NOCOUNT ON
+	DECLARE @Err int
+
+    SELECT COUNT(tblCase.casenbr) FROM tblCase
+		INNER JOIN tblPublishOnWeb ON tblCase.casenbr = tblPublishOnWeb.tablekey
+		WHERE (tblPublishOnWeb.tabletype = 'tblCase')
+			AND (tblPublishOnWeb.PublishOnWeb = 1)
+			AND (tblPublishOnWeb.UserType = @userType)
+			AND (tblPublishOnWeb.UserCode = @userCode)
+			AND (tblPublishOnWeb.dateadded > @LastLoginDate)
+
+	SET @Err = @@Error
+
+	RETURN @Err
+END
