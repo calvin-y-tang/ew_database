f78d6c537e36fed7cbd0f4a12c7d28626e5126d1
Calvin Tang <calvin.tang@examworks.com>
2018-11-13 11:30:31 -0500

    Remove last version data script

--- IMECentricDB/trunk/Data/NextRelease.sql
+++ IMECentricDB/trunk/Data/NextRelease.sql
@@ -1,13 +1,3 @@
--- Issue 7708 - delete previous values from tblSettings that are no longer used and create a new generic setting
-DELETE FROM tblSetting WHERE Name = 'EnableMultEEAddrDocs'
-GO
-DELETE FROM tblSetting WHERE Name = 'MultEEAddrDocsOffices'
-GO 
--- TODO: fill in a semi-colon delimited list of office codes that you want to enable new gen docs for
-INSERT INTO tblSetting (Name, Value)
-VALUES('UseNewGenDocFormOffices', ';;')
-GO 
-
 -- Issue 7804 Data patch for Market in GPInvoice table
 UPDATE [IMECentricMaster].[dbo].[GPInvoice]
   SET 
