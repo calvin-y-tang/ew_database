aa3c6098d64662f2e618d9fde0def86fb688bab7
Kevin.Chen <Kevin.Chen@examworks.com>
2018-12-18 18:13:35 +0000

    Issue 8065 - mi-Clinic

--- IMECentricDB/trunk/Tables/tblEWTimeZone.sql
+++ IMECentricDB/trunk/Tables/tblEWTimeZone.sql
@@ -3,6 +3,7 @@
     [Name]                       VARCHAR (100) NOT NULL,
     [SupportsDaylightSavingTime] BIT           NOT NULL,
     [BaseUtcOffsetSec]           INT           NOT NULL,
+    [ShortDesc]					 VARCHAR(10)   NULL, 
     CONSTRAINT [PK_EWTimeZone] PRIMARY KEY CLUSTERED ([EWTimeZoneID] ASC)
 );
 
