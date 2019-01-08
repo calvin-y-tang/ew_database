5ea4bf75bc3b3cf3504f8c6708fa3f17a3c01063
Terri Lyde <terri.lyde@examworks.com>
2019-01-08 13:29:55 -0500

    Issue 8016 - Added new function and new view for the new Peer Bills report.  Function pulls value fomr key-value pairs in a string.

---
+++ IMECentricDB/trunk/Views/vwRptProgressivePeerBills.sql
@@ -0,0 +1,30 @@
+﻿CREATE [dbo].[vwRptProgressivePeerBills]
+AS
+SELECT
+ CPB.CaseNbr,
+ CPB.DateBillReceived AS DateSentToClient,
+ C.InvoiceAmt,
+ CPB.ProviderName,
+ CPB.ProviderZip,
+ -- provider region
+ CPB.ServiceDate,
+ CPB.ServiceEndDate,
+ CPB.ServiceRendered,
+ CPB.BillAmount,
+ CPB.BillAmountApproved,
+ CPB.BillAmountDenied,
+ CPB.BillNumber,
+ ProviderAttorney = 
+  CASE
+	WHEN REPLACE([IMECentricEW].[dbo].fnReturnValueFromKeyInString(Param, 'ProviderAttorney'), '"', '') = 'Yes' THEN 'Y'
+	ELSE 'N'
+  END,  
+
+ CL.CompanyCode,
+ dbo.fnDateValue(CPB.DateBillReceived) AS FilterDate
+
+ FROM tblCasePeerBill AS CPB
+ INNER JOIN tblCase AS C ON CPB.CaseNbr = C.CaseNbr
+ INNER JOIN tblClient AS CL ON CL.ClientCode = C.ClientCode
+ LEFT OUTER JOIN tblCustomerData AS CD ON CD.TableKey = CPB.CaseNbr AND CD.TableType = 'tblCase' AND CD.CustomerName = 'Progressive PeerReview'
+ WHERE 1=1
