5b618cb3f71746f4b2b1a59d31e60006660f2cd7
David Rose <david.rose@examworks.com>
2019-01-09 09:40:34 -0500

    DB Release 3.46

---
+++ 3.46/Stored Procedures/proc_WebUserAccount_Delete.sql
@@ -0,0 +1,24 @@
+﻿
+
+CREATE PROCEDURE [proc_WebUserAccount_Delete]
+(
+ @WebUserID int,
+ @UserCode int
+)
+AS
+BEGIN
+
+ SET NOCOUNT OFF
+ DECLARE @Err int
+
+ DELETE
+ FROM [tblWebUserAccount]
+ WHERE
+  [WebUserID] = @WebUserID AND
+  [UserCode] = @UserCode
+ SET @Err = @@Error
+
+ RETURN @Err
+END
+
+
---
+++ 3.46/Tables/tblDoctorDrAssistant.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE dbo.tblDoctorDrAssistant(
+	[DrAssistantID] INT			NOT NULL,
+	[DoctorCode]	INT			NOT NULL,
+	[UserIDAdded]	VARCHAR(15) NOT NULL,
+	[DateAdded]		DATETIME	NOT NULL
+    CONSTRAINT [PK_tblDoctorDrAssistant] PRIMARY KEY CLUSTERED ([DrAssistantID] ASC, [DoctorCode] ASC)
+);
