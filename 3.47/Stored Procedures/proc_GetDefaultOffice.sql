9a1c62bef85eb23af65befc99edf1e8e6d433f92
David Rose <david.rose@examworks.com>
2019-01-16 12:32:23 -0500

    DB Release 3.47

---
+++ 3.47/Stored Procedures/proc_GetDefaultOffice.sql
@@ -0,0 +1,18 @@
+﻿
+CREATE PROCEDURE [proc_GetDefaultOffice]
+
+AS
+BEGIN
+
+	SET NOCOUNT ON
+	DECLARE @Err int
+
+	SELECT MIN(OfficeCode)
+
+	FROM [tblOffice]
+	WHERE Status = 'Active'
+	
+	SET @Err = @@Error
+
+	RETURN @Err
+END
---
+++ 3.47/Views/vwDoctorScheduleSummary.sql
@@ -0,0 +1,27 @@
+﻿ 
+ CREATE VIEW vwDoctorScheduleSummary
+AS  
+   SELECT  tblDoctor.LastName ,
+            tblDoctor.FirstName ,
+            tblLocation.Location ,
+            tblDoctorSchedule.Date ,
+            tblDoctorSchedule.Status ,
+            tblLocation.InsideDr ,
+            tblDoctor.DoctorCode ,
+            tblDoctorOffice.OfficeCode ,
+            tblDoctorSchedule.LocationCode ,
+            tblDoctor.Booking ,
+            tblDoctorSchedule.CaseNbr1 ,
+            tblDoctorSchedule.CaseNbr2 ,
+            tblDoctorSchedule.CaseNbr3 ,
+            tblDoctorSchedule.CaseNbr4 ,
+            tblDoctorSchedule.CaseNbr5 ,
+            tblDoctorSchedule.CaseNbr6 ,
+            tblDoctorSchedule.StartTime, 
+			tblLocationOffice.OfficeCode as LocationOffice 
+    FROM    tblDoctorSchedule
+            INNER JOIN tblDoctor ON tblDoctorSchedule.DoctorCode = tblDoctor.DoctorCode
+            INNER JOIN tblLocation ON tblDoctorSchedule.LocationCode = tblLocation.LocationCode
+            INNER JOIN tblDoctorOffice ON tblDoctor.DoctorCode = tblDoctorOffice.DoctorCode 
+			inner join tbllocationoffice on (tbllocationoffice.officecode = tblDoctorOffice.OfficeCode and tblLocationOffice.LocationCode = tbllocation.LocationCode) 
+    WHERE   ( tblDoctorSchedule.Status <> 'Off' )
