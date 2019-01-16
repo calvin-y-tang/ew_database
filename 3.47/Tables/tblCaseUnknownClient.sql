45f030972a654fcc1b2faccc3b1f03d9f7cf581b
David Rose <david.rose@examworks.com>
2019-01-16 12:26:34 -0500

    Re-building

--- 3.47/Tables/tblCaseUnknownClient.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[tblCaseUnknownClient] (
-    [CaseNbr]       INT          NOT NULL,
-    [ReferralEmail] VARCHAR (70) NULL,
-    [Processed]     BIT          NULL,
-    CONSTRAINT [PK_tblCaseUnknownClient] PRIMARY KEY CLUSTERED ([CaseNbr] ASC)
-);
-
