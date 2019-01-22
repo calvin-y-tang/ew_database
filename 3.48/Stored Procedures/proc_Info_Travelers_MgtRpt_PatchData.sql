34f41c924b5bb98c1ae0a6f4f60def62ecf62963
David Rose <david.rose@examworks.com>
2019-01-22 14:54:25 -0500

    DB Release 3.48

---
+++ 3.48/Stored Procedures/proc_Info_Travelers_MgtRpt_PatchData.sql
@@ -0,0 +1,52 @@
+﻿CREATE PROCEDURE [dbo].[proc_Info_Travelers_MgtRpt_PatchData]
+	@startDate Date,
+	@endDate Date,
+	@ewFacilityIdList VarChar(255)
+AS
+
+  print 'update results with exam and other fee amounts'
+  UPDATE TI SET 
+		TI.ExamCost = LI.FeeDetailExam, 
+		TI.OtherCosts = LI.FeeDetailOther
+  FROM ##tmp_TravelersInvoices AS TI
+	LEFT OUTER JOIN 	  
+    (select tAD.HeaderID,
+		sum(case when tEWFC.Mapping6 = 'Exam' then tAD.ExtAmountUS else 0 end) as FeeDetailExam,
+		sum(case when tEWFC.Mapping6 = 'Other' or tEWFC.Mapping6 is null then tAD.ExtAmountUS else 0 end) as FeeDetailOther
+		from tblAcctHeader as tAH
+		inner join tblAcctDetail as tAD on tAH.HeaderID = tAD.HeaderID
+		inner join tblCase as tC on tAH.CaseNbr = tC.CaseNbr
+		left outer join tblFRCategory as tFRC on (tC.CaseType = tFRC.CaseType) and (tAD.ProdCode = tFRC.ProductCode)
+		left outer join tblEWFlashCategory as tEWFC on tFRC.EWFlashCategoryID = tEWFC.EWFlashCategoryID
+		group by tAD.HeaderID
+	 ) LI on TI.HeaderID = LI.HeaderID
+
+	print 'Retrieve the most recent Report Date Viewed from the portal for each case ...'
+	UPDATE ti SET ti.ReportDateViewed = docs.DateViewed
+	  FROM ##tmp_TravelersInvoices as ti
+		INNER JOIN (
+		SELECT *
+			FROM (SELECT ROW_NUMBER() OVER (PARTITION BY cd.CaseNbr ORDER BY cd.SeqNo DESC) as ROWNUM, cd.CaseNbr, cd.SeqNo, pow.DateViewed
+					FROM tblCaseDocuments as cd
+						LEFT OUTER JOIN tblPublishOnWeb as pow on cd.SeqNo = pow.TableKey and pow.TableType = 'tblCaseDocuments'
+					WHERE (cd.CaseNbr IN (Select pr.CaseNbr FROM ##tmp_TravelersInvoices as pr))
+					AND (cd.Type = 'Report') 
+					) as tbl
+			WHERE tbl.ROWNUM = 1) AS docs ON ti.CaseNbr = docs.CaseNbr	 
+
+	print 'calculate all Turn-Around-Time Values'
+	UPDATE ti SET ti.ReferralToScheduledBusDays = [dbo].[fnGetBusinessDays](ti.ReferralDate, ti.ScheduledDate, 1)
+	  FROM ##tmp_TravelersInvoices as ti  
+
+	UPDATE ti SET 
+				ti.ReferralToMedRecsRecvdCalDays = DATEDIFF(DAY, ti.ReferralDate, ti.MedicalRecordsReceived),
+				ti.ScheduledToApptCalDays = DATEDIFF(DAY, ti.ScheduledDate, ti.ApptDate),
+				ti.ApptToReportSentCalDays = DATEDIFF(DAY, ti.ApptDate, ti.ReportSent)				
+	  FROM ##tmp_TravelersInvoices as ti  
+	
+	print 'remove scheduled date for NON-IME Services'
+	UPDATE ##tmp_TravelersInvoices SET ScheduledDate = NULL WHERE ProductName <> 'IME'	  
+
+    print 'Return results'
+    SELECT *
+      FROM ##tmp_TravelersInvoices
---
+++ 3.48/Stored Procedures/spCaseHistory.sql
@@ -0,0 +1,62 @@
+﻿CREATE PROCEDURE [dbo].[spCaseHistory]
+    @iCaseNbr INT ,
+    @sSort VARCHAR(10) ,
+    @sIncludeExclude VARCHAR(1) ,
+    @sTypes VARCHAR(120) ,
+    @blnNoAccting INT
+AS
+    DECLARE @sSelectStmt NVARCHAR(2000) ,
+        @sCaseNbr AS NVARCHAR(10) ,
+        @WhereClause AS NVARCHAR(500) ,
+        @NoAccting AS NVARCHAR(100)
+    SET NOCOUNT ON
+    SET @sCaseNbr = CAST(@iCaseNbr AS VARCHAR(10))
+
+    IF @blnNoAccting = 1
+        BEGIN
+            SET @NoAccting = ' and (CH.type is null or CH.type <> ''Acct'')  '
+        END
+    ELSE
+        BEGIN
+            SET @NoAccting = ''
+        END
+    IF @sIncludeExclude = ''  -- select everything
+        BEGIN
+            SET @WhereClause = 'where CH.CaseNbr = '
+                + @sCaseNbr 
+        END
+    IF @sIncludeExclude = 'A'  -- include all records for this case
+        BEGIN
+            SET @WhereClause = 'where CH.CaseNbr = '
+                + @sCaseNbr 
+        END
+
+    IF @sIncludeExclude = 'I'  -- only include certain types
+        BEGIN
+            SET @WhereClause = 'where CH.CaseNbr = '
+                + @sCaseNbr + ' and (CH.type in (' + @stypes
+                + '))' 
+        END
+    IF @sIncludeExclude = 'E'  -- only exclude certain types
+        BEGIN
+            SET @WhereClause = 'where CH.CaseNbr = '
+                + @sCaseNbr
+                + ' and (CH.type is null or CH.type not in ('
+                + @sTypes + '))' 
+        END
+    SET @sSelectStmt = 'SELECT TOP 100 PERCENT CH.CaseNbr, CH.EventDate, CH.EventDesc, CH.UserID, CH.Highlight, '
+        + 'CH.otherinfo, CH.type, CH.status, '
+        + 'CASE WHEN CH.type = ''StatChg'' THEN cast(isnull(CH.duration / 24, DATEDIFF(day, C.laststatuschg, '
+        + 'GETDATE())) AS decimal(6, 1)) END AS IQ, CH.ID, C.LastStatusChg, CH.Duration, '
+        + 'CH.PublishOnWeb, CH.PublishedTo, CH.UserIDEdited '
+        + 'FROM tblCaseHistory AS CH INNER JOIN tblCase AS C ON CH.CaseNbr = C.CaseNbr '
+        + @WhereClause + @NoAccting
+        + ' ORDER BY CH.EventDate ' + @sSort 
+
+--print @sselectstmt
+    EXEC Sp_executesql @sSelectStmt
+
+
+GO
+
+
