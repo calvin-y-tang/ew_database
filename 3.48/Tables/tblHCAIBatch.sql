15dbaecfff20d827c2771bd8c993525209017817
David Rose <david.rose@examworks.com>
2019-01-22 14:50:04 -0500

    Redoing Script

--- 3.48/Tables/tblHCAIBatch.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[tblHCAIBatch] (
-    [HCAIBatchNumber] INT          IDENTITY (100, 1) NOT NULL,
-    [DateAdded]       DATETIME     NULL,
-    [UserIDAdded]     VARCHAR (50) NULL,
-    CONSTRAINT [PK_tblHCAIBatch] PRIMARY KEY CLUSTERED ([HCAIBatchNumber] ASC)
-);
-
