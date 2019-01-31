bcdfd4a76cc1a1af53fb7b3591064c4842619b75
Calvin Tang <calvin.tang@examworks.com>
2019-01-31 13:12:12 -0500

    Issue 8016 - add index for lookup

--- IMECentricDB/trunk/Tables/tblCustomerMapping.sql
+++ IMECentricDB/trunk/Tables/tblCustomerMapping.sql
@@ -1,12 +1,24 @@
 ﻿CREATE TABLE [dbo].[tblCustomerMapping] (
-    [CustomerMappingID]   INT          IDENTITY (1, 1) NOT NULL,
-    [EntityType]          VARCHAR (2)  NOT NULL,
-    [EntityID]            INT          NOT NULL,
-    [TableType]           VARCHAR (50) NOT NULL,
-    [TableKey]            INT          NOT NULL,
-    [MappingName]         VARCHAR (50) NOT NULL,
-    [MappingValue]        VARCHAR (50) NOT NULL,
-    [IsPrimary]           BIT          NOT NULL CONSTRAINT [DF_tblCustomerMapping_IsPrimary] DEFAULT 0,
+    [CustomerMappingID] INT           IDENTITY (1, 1) NOT NULL,
+    [EntityType]        VARCHAR (2)   NOT NULL,
+    [EntityID]          INT           NOT NULL,
+    [TableType]         VARCHAR (50)  NOT NULL,
+    [TableKey]          INT           NOT NULL,
+    [MappingName]       VARCHAR (50)  NOT NULL,
+    [MappingValue]      VARCHAR (100) NOT NULL,
+    [IsPrimary]         BIT           CONSTRAINT [DF_tblCustomerMapping_IsPrimary] DEFAULT ((0)) NOT NULL,
     CONSTRAINT [PK_tblCustomerMapping] PRIMARY KEY CLUSTERED ([CustomerMappingID] ASC)
 );
+
+
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_tblCustomerMapping_MappingNameTableTypeTableKeyEntityTypeEntityID]
+    ON [dbo].[tblCustomerMapping]([MappingName] ASC, [TableType] ASC, [TableKey] ASC, [EntityType] ASC, [EntityID] ASC);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_tblCustomerMapping_EntityTypeEntityIDTableType]
+    ON [dbo].[tblCustomerMapping]([EntityType] ASC, [EntityID] ASC, [TableType] ASC);
 
