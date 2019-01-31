165a2e5f2f282d3800054d20c3f85631eecbd454
David Rose <david.rose@examworks.com>
2019-01-31 13:29:10 -0500

    DB Release 3.49

---
+++ 3.49/Stored Procedures/proc_CaseType_LoadAll.sql
@@ -0,0 +1,22 @@
+﻿
+
+CREATE PROCEDURE [proc_CaseType_LoadAll]
+
+AS
+BEGIN
+
+ SET NOCOUNT ON
+ DECLARE @Err int
+
+ SELECT *
+
+ FROM [tblCaseType]
+ WHERE PublishOnWeb = 1
+ ORDER BY [description]
+
+ SET @Err = @@Error
+
+ RETURN @Err
+END
+
+
---
+++ 3.49/Views/vwCaseReports.sql
@@ -0,0 +1,29 @@
+﻿CREATE VIEW [dbo].[vwCaseReports]
+AS 
+
+	SELECT 
+		CaseNbr AS DocCaseNbr, 
+        document,
+        type,
+        description,
+        sfilename,
+        dateadded,
+        useridadded,
+        reporttype,
+        PublishOnWeb,
+        dateedited,
+        useridedited,
+        seqno,
+		PublishedTo,
+        Source,
+        FileSize,
+        Pages,
+		FolderID,
+		SubFolder, 
+		CaseDocTypeID,
+		SharedDoc, 
+		MasterCaseNbr
+	FROM    
+		dbo.tblCaseDocuments AS CD
+	WHERE
+		( type = 'Report' )
