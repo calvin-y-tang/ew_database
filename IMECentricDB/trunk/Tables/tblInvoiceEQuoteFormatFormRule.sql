9ac664578207f2efb621d83463b02e0f2f9e0f63
Terri Lyde <terri.lyde@examworks.com>
2019-02-19 16:11:52 -0500

    Issue 8118 - added a table for the new form

---
+++ IMECentricDB/trunk/Tables/tblInvoiceEQuoteFormatFormRule.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[tblInvoiceEQuoteFormatFormRule](
+	[InvoiceEQuoteFormatFormRuleID] [int] IDENTITY(1,1) NOT NULL,
+	[Priority]						[int]			NOT NULL,
+	[CompanyCode]					[int]			NOT NULL,
+	[CaseType]						[int]			NULL,
+	[Jurisdiction]					[varchar](2)	NULL,
+	[EWServiceTypeID]				[int]			NULL,
+	[eQuoteDocument]				[varchar] (15)	NOT NULL,
+	[eQuoteRequiredWhenScheduled]	[bit]			NOT NULL,
+	[Exclude]						[bit]			NOT NULL,
+ CONSTRAINT [PK_tblInvoiceEQuoteFormatFormRule] PRIMARY KEY CLUSTERED 
+(
+	[InvoiceEQuoteFormatFormRuleID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+
+
