df8a346107454caea77d6ea7448fceb1b77693a6
Terri Lyde <terri.lyde@examworks.com>
2019-02-25 15:56:11 -0500

    Issue 8118 - renamed the table for the form from tblInvoiceEQuoteFormatFormRule to tblInvoiceVoucherQuoteRule; added a stored procedure to handle checking for a rule and returning document if a rule exists

---
+++ IMECentricDB/trunk/Stored Procedures/spGetInvoiceVoucherQuoteRules.sql
@@ -0,0 +1,25 @@
+﻿CREATE PROCEDURE [dbo].[spGetInvoiceVoucherQuoteRules]
+(
+    @caseNbr INT,
+    @quoteType VARCHAR(10),
+    @statusID INT,
+    @acctQuoteID INT
+)
+AS
+BEGIN
+
+	SET NOCOUNT ON
+
+	SELECT IVQR.QuoteDocument 
+	FROM tblInvoiceVoucherQuoteRule AS IVQR
+		LEFT OUTER JOIN tblCase AS C ON C.CaseNbr = @CaseNbr
+		INNER JOIN tblServices AS tS ON C.ServiceCode = tS.ServiceCode
+
+	WHERE IVQR.CaseType = C.CaseType
+	AND IVQR.Jurisdiction = C.Jurisdiction
+	AND IVQR.CompanyCode = C.CompanyCode
+	AND IVQR.EWServiceTypeID = ts.EWServiceTypeID
+	ORDER BY InvoiceVoucherQuoteRuleID
+
+END
+GO
--- IMECentricDB/trunk/Tables/tblInvoiceVoucherQuoteRule.sql
+++ IMECentricDB/trunk/Tables/tblInvoiceVoucherQuoteRule.sql
@@ -1,18 +1,19 @@
-﻿CREATE TABLE [dbo].[tblInvoiceEQuoteFormatFormRule](
-	[InvoiceEQuoteFormatFormRuleID] [int] IDENTITY(1,1) NOT NULL,
+﻿CREATE TABLE [dbo].[tblInvoiceVoucherQuoteRule](
+	[InvoiceVoucherQuoteRuleID]     [int] IDENTITY(1,1) NOT NULL,
 	[ProcessOrder]					[int]			NOT NULL,
 	[CompanyCode]					[int]			NOT NULL,
 	[CaseType]						[int]			NULL,
 	[Jurisdiction]					[varchar](2)	NULL,
 	[EWServiceTypeID]				[int]			NULL,
-	[eQuoteDocument]				[varchar] (15)	NOT NULL,
+	[QuoteDocument]				    [varchar] (15)	NOT NULL,
 	[eQuoteRequiredWhenScheduled]	[bit]			NOT NULL,
 	[Exclude]						[bit]			NOT NULL,
 	[QuoteType]						[int]			NOT NULL,
 	[ParentCompanyID]				[int]			NOT NULL,
+	[Handling]                      [varchar] (20)  NOT NULL,
  CONSTRAINT [PK_tblInvoiceEQuoteFormatFormRule] PRIMARY KEY CLUSTERED 
 (
-	[InvoiceEQuoteFormatFormRuleID] ASC
+	[InvoiceVoucherQuoteRuleID] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 
