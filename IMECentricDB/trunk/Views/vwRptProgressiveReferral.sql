1b4e8e4c010f80ca5846cf5566b3cb4502321efc
Calvin Tang <calvin.tang@examworks.com>
2019-02-26 17:28:57 -0500

    Issue 8180 - Include all service types

--- IMECentricDB/trunk/Views/vwRptProgressiveReferral.sql
+++ IMECentricDB/trunk/Views/vwRptProgressiveReferral.sql
@@ -17,6 +17,7 @@ SELECT
  C.DateOfInjury,
 
  FZ.Name AS FeeZone,
+
  (SELECT TOP 1 Description FROM tblCaseDocuments AS CD WHERE (CD.CaseNbr=C.CaseNbr OR (CD.MasterCaseNbr=C.MasterCaseNbr AND CD.SharedDoc=1)) 
      AND Description LIKE '% EFF[ .]%' ORDER BY SeqNo DESC) AS LastEff,
 
@@ -32,4 +33,3 @@ SELECT
  LEFT OUTER JOIN tblEWFeeZone AS FZ ON FZ.EWFeeZoneID = C.EWFeeZoneID
  LEFT OUTER JOIN tblLocation AS L ON C.DoctorLocation=L.LocationCode
  WHERE 1=1
- AND S.EWServiceTypeID=1
