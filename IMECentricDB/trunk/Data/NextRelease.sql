60440ad1184c84524fbcb57f307dcfe0e0d7db2e
Terri Lyde <terri.lyde@examworks.com>
2019-03-06 10:53:23 -0500

    Issue 8336 - need to add the new form to the table of queue forms for selection

---
+++ IMECentricDB/trunk/Data/NextRelease.sql
@@ -0,0 +1,5 @@
+-- Issue 8336 - Add Requested Doctor to Misc Select Form  (TL)
+----  Created new form frmStatusReqDr which can replace frmStatusMiscSelect.
+----  Form needs to be added to the list of queue forms
+INSERT INTO [dbo].[tblQueueForms] VALUES ('frmStatusReqDr', ' Form with Requested Doctor')
+
