c09a096dab23172c8bb0c1903a2ced92d19cc11f
Jose Pais <jose.pais@examworks.com>
2019-03-06 13:24:00 -0500

    Issue 7857 - populate some new config tables for IN/VO Quote feature

--- IMECentricDB/trunk/Data/NextRelease.sql
+++ IMECentricDB/trunk/Data/NextRelease.sql
@@ -3,3 +3,51 @@
 ----  Form needs to be added to the list of queue forms
 INSERT INTO [dbo].[tblQueueForms] VALUES ('frmStatusReqDr', ' Form with Requested Doctor')
 
+-- Issue 7857 - populate some new config tables for Quote feature
+INSERT INTO tblQuoteHandling(Description, DateAdded, UserIDAdded)
+VALUES('Fee Quote', GetDate(), 'Admin'),
+      ('Fee Approval', GetDate(), 'Admin')
+GO
+INSERT INTO tblQuoteFeeConfig(FeeValueName, QuoteType, DisplayOrder, DateAdded, UserIDAdded)
+VALUES('Diagnostic Study', NULL, 10, GetDate(), 'Admin'), 
+      ('Record Review', NULL, 20, GetDate(), 'Admin'), 
+	  ('Report Prep', NULL, 30, GetDate(), 'Admin'), 
+	  ('Travel', NULL, 40, GetDate(), 'Admin'), 
+	  ('Consultation', NULL, 50, GetDate(), 'Admin'), 
+	  ('Exam Room Rental', NULL, 60, GetDate(), 'Admin'), 
+	  ('Indexing Chart Prep', NULL, 70, GetDate(), 'Admin')
+GO
+-- tblQuoteStatus (Data Patch)
+UPDATE tblQuoteStatus 
+   SET QuoteHandlingID = 1 
+ WHERE QuoteStatusID IN (1, 2, 3, 6)
+ GO
+UPDATE tblQuoteStatus 
+   SET QuoteHandlingID = 1 
+ WHERE QuoteStatusID IN (4, 5)
+GO
+-- New Tokens for IN/VO Quotes
+INSERT INTO tblMessageToken (Name)
+VALUES('@ClientAddr1@'), 
+      ('@ClientAddr2@'),
+	  ('@ClientAddr3@'), 
+	  ('@ClientCity@'),
+	  ('@ClientState@'),
+	  ('@ClientZip@'),
+	  ('@QuoteFeeRange@'),
+	  ('@QuoteDiagnosticStudyFee@'),
+	  ('@QuoteRecordReviewFee@'),
+	  ('@QuoteReportPrepFee@'),
+	  ('@QuoteTravelFee@'),
+	  ('@QuoteConsultationFee@'),
+	  ('@QuoteExamRoomRentalFee@'),
+	  ('@QuoteIndexingChartPrepFee@'),
+	  ('@QuoteAdditionalFees@'),
+	  ('@QuoteDiagnosticStudyUnit@'),
+	  ('@QuoteRecordReviewUnit@'),
+	  ('@QuoteReportPrepUnit@'),
+	  ('@QuoteTravelUnit@'),
+	  ('@QuoteConsultationUnit@'),
+	  ('@QuoteExamRoomRentalUnit@'),
+	  ('@QuoteIndexingChartPrepUnit@')
+GO
