ec939c4a6f677b2fcb9c7dff5a29155a6ef33b4f
Jose Pais <jose.pais@examworks.com>
2019-03-14 18:42:49 -0400

    Issue 8067 - added Panel Doctors; added address details to result set (used in GenDoc)

--- IMECentricDB/trunk/Views/vwCaseDocMailEntities.sql
+++ IMECentricDB/trunk/Views/vwCaseDocMailEntities.sql
@@ -5,6 +5,7 @@ AS
 			tblCase.CaseNbr,
 			'CL' AS EntityType, tblCase.ClientCode AS EntityID, 
 			tblClient.firstName + ' ' + tblClient.lastName AS EntityName, 
+			Addr1, Addr2, City, State, Zip,
 			dbo.fnIsAddressComplete(Addr1, Addr2, City, State, Zip) AS AddrMsg
 		FROM tblCase 
 				INNER JOIN tblClient ON tblClient.ClientCode = tblCase.ClientCode
@@ -14,6 +15,7 @@ AS
 			tblCase.CaseNbr,
 			'BCL' AS EntityType, tblCase.ClientCode AS EntityID, 
 			tblClient.firstName + ' ' + tblClient.lastName AS EntityName, 
+			Addr1, Addr2, City, State, Zip,
 			dbo.fnIsAddressComplete(Addr1, Addr2, City, State, Zip) AS AddrMsg
 		FROM tblCase 
 				INNER JOIN tblClient ON tblClient.ClientCode = tblCase.BillClientCode
@@ -23,25 +25,38 @@ AS
 			tblCase.CaseNbr, 
 			'EE' AS EntityType, tblCase.ChartNbr AS EntityID, 
 			tblExaminee.firstName + ' ' + tblExaminee.lastName AS EntityName,
+			Addr1, Addr2, City, State, Zip, 
 			dbo.fnIsAddressComplete(Addr1, Addr2, City, State, Zip) AS AddrMsg
 		FROM tblCase 
 				INNER JOIN tblExaminee ON tblExaminee.ChartNbr = tblCase.ChartNbr
 	UNION
-		-- Doctor
-		-- TODOJP: Need to account for panel exam!!!
+		-- Doctor (Normal Appt)
 		SELECT 
 			tblCase.CaseNbr,
 			'DR' as EntityType, tblCase.DoctorCode AS EntityID, 
 			tblDoctor.firstName + ' ' + tblDoctor.lastName AS EntityName,
+			Addr1, Addr2, City, State, Zip, 
 			dbo.fnIsAddressComplete(Addr1, Addr2, City, State, Zip) AS AddrMsg
 		FROM tblCase 
 				INNER JOIN tblDoctor ON tblDoctor.DoctorCode = tblCase.DoctorCode
+	UNION
+		-- Doctor(s) (Appt Panel)
+		SELECT 
+			tblCase.CaseNbr,
+			'DR' as EntityType, tblDoctor.DoctorCode AS EntityID, 
+			tblDoctor.firstName + ' ' + tblDoctor.lastName AS EntityName,
+			Addr1, Addr2, City, State, Zip, 
+			dbo.fnIsAddressComplete(Addr1, Addr2, City, State, Zip) AS AddrMsg
+		FROM tblCase
+				INNER JOIN tblCasePanel ON tblCasePanel.PanelNbr = tblCase.PanelNbr
+				INNER JOIN tblDoctor ON tblDoctor.DoctorCode = tblCasePanel.DoctorCode
 	UNION
 		-- Plaintiff Attorney
 		SELECT 
 			tblCase.CaseNbr, 
 			'PAT' as EntityType, tblCase.PlaintiffAttorneyCode AS EntityID, 
 			ISNULL(Attrny.firstName + ' ' + Attrny.lastName, Attrny.Company) AS EntityName,
+			Address1, Address2, City, State, Zip,
 			dbo.fnIsAddressComplete(Address1, Address2, City, State, Zip) AS AddrMsg
 		FROM tblCase 
 				INNER JOIN tblCCAddress AS Attrny ON Attrny.ccCode = tblCase.PlaintiffAttorneyCode
@@ -51,6 +66,7 @@ AS
 			tblCase.CaseNbr, 
 			'DAT' as EntityType, tblCase.DefenseAttorneyCode AS EntityID, 
 			ISNULL(Attrny.firstName + ' ' + Attrny.lastName, Attrny.Company) AS EntityName,
+			Address1, Address2, City, State, Zip, 
 			dbo.fnIsAddressComplete(Address1, Address2, City, State, Zip) AS AddrMsg
 		FROM tblCase 
 				INNER JOIN tblCCAddress AS Attrny ON Attrny.ccCode = tblCase.DefenseAttorneyCode
@@ -58,9 +74,10 @@ AS
 		-- Treating Physician
 		SELECT 
 			tblCase.CaseNbr, 
-			'TDR' as EntityType, 
+			'TD' as EntityType, 
 			IIF(tblExaminee.TreatingPhysician IS NULL OR LEN(tblExaminee.TreatingPhysician) = 0, NULL, tblCase.ChartNbr) AS EntityID,
 			tblExaminee.TreatingPhysician AS EntityName,
+			TreatingPhysicianAddr1, '', TreatingPhysicianCity, TreatingPhysicianState, TreatingPhysicianZip, 
 			IIF(tblExaminee.TreatingPhysician IS NULL OR LEN(tblExaminee.TreatingPhysician) = 0, 
 				'', 
 				dbo.fnIsAddressComplete(TreatingPhysicianAddr1, '', TreatingPhysicianCity, TreatingPhysicianState, TreatingPhysicianZip)) AS AddrMsg
@@ -70,8 +87,9 @@ AS
 		-- NF-10 Doctors
 		SELECT 
 			tblCase.CaseNbr, 
-			'NFDR' as EntityType, TreatDr.TreatingDoctorID AS EntityID, 
+			'NF' as EntityType, TreatDr.TreatingDoctorID AS EntityID, 
 			LTRIM(IIF(LEFT(LTRIM(FirstName), 1) = '-', '', FirstName) + ' ' + LastName) AS EntityName, 
+			Addr1, Addr2, City, State, Zip,
 			dbo.fnIsAddressComplete(Addr1, Addr2, City, State, Zip) AS AddrMsg
 		FROM tblCase 
 				INNER JOIN tblCaseOtherTreatingDoctor AS CaseTreatDr ON CaseTreatDr.CaseNbr = tblCase.CaseNbr
@@ -82,6 +100,7 @@ AS
 			tblCase.CaseNbr, 
 			'CC' as EntityType, tblCCAddress.ccCode AS EntityID, 
 			ISNULL(tblCCAddress.firstName + ' ' + tblCCAddress.lastName, tblCCAddress.Company) AS EntityName,
+			tblCCAddress.Address1, tblCCAddress.Address2, tblCCAddress.City, tblCCAddress.State, tblCCAddress.Zip,
 			dbo.fnIsAddressComplete(tblCCAddress.Address1, tblCCAddress.Address2, tblCCAddress.City, tblCCAddress.State, tblCCAddress.Zip) AS AddrMsg
 		FROM tblCase 
 				INNER JOIN tblExaminee ON tblExaminee.ChartNbr = tblCase.ChartNbr
