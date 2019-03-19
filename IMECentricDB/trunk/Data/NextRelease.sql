7a13220dec1c2866fdca94b30885cd3b947376b6
Jose Pais <jose.pais@examworks.com>
2019-03-19 11:16:09 -0400

    Issue 8408 - new business rules for setting Other email on distribute form

--- IMECentricDB/trunk/Data/NextRelease.sql
+++ IMECentricDB/trunk/Data/NextRelease.sql
@@ -9,13 +9,13 @@ VALUES('Fee Quote', GetDate(), 'Admin'),
       ('Fee Approval', GetDate(), 'Admin')
 GO
 INSERT INTO tblQuoteFeeConfig(FeeValueName, QuoteType, DisplayOrder, DateAdded, UserIDAdded)
-VALUES('Diagnostic Study', NULL, 10, GetDate(), 'Admin'), 
-      ('Record Review', NULL, 20, GetDate(), 'Admin'), 
-	  ('Report Prep', NULL, 30, GetDate(), 'Admin'), 
-	  ('Travel', NULL, 40, GetDate(), 'Admin'), 
-	  ('Consultation', NULL, 50, GetDate(), 'Admin'), 
-	  ('Exam Room Rental', NULL, 60, GetDate(), 'Admin'), 
-	  ('Indexing Chart Prep', NULL, 70, GetDate(), 'Admin')
+VALUES('Diagnostic Study', NULL, 20, GetDate(), 'Admin'), 
+      ('Record Review', NULL, 50, GetDate(), 'Admin'), 
+	  ('Report Prep', NULL, 60, GetDate(), 'Admin'), 
+	  ('Travel', NULL, 70, GetDate(), 'Admin'), 
+	  ('Consultation', NULL, 10, GetDate(), 'Admin'), 
+	  ('Exam Room Rental', NULL, 30, GetDate(), 'Admin'), 
+	  ('Indexing Chart Prep', NULL, 40, GetDate(), 'Admin')
 GO
 -- tblQuoteStatus (Data Patch)
 UPDATE tblQuoteStatus 
@@ -53,8 +53,6 @@ VALUES('@ClientAddr1@'),
 GO
 
 
-
-
 DELETE FROM tblCodes WHERE Category='WorkCompCaseType' AND SubCategory='CA'
 GO
 INSERT INTO tblCodes(Category, SubCategory, Value) VALUES 
@@ -88,3 +86,22 @@ GO
 INSERT INTO tblSetting (Name, Value)
 VALUES('AllowDistDocToEnvelope', '')
 GO	
+
+-- Issue 8408 - new business rules for setting Other email on distribute form
+-- DEV NTOE: setting these as Incative. Michelle Andjus will tell us when this is to be made "active"
+DECLARE @iBusRuleID INTEGER
+INSERT INTO tblBusinessRule(Name, Category, Descrip, IsActive, EventID, AllowOverride, Param1Desc, BrokenRuleAction)
+VALUES('DistDocDefaultOtherEmail', 'Case', 'Set Default Email for Other Party when distributing document', 0, 1202, 0, 'OtherEmail', 0)
+SELECT @iBusRuleID = @@IDENTITY
+	
+INSERT INTO tblBusinessRuleCondition(EntityType, EntityID, BillingEntity, ProcessOrder, BusinessRuleID, DateAdded, UserIDAdded, DateEdited, UserIDEdited, OfficeCode, EWBusLineID, EWServiceTypeID, Jurisdiction, Param1, Param2, Param3, Param4, Param5)
+VALUES('PC', 46, 2, 1, @iBusRuleID, GetDate(), 'Admin', GetDate(), 'Admin', null, null, null, null, 'MCCUTPAOffices@Sentry.com', null, null, null, null)
+
+INSERT INTO tblBusinessRule(Name, Category, Descrip, IsActive, EventID, AllowOverride, Param1Desc, BrokenRuleAction)
+VALUES('DistRptDefaultOtherEmail', 'Report', 'Set Default Email for Other Party when distributing report', 0, 1320, 0, 'OtherEmail', 0)
+SELECT @iBusRuleID = @@IDENTITY
+
+INSERT INTO tblBusinessRuleCondition(EntityType, EntityID, BillingEntity, ProcessOrder, BusinessRuleID, DateAdded, UserIDAdded, DateEdited, UserIDEdited, OfficeCode, EWBusLineID, EWServiceTypeID, Jurisdiction, Param1, Param2, Param3, Param4, Param5)
+VALUES('PC', 46, 2, 1, @iBusRuleID, GetDate(), 'Admin', GetDate(), 'Admin', null, null, null, null, 'MCCUTPAOffices@Sentry.com', null, null, null, null)
+GO
+
