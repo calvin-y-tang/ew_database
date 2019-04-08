3e05eb0b0e39f1a6a742ac626ce093487a053d9c
Terri Lyde <terri.lyde@examworks.com>
2019-04-08 13:46:22 -0400

    Issue 10048 - set the values for the new field AlertType based on the old field ExceptionAlert and FollowUpDate

--- IMECentricDB/trunk/Data/NextRelease.sql
+++ IMECentricDB/trunk/Data/NextRelease.sql
@@ -4,3 +4,5 @@ INSERT INTO tblExceptionList (Description, Status, DateAdded, UserIDAdded, DateE
 VALUES('SLA Violation', 'Active', GETDATE(), 'Admin', GETDATE(), 'Admin')
 GO
 
+-- Issue 10048 - no longer using field ExceptionAlert.  Replacing with AlertType.  Set the values
+UPDATE tblCaseHistory SET AlertType = 1 WHERE ExceptionAlert = 1 OR FollowUpDate IS NOT NULL
