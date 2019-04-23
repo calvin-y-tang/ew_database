eaa693e4859c2988d03fd2a564c50247c6ea94ec
David Rose <david.rose@examworks.com>
2019-04-23 06:48:49 -0400

    DB Release 3.53

---
+++ 3.53/Stored Procedures/proc_GetDocumentShareByGuid.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE [proc_GetDocumentShareByGuid]
+
+@Guid varchar(36)
+
+AS
+
+SELECT tblCaseDocuments.CaseNbr, tblCaseDocuments.sFileName, tblCaseDocuments.Description, tblWebActionLink.UserType, 
+	tblWebActionLink.UserCode, ISNULL(tblPublishOnWeb.Viewed, 0) Viewed, tblCaseDocuments.SeqNo, tblWebActionLink.Param,
+	ISNULL(tblPublishOnWeb.PublishAsPDF, 0) PublishAsPDF, tblCaseDocuments.Type, tblCaseDocType.Description DocTypeDesc, 
+	tblCaseDocuments.DateAdded, tblWebActionLink.ExpirationDate
+	FROM tblWebActionLink
+		INNER JOIN tblWebActionLinkItem ON tblWebActionLink.WebActionLinkID = tblWebActionLinkItem.WebActionLinkID
+		INNER JOIN tblCaseDocuments ON tblWebActionLinkItem.ActionKey = tblCaseDocuments.SeqNo
+		INNER JOIN tblPublishOnWeb ON tblCaseDocuments.SeqNo = tblPublishOnWeb.TableKey
+		INNER JOIN tblCaseDocType ON tblCaseDocuments.CaseDocTypeId = tblCaseDocType.CaseDocTypeId
+		WHERE tblPublishOnWeb.TableType = 'tblCaseDocuments'
+		AND tblPublishOnWeb.UserCode = tblWebActionLink.UserCode
+		AND tblWebActionLink.UniqueKey = @Guid
+
---
+++ 3.53/Stored Procedures/proc_GetProgressiveClients.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [proc_GetProgressiveClients]
+
+@CompanyCode int
+
+AS
+
+SELECT DISTINCT lastname + ', ' + firstname + ' ' + tblCompany.intname name, clientcode FROM tblclient
+	INNER JOIN tblCompany ON tblClient.CompanyCode = tblCompany.CompanyCode
+	INNER JOIN tblWebUser ON tblClient.ClientCode = tblWebUser.IMECentricCode and tblWebUser.UserType = 'CL'
+	WHERE tblClient.CompanyCode = @CompanyCode
+		AND tblClient.Status = 'Active'
+	ORDER BY name
