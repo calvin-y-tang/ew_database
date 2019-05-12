f1b2f188baacdf3e1ebe676ecee86a8d608da6d5
David Rose <david.rose@examworks.com>
2019-05-11 21:01:07 -0400

    Line was trying to insert a value that doesn't accept NULLs.  Added the ID field after verifying that all tables in production matched.

--- Scripts/3.53.sql
+++ Scripts/3.53.sql
@@ -288,7 +288,7 @@ GO
 
 -- -Issue 10022 - add new Exception trigger
 INSERT INTO tblExceptionList (Description, Status, DateAdded, UserIDAdded, DateEdited, UserIDEdited)
-VALUES('SLA Violation', 'Active', GETDATE(), 'Admin', GETDATE(), 'Admin')
+VALUES(31, 'SLA Violation', 'Active', GETDATE(), 'Admin', GETDATE(), 'Admin')
 GO
 
 -- Issue 10048 - no longer using field ExceptionAlert.  Replacing with AlertType.  Set the values
