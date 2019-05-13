5abc2cbdadcebf826d6855c9e042a5c845109f2c
David Rose <david.rose@examworks.com>
2019-05-13 13:43:11 -0400

    Added from NextRelease from trunk.  There were probably problems with the merge on this script.

--- Scripts/3.53.sql
+++ Scripts/3.53.sql
@@ -285,3 +285,9 @@ PRINT N'Update complete.';
 
 
 GO
+
+
+INSERT INTO tblBusinessRule (BusinessRuleID, Name, Category, Descrip, IsActive, EventID, AllowOverride, Param1Desc, Param2Desc, Param3Desc, Param4Desc, Param5Desc, BrokenRuleAction)
+VALUES(103, 'DistAsZipToClient', 'Case', 'Distribute doc/rpt to client as a password protected zip file', 1, 1202, 0, 'Password', NULL, NULL, NULL, NULL, 0)
+GO 
+
