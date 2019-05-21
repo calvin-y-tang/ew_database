024128c5b200143ac1c7ac36f051a4ef28b4cee7
Calvin Tang <calvin.tang@examworks.com>
2019-05-21 11:13:36 -0400

    Prepare data script files for S37

--- IMECentricDB/trunk/Data/NextRelease.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO tblBusinessRule (BusinessRuleID, Name, Category, Descrip, IsActive, EventID, AllowOverride, Param1Desc, Param2Desc, Param3Desc, Param4Desc, Param5Desc, BrokenRuleAction)
-VALUES(103, 'DistAsZipToClient', 'Case', 'Distribute doc/rpt to client as a password protected zip file', 1, 1202, 0, 'Password', NULL, NULL, NULL, NULL, 0)
-GO 
