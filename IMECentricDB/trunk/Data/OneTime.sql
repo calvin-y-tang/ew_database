a84d00a91433e12561d94adc49f418fc18f4b489
Jose Pais <jose.pais@examworks.com>
2019-05-23 09:43:28 -0400

    Issue 11085 - changes to EWParentCompany; set initial value for "added" columns.

--- IMECentricDB/trunk/Data/OneTime.sql
+++ IMECentricDB/trunk/Data/OneTime.sql
@@ -1,1 +1,14 @@
 ﻿
+-- Issue 11085 - new columns to add to IMECentricMaster
+ALTER TABLE EWParentCompany ADD 
+	[DateAdded]                      DATETIME    NULL, 
+	[UserIDAdded]                    VARCHAR(15) NULL, 
+	[DateEdited]                     DATETIME    NULL, 
+	[UserIDEdited]                   VARCHAR(15) NULL
+GO
+
+-- Issue 11085 - set initial value for parent company add columns
+UPDATE EWParentCompany 
+   SET DateAdded = ISNULL(DateAdded, '2019-01-01'), 
+       UserIDAdded = ISNULL(UserIDAdded, 'Admin')
+GO 
