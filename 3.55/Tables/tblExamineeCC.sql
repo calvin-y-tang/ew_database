b6fec20e56c11373bf3194e0ae4f889def4f1c7c
David Rose <david.rose@examworks.com>
2019-06-14 13:19:13 -0400

    DB Release 3.55

---
+++ 3.55/Tables/tblExamineeCC.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[tblExamineeCC] (
+    [ChartNbr] INT NOT NULL,
+    [ccCode]   INT NOT NULL,
+    CONSTRAINT [PK_tblExamineeCC] PRIMARY KEY CLUSTERED ([ChartNbr] ASC, [ccCode] ASC)
+);
+
