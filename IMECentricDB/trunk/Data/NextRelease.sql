b5132b3346f82368481da68a067ba45c21cd1b32
Jose Pais <jose.pais@examworks.com>
2019-06-25 10:08:44 -0400

    Issue 11119 - add new Exception type for Generate Quote document.

---
+++ IMECentricDB/trunk/Data/NextRelease.sql
@@ -0,0 +1,8 @@
+
+
+-- Issue 11119 - add new item to tblExceptioList for Generate Quote Document
+INSERT INTO tblExceptionList (ExceptionID, Description, Status, DateAdded, UserIDAdded, DateEdited, UserIDEdited)
+VALUES(31, 'Generate Quote Document', 'Active', '2019-06-21 09:28:00.000', 'Admin', '2019-06-21 09:28:00.000', 'Admin')
+GO 
+
+
