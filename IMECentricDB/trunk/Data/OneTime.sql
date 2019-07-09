a00680c100f81750d69262655948706089a3bd45
Calvin Tang <calvin.tang@examworks.com>
2019-07-09 15:50:50 -0400

    Prepare DB Project for S39

--- IMECentricDB/trunk/Data/OneTime.sql
+++ IMECentricDB/trunk/Data/OneTime.sql
@@ -1,28 +1,4 @@
-﻿--S37
-
--- Issue 11085 - new columns to add to IMECentricMaster for EWParentCompany 
-ALTER TABLE EWParentCompany ADD 
-	[DateAdded] DATETIME NULL, 
-	[UserIDAdded] VARCHAR(15) NULL, 
-	[DateEdited] DATETIME NULL, 
-	[UserIDEdited] VARCHAR(15) NULL
-GO
-
--- Issue 11085 - set initial value for parent company add columns
-UPDATE EWParentCompany 
-   SET DateAdded = ISNULL(DateAdded, '2019-01-01'), 
-       UserIDAdded = ISNULL(UserIDAdded, 'Admin')
-GO 
-
--- Issue 11093 - new columns to add to IMECentricMaster for EWParentCompany 
-ALTER TABLE EWParentCompany ADD 
-	[DICOMHandlingPreference] INT NULL
-GO
-
-
-
-
--- Sprint 38
+﻿-- Sprint 39
 -- Issue 111142 - Convert Allstate tblCustomerFeeHeader to new FeeSchedule tables (tblFS*****)
 -- >>>>>>>>>>>>>>>>>> START SCRIPT 111142 <<<<<<<<<<<<<<<<<<<<
 DECLARE @iOrigFeeHdrID INT = 1 -- Allstate
