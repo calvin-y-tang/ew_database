83b7115e647e465e2e3c4c1bc91bd1f097e4fb36
Calvin Tang <calvin.tang@examworks.com>
2019-07-29 22:30:37 -0400

    Script Revision

--- Scripts/3.44.sql
+++ Scripts/3.44.sql
@@ -493,7 +493,3 @@ PRINT N'Update complete.';
 
 
 GO
----Issue 7809 Progressive - Add link in email for report
-INSERT INTO tblMessageToken (Name,Description) 
-VALUES ('@ViewDocumentURL@','') 
-GO
