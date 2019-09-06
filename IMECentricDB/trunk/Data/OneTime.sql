21d169a2b843e4d041a631dc54a354bac12e6371
Calvin Tang <calvin.tang@examworks.com>
2019-09-06 13:23:41 -0400

    Prepare project for DB Release

--- IMECentricDB/trunk/Data/OneTime.sql
+++ IMECentricDB/trunk/Data/OneTime.sql
@@ -76,3 +76,108 @@ GO
 INSERT INTO tblSetting (Name, Value) VALUES ('DPSCancelBundleSendFile', 'True')
 GO
 
+
+
+-- DEV NOTE: this business rule condition will need to adjusted for the target deployment DB
+--INSERT INTO tblBusinessRuleCondition(EntityType, EntityID, BillingEntity, ProcessOrder, BusinessRuleID, DateAdded, UserIDAdded, DateEdited, UserIDEdited, OfficeCode, EWBusLineID, EWServiceTypeID, Jurisdiction, Param1, Param2, Param3, Param4, Param5)
+--VALUES('CO', 6741, 2, 1, 104, '2019-08-12', 'Admin', '2019-08-12', 'Admin', NULL, NULL, NULL, NULL, '000808', NULL, '525', NULL, 'ClaimEmployerOverride')
+--GO
+
+-- DEV NOTE: these business rule conditions will need to adjusted for the target deployment DB. Need to set the EntityCode to the
+--		desired CompanyCode Value in the target DB
+--INSERT INTO tblBusinessRuleCondition(EntityType, EntityID, BillingEntity, ProcessOrder, BusinessRuleID, DateAdded, UserIDAdded, DateEdited, UserIDEdited, OfficeCode, EWBusLineID, EWServiceTypeID, Jurisdiction, Param1, Param2, Param3, Param4, Param5)
+--VALUES('CO', 0, 2, 2, 105, '2019-08-13', 'Admin', '2019-08-13', 'Admin', NULL, NULL, NULL, NULL, '', '', '', NULL, NULL),
+--	  ('CO', 0, 2, 2, 106, '2019-08-13', 'Admin', '2019-08-13', 'Admin', NULL, NULL, NULL, NULL, '', '', '', NULL, NULL),
+--	  ('CO', 0, 2, 2, 107, '2019-08-13', 'Admin', '2019-08-13', 'Admin', NULL, NULL, NULL, NULL, '', '', '', NULL, NULL),
+
+--    ('CO', 0, 2, 1, 105, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 12, NULL, NULL, NULL, 'COIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 106, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 12, NULL, NULL, NULL, 'COIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 107, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 12, NULL, NULL, NULL, 'COIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+
+--	  ('CO', 0, 2, 1, 105, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 29, NULL, NULL, NULL, 'WVIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 106, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 29, NULL, NULL, NULL, 'WVIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 107, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 29, NULL, NULL, NULL, 'WVIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+
+--	  ('CO', 0, 2, 1, 105, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 32, NULL, NULL, NULL, 'TXIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 106, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 32, NULL, NULL, NULL, 'TXIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 107, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 33, NULL, NULL, NULL, 'TXIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 105, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 33, NULL, NULL, NULL, 'TXIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 106, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 33, NULL, NULL, NULL, 'TXIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 107, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 33, NULL, NULL, NULL, 'TXIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 105, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 34, NULL, NULL, NULL, 'TXIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 106, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 34, NULL, NULL, NULL, 'TXIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 107, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 34, NULL, NULL, NULL, 'TXIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+
+--	  ('CO', 0, 2, 1, 105, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 36, NULL, NULL, NULL, 'ILIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 106, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 36, NULL, NULL, NULL, 'ILIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 107, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 36, NULL, NULL, NULL, 'ILIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+
+--	  ('CO', 0, 2, 1, 105, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 22, NULL, NULL, NULL, 'MidwestIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 106, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 22, NULL, NULL, NULL, 'MidwestIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 107, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 22, NULL, NULL, NULL, 'MidwestIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+
+--	  ('CO', 0, 2, 1, 105, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 20, NULL, NULL, NULL, 'DEIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 106, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 20, NULL, NULL, NULL, 'DEIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 107, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 20, NULL, NULL, NULL, 'DEIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+
+--	  ('CO', 0, 2, 1, 105, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 26, NULL, NULL, NULL, 'NYIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 106, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 26, NULL, NULL, NULL, 'NYIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 107, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 26, NULL, NULL, NULL, 'NYIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 105, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 27, NULL, NULL, NULL, 'NYIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 106, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 27, NULL, NULL, NULL, 'NYIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 107, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 27, NULL, NULL, NULL, 'NYIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 105, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 28, NULL, NULL, NULL, 'NYIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 106, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 28, NULL, NULL, NULL, 'NYIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 107, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 28, NULL, NULL, NULL, 'NYIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+	   
+--	  ('CO', 0, 2, 1, 105, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 31, NULL, NULL, NULL, 'PAIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 106, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 31, NULL, NULL, NULL, 'PAIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 107, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 31, NULL, NULL, NULL, 'PAIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+
+--	  ('CO', 0, 2, 1, 105, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 30, NULL, NULL, NULL, 'ORIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 106, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 30, NULL, NULL, NULL, 'ORIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 107, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 30, NULL, NULL, NULL, 'ORIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 105, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 35, NULL, NULL, NULL, 'ORIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 106, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 35, NULL, NULL, NULL, 'ORIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 107, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 35, NULL, NULL, NULL, 'ORIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+
+--	  ('CO', 0, 2, 1, 105, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 13, NULL, NULL, NULL, 'CAIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 106, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 13, NULL, NULL, NULL, 'CAIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 107, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 13, NULL, NULL, NULL, 'CAIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 105, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 14, NULL, NULL, NULL, 'CAIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 106, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 14, NULL, NULL, NULL, 'CAIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 107, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 14, NULL, NULL, NULL, 'CAIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 105, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 15, NULL, NULL, NULL, 'CAIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 106, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 15, NULL, NULL, NULL, 'CAIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 107, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 15, NULL, NULL, NULL, 'CAIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 105, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 16, NULL, NULL, NULL, 'CAIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 106, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 16, NULL, NULL, NULL, 'CAIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 107, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 16, NULL, NULL, NULL, 'CAIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 105, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 38, NULL, NULL, NULL, 'CAIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 106, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 38, NULL, NULL, NULL, 'CAIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 107, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 38, NULL, NULL, NULL, 'CAIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 105, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 39, NULL, NULL, NULL, 'CAIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 106, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 39, NULL, NULL, NULL, 'CAIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 107, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 39, NULL, NULL, NULL, 'CAIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 105, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 40, NULL, NULL, NULL, 'CAIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 106, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 40, NULL, NULL, NULL, 'CAIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 107, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 40, NULL, NULL, NULL, 'CAIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+
+--	  ('CO', 0, 2, 1, 105, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 23, NULL, NULL, NULL, 'NJIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 106, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 23, NULL, NULL, NULL, 'NJIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 107, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 23, NULL, NULL, NULL, 'NJIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 105, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 24, NULL, NULL, NULL, 'NJIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 106, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 24, NULL, NULL, NULL, 'NJIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 107, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 24, NULL, NULL, NULL, 'NJIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 105, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 25, NULL, NULL, NULL, 'NJIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 106, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 25, NULL, NULL, NULL, 'NJIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 107, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 25, NULL, NULL, NULL, 'NJIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 105, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 41, NULL, NULL, NULL, 'NJIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 106, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 41, NULL, NULL, NULL, 'NJIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 107, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 41, NULL, NULL, NULL, 'NJIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+
+--	  ('CO', 0, 2, 1, 105, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 19, NULL, NULL, NULL, 'MAIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 106, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 19, NULL, NULL, NULL, 'MAIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL), 
+--	  ('CO', 0, 2, 1, 107, '2019-08-13', 'Admin', '2019-08-13', 'Admin', 19, NULL, NULL, NULL, 'MAIME@stradix.com', 'IMEStatus@strdix.com', ' imereports@stradix.com', NULL, NULL)
+--GO 
+
