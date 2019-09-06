aa5e392964f6a5a659c93e0657af4cf14e990cf0
Calvin Tang <calvin.tang@examworks.com>
2019-09-06 13:26:29 -0400

    Prepare project for DB Release

--- IMECentricDB/branches/CurrentRelease/Data/NextRelease.sql
+++ IMECentricDB/branches/CurrentRelease/Data/NextRelease.sql
@@ -1,65 +1,15 @@
 
---// add new data fields values for IMEC-11200 (Create TAT fields and setup calculations in IMEC)
-insert into tblDataField (DataFieldID, TableName, FieldName, Descrip) values
-(117, 'tblCase', 'TATDateLossToApptDate', '')
-,(118, 'tblCase', 'TATInitialApptDateToApptDate', '')
-,(119, 'tblCase', 'TATDateReceivedToInitialApptDate', '')
-,(212, 'tblCase', 'DateOfInjury', 'Date of Loss')
-,(213, 'tblCase', 'OrigApptTime', 'Initial Appt Date')
-;
+
+-- Issue 11119 - add new item to tblExceptioList for Generate Quote Document
+INSERT INTO tblExceptionList (ExceptionID, Description, Status, DateAdded, UserIDAdded, DateEdited, UserIDEdited)
+VALUES(31, 'Generate Quote Document', 'Active', '2019-06-21 09:28:00.000', 'Admin', '2019-06-21 09:28:00.000', 'Admin')
+GO 
 
 
---// inserting new calc method details for IMEC-11200 (Create TAT fields and setup calculations in IMEC)
-insert into tblTATCalculationMethod (TATCalculationMethodID, StartDateFieldID, EndDateFieldID, Unit, TATDataFieldID, UseTrend)
-select 
-	17,
-	212,
-	208, 
-	'Day', 
-	117, 
-	0
-union all
-select 
-	18,
-	213, 
-	208, 
-	'Day', 
-	118, 
-	0
-union all
-select 
-	19,
-	201, 
-	213, 
-	'Day', 
-	119, 
-	0
-;
-
---// insert new calc group details for IMEC-11200 (Create TAT fields and setup calculations in IMEC)
-insert into tblTATCalculationGroupDetail (TATCalculationGroupID, TATCalculationMethodID, DisplayOrder) values
-	(1, 17, 13)
-	,(1, 18, 14)
-	,(1, 19, 15)
-
-	,(2, 17, 13)
-	,(2, 18, 14)
-	,(2, 19, 15)
-	
-	,(3, 17, 13)
-	,(3, 18, 14)
-	,(3, 19, 15)
-	
-	,(4, 17, 13)
-	,(4, 18, 14)
-	,(4, 19, 15)
-;
-
-
-
-
-
-
+-- Issue 11142 - Network Fee Schedule Calculation Method (switch between "new" and "old" fee schedule tables)
+INSERT INTO tblSetting(Name, Value)
+VALUES('NetworkFeeSchedCalcMethod', '1')
+GO
 
 
 
---
+++ IMECentricDB/branches/CurrentRelease/Tables/tblFSDetail.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[tblFSDetail]
+(
+	[FSDetailID]    INT IDENTITY (1, 1) NOT NULL,
+	[FSHeaderID]    INT         NOT NULL,
+	[ProcessOrder]  INT         NOT NULL, 
+	[FeeUnit]       VARCHAR(25) NOT NULL,
+	[FeeAmt]        MONEY       NOT NULL,
+	[NSFeeAmt1]     MONEY       NULL,
+	[NSFeeAmt2]     MONEY       NULL,
+	[NSFeeAmt3]     MONEY       NULL,
+	[LateCancelAmt] MONEY       NULL,
+	[CancelDays]    INT         NULL,
+    [DateAdded]     DATETIME    NOT NULL,
+    [UserIDAdded]   VARCHAR(30) NOT NULL,
+    [DateEdited]    DATETIME    NULL,
+    [UserIDEdited]  VARCHAR(30) NULL,
+	CONSTRAINT [PK_tblFSDetail] PRIMARY KEY CLUSTERED ([FSDetailID] ASC)
+)
