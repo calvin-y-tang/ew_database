740d765574ca1be8a86f7ce16e52596f9358836f
David Rose <david.rose@examworks.com>
2019-09-27 06:36:36 -0400

    DB Release 3.60

---
+++ 3.60/Stored Procedures/proc_CaseType_LoadByPrimaryKey.sql
@@ -0,0 +1,23 @@
+﻿
+
+CREATE PROCEDURE [proc_CaseType_LoadByPrimaryKey]
+(
+ @Code int
+)
+AS
+BEGIN
+ SET NOCOUNT ON
+ DECLARE @Err int
+
+ SELECT *
+
+ FROM [tblCaseType]
+ WHERE
+  ([Code] = @Code)
+
+ SET @Err = @@Error
+
+ RETURN @Err
+END
+
+
---
+++ 3.60/Tables/tblWebPortalVersionRule.sql
@@ -0,0 +1,15 @@
+CREATE TABLE [dbo].[tblWebPortalVersionRule](
+	[WebPortalVersionRule] [int] IDENTITY(1,1) NOT NULL,
+	[ProcessOrder] [int] NOT NULL,
+	[UserType] [varchar](2) NULL,
+	[WebUserID] [int] NULL,
+	[CompanyCode] [int] NULL,
+	[ParentCompanyID] [int] NULL,
+	[EffectiveDate] [datetime] NOT NULL,
+	[PortalVersion] [int] NOT NULL,
+	[DateAdded] [datetime] NULL,
+	[UserIDAdded] [varchar](15) NULL,
+	[DateEdited] [datetime] NULL,
+	[UserIDEdited] [varchar](15) NULL,
+ CONSTRAINT [PK_tblWebPortalVersionRule] PRIMARY KEY CLUSTERED ([WebPortalVersionRule] ASC)
+);
