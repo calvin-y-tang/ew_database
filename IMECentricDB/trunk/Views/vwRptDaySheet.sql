5920c9ef7fb00dc20b73775e9514087f16c50f43
Terri Lyde <terri.lyde@examworks.com>
2019-10-23 13:38:43 -0400

    Issue 11302 - cleanup of view - removed items not being used

--- IMECentricDB/trunk/Views/vwRptDaySheet.sql
+++ IMECentricDB/trunk/Views/vwRptDaySheet.sql
@@ -2,57 +2,49 @@
 -- Issue 4744 - JAP - added DoctorOffice column to result set
 CREATE VIEW vwRptDaySheet
 AS
-     select CA.CaseApptID AS SchedCode ,
-		    CA.LocationCode ,
-            CAST(CAST(CA.ApptTime AS DATE) AS DATETIME) AS date,
-            CA.ApptTime AS StartTime, 
-            '' AS Description ,
-            --tblApptStatus.Name  AS Status,
+
+     SELECT CA.CaseApptID AS RecID,  -- SchedCode
             CA.DoctorCode ,            
+			CA.LocationCode ,
+            CAST(CAST(CA.ApptTime AS DATE) AS DATETIME) AS Date,
+            CA.ApptTime AS StartTime, 
+
             C.CaseNbr , 
-		    C.ExtCaseNbr , 
-            CO.ExtName AS Company ,
+			C.ExtCaseNbr , 
+            CAST(C.SpecialInstructions AS VARCHAR(1000)) AS SpecialInstructions ,
+            C.PhotoRqd ,
+            C.PanelNbr ,
+            C.DoctorName AS PanelDesc,
+            C.OfficeCode AS CaseOfficeCode,
+
+            CASE WHEN C.InterpreterRequired = 1 THEN 'Interpreter'
+                 ELSE ''
+            END AS Interpreter,
+
+            CT.Description AS Casetypedesc ,
+
             EE.FirstName + ' ' + EE.LastName AS ExamineeName ,
-            EE.Sex ,
+
+            CO.ExtName AS Company ,
+
+            CL.FirstName + ' ' + CL.LastName AS ClientName ,
+            CL.Phone1 AS ClientPhone ,
+
+			LO.OfficeCode as LocationOffice,
             L.Location,
-		    L.Addr1,
+			L.Addr1,
             L.Addr2,
             L.City,
             L.State,
             L.Zip,
-            EWF.LegalName AS CompanyName ,
-            ISNULL(DR.FirstName, '') + ' ' + ISNULL(DR.LastName, '') + ', ' + ISNULL(DR.Credentials, '') AS DoctorName ,
-            C.ClaimNbr ,
-            CL.FirstName + ' ' + CL.LastName AS ClientName ,
-            CT.Description AS Casetypedesc ,
-		    CT.EWBusLineID, 
-            S.Description AS Servicedesc ,
-            CAST(C.SpecialInstructions AS VARCHAR(1000)) AS specialinstructions ,
-            C.WCBNbr ,
             L.Phone AS DoctorPhone ,
             L.Fax AS DoctorFax ,
-            C.PhotoRqd ,
-            CL.Phone1 AS ClientPhone ,
-            C.DoctorName AS Paneldesc ,
-            C.PanelNbr ,
-            NULL AS Panelnote ,
-            C.OfficeCode,
-            CASE WHEN C.CaseNbr IS NULL
-                 THEN 'CaseNbr1desc'
-                 ELSE NULL
-            END AS ScheduleDescription ,
-            S.ShortDesc ,
-            EWF.Fax ,
-            CASE WHEN C.InterpreterRequired = 1 
-			  THEN 'Interpreter'
-                 ELSE ''
-            END AS Interpreter ,
-		    CASE WHEN C.LanguageID > 0 
-			  THEN LA.Description
-			  ELSE ''
-		    END AS [Language],
-            1 AS Duration ,
-            CO.IntName AS CompanyIntName ,
+
+            EWF.LegalName AS CompanyName ,
+
+            ISNULL(DR.FirstName, '') + ' ' + ISNULL(DR.LastName, '') + ', ' + ISNULL(DR.Credentials, '') AS DoctorName ,
+		    DRO.OfficeCode as DoctorOffice,
+
             CASE WHEN ( SELECT TOP 1
                                 Type
                         FROM    tblRecordHistory
@@ -62,10 +54,15 @@ AS
 			  THEN 'Films'
                  ELSE ''
             END AS films , 
-		  LO.OfficeCode as LocationOffice, 
-		  DRO.OfficeCode as DoctorOffice
-    FROM    tblCaseAppt AS CA
 
+            S.ShortDesc ,
+
+		    CASE WHEN C.LanguageID > 0 
+			  THEN LA.Description
+			  ELSE ''
+		    END AS [Language]
+
+    FROM    tblCaseAppt AS CA
 				INNER JOIN tblCase AS C ON CA.CaseApptID = C.CaseApptID
 				INNER JOIN tblExaminee AS EE on C.ChartNbr = EE.ChartNbr
 				INNER JOIN tblClient AS CL ON C.ClientCode = CL.ClientCode
@@ -84,5 +81,4 @@ AS
 				INNER JOIN tblLocationOffice AS LO ON LO.OfficeCode = DRO.OfficeCode AND LO.LocationCode = L.LocationCode
 				LEFT JOIN tblLanguage AS LA ON LA.LanguageID = C.LanguageID	
 				WHERE CA.ApptStatusID IN (10,100,101,102)
-
 
