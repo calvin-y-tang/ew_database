71847b32c378da108a99603984936ed440b6555c
David Rose <david.rose@examworks.com>
2019-10-30 12:39:02 -0400

    DB Release 3.61

---
+++ 3.61/Tables/tblEWLocation.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[tblEWLocation] (
+    [EWLocationID] INT          NOT NULL,
+    [Name]         VARCHAR (30) NULL,
+    [GPLocation]   VARCHAR (3)  NULL,
+    [Active]       BIT          NULL,
+    [Accting]      VARCHAR (5)  NULL,
+    CONSTRAINT [PK_tblEWLocation] PRIMARY KEY CLUSTERED ([EWLocationID] ASC)
+);
+
