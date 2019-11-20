6d3875386a96e02cebe1c8dfcc940d58fdebf7c1
David Rose <david.rose@examworks.com>
2019-11-20 16:01:00 -0500

    DB Release 3.62

---
+++ 3.62/Functions/fnIsWorkDay.sql
@@ -0,0 +1,28 @@
+﻿
+
+--Determin if a given day is a work day.  Currently it only checks if the day is a weekday, not taking holiday into consideration
+CREATE FUNCTION fnIsWorkDay
+(
+  @Date DATETIME
+)
+RETURNS BIT
+BEGIN
+
+DECLARE @isWorkDay BIT
+
+SELECT
+  @isWorkDay =
+  CASE (DATEPART(dw, @Date) + @@DATEFIRST) % 7
+    WHEN 1 THEN 0 --'Sunday'
+    WHEN 2 THEN 1 --'Monday'
+    WHEN 3 THEN 1 --'Tuesday'
+    WHEN 4 THEN 1 --'Wednesday'
+    WHEN 5 THEN 1 --'Thursday'
+    WHEN 6 THEN 1 --'Friday'
+    WHEN 0 THEN 0 --'Saturday'
+  END
+
+RETURN @isWorkDay
+
+END
+
---
+++ 3.62/Stored Procedures/proc_WebUser_LoadAll.sql
@@ -0,0 +1,21 @@
+﻿
+
+CREATE PROCEDURE [proc_WebUser_LoadAll]
+
+AS
+BEGIN
+
+ SET NOCOUNT ON
+ DECLARE @Err int
+
+ SELECT *
+
+ FROM [tblWebUser]
+
+
+ SET @Err = @@Error
+
+ RETURN @Err
+END
+
+
