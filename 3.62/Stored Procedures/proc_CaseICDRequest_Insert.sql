d42266c3be1f9ea5277a87c8abfc3bccdcf70907
David Rose <david.rose@examworks.com>
2019-12-03 15:25:33 -0500

    DB Release 3.62

---
+++ 3.62/Stored Procedures/proc_CaseICDRequest_Insert.sql
@@ -0,0 +1,49 @@
+﻿
+CREATE PROCEDURE [proc_CaseICDRequest_Insert]
+(
+	@seqno int = NULL output,
+	@casenbr int,
+	@ICDCode varchar(10) = NULL,
+	@Status varchar(50) = NULL,
+	@Description varchar(200) = NULL,
+	@DateAdded datetime = NULL,
+	@UserIDAdded varchar(15) = NULL,
+	@DateEdited datetime = NULL,
+	@UserIDEdited varchar(15) = NULL
+)
+AS
+BEGIN
+
+	SET NOCOUNT OFF
+	DECLARE @Err int
+
+	INSERT
+	INTO [tblCaseICDRequest]
+	(
+		[casenbr],	
+		[ICDCode],	
+		[Status],
+		[Description],	
+		[DateAdded],	
+		[UserIDAdded],	
+		[DateEdited],	
+		[UserIDEdited]
+	)
+	VALUES
+	(
+		@casenbr,	
+		@ICDCode,	
+		@Status,
+		@Description,	
+		@DateAdded,	
+		@UserIDAdded,	
+		@DateEdited,	
+		@UserIDEdited
+	)
+
+	SET @Err = @@Error
+
+	SELECT @seqno = SCOPE_IDENTITY()
+
+	RETURN @Err
+END
---
+++ 3.62/Stored Procedures/proc_ExamineeCheckForDupe.sql
@@ -0,0 +1,24 @@
+﻿
+CREATE PROCEDURE [proc_ExamineeCheckForDupe]
+
+@FirstName varchar(50) = NULL,
+@LastName varchar(50) = NULL,
+@Phone1 varchar(50) = NULL
+
+AS
+
+SET NOCOUNT OFF
+DECLARE @Err int
+
+
+SELECT TOP 1 chartnbr FROM tblExaminee WHERE chartnbr > 0 
+ AND FirstName = COALESCE(@FirstName,FirstName) 
+ AND LastName = COALESCE(@LastName,LastName) 
+ AND 
+  REPLACE( REPLACE( REPLACE( REPLACE(Phone1, '(', '' ), ')', '' ), ' ', '' ), '-', '' ) = 
+  COALESCE( REPLACE( REPLACE( REPLACE( REPLACE(@Phone1, '(', '' ), ')', '' ), ' ', '' ), '-', '' ),Phone1) 
+ ORDER BY chartnbr
+ 
+SET @Err = @@Error
+RETURN @Err
+
