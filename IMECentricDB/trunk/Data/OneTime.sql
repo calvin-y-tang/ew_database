ac47d524c2ce297162bea5df1cc53642b02d6e61
Sam.Chiang <Sam.Chiang@examworks.com>
2020-01-15 15:58:02 +0000

    Issue 11382 - Added in data insert for the new tokens for examinee first name, last name, and middle initial.

--- IMECentricDB/trunk/Data/OneTime.sql
+++ IMECentricDB/trunk/Data/OneTime.sql
@@ -1,1 +1,6 @@
 ﻿INSERT INTO tblUserFunction VALUES ('AckNewPortalAcct', 'Acknowledge - New Portal Accts Auto Provision', '2019-12-12')
+
+--// IMEC-11382 - adding record into the tblMessageToken for the examinee last name, first name, and middle inital
+insert into tblMessageToken (Name, Description)
+values ('@ExamineeLastName@',''), ('@ExamineeFirstName@',''), ('@ExamineeMiddleInitial@','')
+;
