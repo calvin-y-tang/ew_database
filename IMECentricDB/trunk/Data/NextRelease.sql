1d03e45d15d213f3d0bc968ce1b7d91cd76d3040
Jose Pais <jose.pais@examworks.com>
2020-01-21 15:50:29 -0500

    Issue 11433 - new business rules for Zurich document/report dist/generate

--- IMECentricDB/trunk/Data/NextRelease.sql
+++ IMECentricDB/trunk/Data/NextRelease.sql
@@ -11,3 +11,11 @@ SET IDENTITY_INSERT [dbo].[tblDPSPriority] OFF
 
 GO
 
+-- Issue 11433
+INSERT INTO tblBusinessRule (BusinessRuleID, Name, Category, Descrip, IsActive, EventID, AllowOverride, Param1Desc, Param2Desc, Param3Desc, Param4Desc, Param5Desc, BrokenRuleAction)
+VALUES(109, 'ClientGenDocsToAddtlEmail', 'Case', 'When sending docs to client cc/bcc additional email addresses', 1, 1201, 0, 'AttachOption', 'CCEmailAddress', 'BccEmailAddress', NULL, NULL, 0),
+      (110, 'ClientDistDocsToAddtlEmail', 'Case', 'When distribute docs to client cc/bcc additional email addresses', 1, 1202, 0, 'AttachOption', 'CCEmailAddress', 'BccEmailAddress', NULL, NULL, 0),
+	  (111, 'ClientDistRptToAddtlEmail', 'Case', 'When distribute rpts to client cc/bcc additional email addresses', 1, 1320, 0, 'AttachOption', 'CCEmailAddress', 'BccEmailAddress', NULL, NULL, 0)
+GO
+
+
--- IMECentricDB/trunk/Data/OneTime.sql
+++ IMECentricDB/trunk/Data/OneTime.sql
@@ -1,6 +1,27 @@
 ﻿INSERT INTO tblUserFunction VALUES ('AckNewPortalAcct', 'Acknowledge - New Portal Accts Auto Provision', '2019-12-12')
+GO
 
 --// IMEC-11382 - adding record into the tblMessageToken for the examinee last name, first name, and middle inital
 insert into tblMessageToken (Name, Description)
 values ('@ExamineeLastName@',''), ('@ExamineeFirstName@',''), ('@ExamineeMiddleInitial@','')
-;
+GO
+
+-- Issue 11433
+-- Parent Company = Zurich = 60
+-- BusinessLine = Workers Comp = 3
+-- ServiceType = IME = 1
+-- ServieType = Record Reviews = 3
+--
+-- attachment option parameter
+--	0 = no attachment present
+--	1 = attachment present
+--	2 = for all email; not dependent on there being an attachment
+INSERT INTO tblBusinessRuleCondition(EntityType, EntityID, BillingEntity, ProcessOrder, BusinessRuleID, DateAdded, UserIDAdded, DateEdited, UserIDEdited, OfficeCode, EWBusLineID, EWServiceTypeID, Jurisdiction, Param1, Param2, Param3, Param4, Param5)
+VALUES('PC', 60, 2, 1, 109, '2020-01-20', 'Admin', '2020-01-20', 'Admin', NULL, '3', '1', NULL, '1', 'usz.zurich.claims.documents@zurichna.com', NULL, NULL, NULL),
+	 ('PC', 60, 2, 1, 109, '2020-01-20', 'Admin', '2020-01-20', 'Admin', NULL, '3', '3', NULL, '1', 'usz.zurich.claims.documents@zurichna.com', NULL, NULL, NULL),
+	 ('PC', 60, 2, 1, 110, '2020-01-20', 'Admin', '2020-01-20', 'Admin', NULL, '3', '1', NULL, '1', 'usz.zurich.claims.documents@zurichna.com', NULL, NULL, NULL),
+	 ('PC', 60, 2, 1, 110, '2020-01-20', 'Admin', '2020-01-20', 'Admin', NULL, '3', '3', NULL, '1', 'usz.zurich.claims.documents@zurichna.com', NULL, NULL, NULL),
+	 ('PC', 60, 2, 1, 111, '2020-01-20', 'Admin', '2020-01-20', 'Admin', NULL, '3', '1', NULL, '1', 'usz.zurich.claims.documents@zurichna.com', NULL, NULL, NULL),
+	 ('PC', 60, 2, 1, 111, '2020-01-20', 'Admin', '2020-01-20', 'Admin', NULL, '3', '3', NULL, '1', 'usz.zurich.claims.documents@zurichna.com', NULL, NULL, NULL)
+GO
+
