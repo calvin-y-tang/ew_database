17c44c0a57f05b83daf8e9a2deeb6e5dc3ba630e
Calvin Tang <calvin.tang@examworks.com>
2020-01-24 09:54:21 -0500

    Prepare project for S45 Build Script

--- IMECentricDB/trunk/Data/NextRelease.sql
+++ IMECentricDB/trunk/Data/NextRelease.sql
@@ -1,17 +1,4 @@
---Issue 11389 - Changes to ext priority code for mi-Support
-
-DELETE FROM tblDPSPriority
-
-SET IDENTITY_INSERT [dbo].[tblDPSPriority] ON
-INSERT INTO [dbo].[tblDPSPriority] ([DPSPriorityID], [Name], [ExtPriorityCode], [DueDateMethod], [DueDateHours], [CancelPriority]) VALUES (1, 'Standard (24hr)', 'Medium', 2, 24, NULL)
-INSERT INTO [dbo].[tblDPSPriority] ([DPSPriorityID], [Name], [ExtPriorityCode], [DueDateMethod], [DueDateHours], [CancelPriority]) VALUES (2, 'Rush (4hr)', 'Rush', 1, 4, 1)
-INSERT INTO [dbo].[tblDPSPriority] ([DPSPriorityID], [Name], [ExtPriorityCode], [DueDateMethod], [DueDateHours], [CancelPriority]) VALUES (3, 'Complex (48hr)', 'Low', 2, 48, NULL)
-INSERT INTO [dbo].[tblDPSPriority] ([DPSPriorityID], [Name], [ExtPriorityCode], [DueDateMethod], [DueDateHours], [CancelPriority]) VALUES (4, 'High (12hr)', 'High', 2, 12, NULL)
-SET IDENTITY_INSERT [dbo].[tblDPSPriority] OFF
-
-GO
-
--- Issue 11433
+delete from tblBusinessRule WHERE BusinessRuleID in (109,110,111)
 INSERT INTO tblBusinessRule (BusinessRuleID, Name, Category, Descrip, IsActive, EventID, AllowOverride, Param1Desc, Param2Desc, Param3Desc, Param4Desc, Param5Desc, BrokenRuleAction)
 VALUES(109, 'ClientGenDocsToAddtlEmail', 'Case', 'When sending docs to client cc/bcc additional email addresses', 1, 1201, 0, 'AttachOption', 'CCEmailAddress', 'BccEmailAddress', NULL, NULL, 0),
       (110, 'ClientDistDocsToAddtlEmail', 'Case', 'When distribute docs to client cc/bcc additional email addresses', 1, 1202, 0, 'AttachOption', 'CCEmailAddress', 'BccEmailAddress', NULL, NULL, 0),
@@ -19,3 +6,9 @@ VALUES(109, 'ClientGenDocsToAddtlEmail', 'Case', 'When sending docs to client cc
 GO
 
 
+INSERT INTO tblUserFunction VALUES ('AckNewPortalAcct', 'Acknowledge - New Portal Accts Auto Provision', '2019-12-12')
+GO
+
+insert into tblMessageToken (Name, Description)
+values ('@ExamineeLastName@',''), ('@ExamineeFirstName@',''), ('@ExamineeMiddleInitial@','')
+GO
