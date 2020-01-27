0bbec45731913daa9178ef64065b160554c2210c
Calvin Tang <calvin.tang@examworks.com>
2020-01-27 08:50:05 -0500

    Prepare for DB Build Script

--- IMECentricDB/branches/CurrentRelease/Data/OneTime.sql
+++ IMECentricDB/branches/CurrentRelease/Data/OneTime.sql
@@ -1,6 +1,1 @@
 ﻿
--- new business rule condition to match employer (Waste Management) to claim number.
-INSERT INTO tblBusinessRuleCondition(EntityType, EntityID, BillingEntity, ProcessOrder, BusinessRuleID, DateAdded, UserIDAdded, DateEdited, UserIDEdited, OfficeCode, EWBusLineID, EWServiceTypeID, Jurisdiction, Param1, Param2, Param3, Param4, Param5)
-VALUES('CO', 77443, 2, 1, 108, '2020-01-08', 'Admin', '2020-01-08', 'Admin', NULL, NULL, NULL, NULL, '003000', NULL, '142', NULL, 'ClaimEmployerOverride')
-
-
