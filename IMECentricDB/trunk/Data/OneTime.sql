931d3b991b67ac65fc9a67af1cb7ac3ee55896f1
Sam.Chiang <Sam.Chiang@examworks.com>
2020-01-30 16:21:35 +0000

    Issue 11281 - Adding in to the one time script file; to add in the 2 new tokens: @ThirdPartyBillingCompany@ and @ThirdPartyBillingClient@.

--- IMECentricDB/trunk/Data/OneTime.sql
+++ IMECentricDB/trunk/Data/OneTime.sql
@@ -1,1 +1,6 @@
 ﻿
+--// 11281 - adding new records into the tblMessageToken table
+insert into tblMessageToken (Name, Description)
+values ('@ThirdPartyBillingCompany@',''), ('@ThirdPartyBillingClient@','')
+;
+
