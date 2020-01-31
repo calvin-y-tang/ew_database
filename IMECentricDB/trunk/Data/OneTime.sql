869e9d6bb1704480e0f5d8ca9b5984a5e653f3f1
Terri Lyde <terri.lyde@examworks.com>
2020-01-31 11:29:45 -0500

    Issue 11454 - add company email to GP feed

--- IMECentricDB/trunk/Data/OneTime.sql
+++ IMECentricDB/trunk/Data/OneTime.sql
@@ -4,3 +4,8 @@ insert into tblMessageToken (Name, Description)
 values ('@ThirdPartyBillingCompany@',''), ('@ThirdPartyBillingClient@','')
 ;
 
+
+-- Issue 11454 - add company email to GP feed
+  ALTER TABLE IMECentricMaster.dbo.GPCompany ADD AcctingEmail varchar(70) NULL
+  ALTER TABLE EWDataRepository.dbo.Company ADD AcctingEmail varchar(70) NULL
+
