46d6c07fee52a8d7ea670a0114a612980eed57f3
Terri Lyde <terri.lyde@examworks.com>
2020-02-11 09:25:12 -0500

    Issue 11439 - changed order of list so that the entries are in alphabetical order with OTHER at bottom

--- IMECentricDB/trunk/Data/NextRelease.sql
+++ IMECentricDB/trunk/Data/NextRelease.sql
@@ -5,9 +5,10 @@ GO
 
 -- Issue 11439 - add rows to tblCodes to add in office contact department types and values to be used in cboDepartment combo box for office Contacts
 INSERT INTO tblCodes (Category, SubCategory, Value) VALUES ('OfficeContactDeptCombo', 'Accounting', '1')
+INSERT INTO tblCodes (Category, SubCategory, Value) VALUES ('OfficeContactDeptCombo', 'Bill Review', '7')
 INSERT INTO tblCodes (Category, SubCategory, Value) VALUES ('OfficeContactDeptCombo', 'Customer Service', '2')
 INSERT INTO tblCodes (Category, SubCategory, Value) VALUES ('OfficeContactDeptCombo', 'Medical Records', '3')
 INSERT INTO tblCodes (Category, SubCategory, Value) VALUES ('OfficeContactDeptCombo', 'QA', '4')
 INSERT INTO tblCodes (Category, SubCategory, Value) VALUES ('OfficeContactDeptCombo', 'Scheduling', '5')
 INSERT INTO tblCodes (Category, SubCategory, Value) VALUES ('OfficeContactDeptCombo', 'Other', '6')
-INSERT INTO tblCodes (Category, SubCategory, Value) VALUES ('OfficeContactDeptCombo', 'Bill Review', '7')
+
