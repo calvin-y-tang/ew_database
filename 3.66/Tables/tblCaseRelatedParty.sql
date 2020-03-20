75b809c20ca67a34d990d0bf9d1bbc350bab99ef
David Rose <david.rose@examworks.com>
2020-03-20 06:03:17 -0400

    DB Release 3.66

---
+++ 3.66/Tables/tblCaseRelatedParty.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[tblCaseRelatedParty] (
+    [CaseRPID]    INT IDENTITY (1, 1) NOT NULL,
+    [CaseNbr]     INT NULL,
+    [RPCode]      INT NULL,
+    [CompanyCode] INT NULL,
+    CONSTRAINT [PK_tblCaseRelatedParty] PRIMARY KEY CLUSTERED ([CaseRPID] ASC)
+);
+
---
+++ 3.66/Tables/tblWebNotifyReason.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[tblWebNotifyReason] (
+    [ReasonCode]  INT           NOT NULL,
+    [Description] VARCHAR (100) NULL,
+    CONSTRAINT [PK_tblWebNotifyReason] PRIMARY KEY CLUSTERED ([ReasonCode] ASC)
+);
+
