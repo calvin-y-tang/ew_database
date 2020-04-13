4903b112fee5c1ad911ca52bb73d96349f7595e8
David Rose <david.rose@examworks.com>
2020-04-13 14:15:19 -0400

    DB Release 3.67

---
+++ 3.67/Tables/tblDoctorSearchWeightedCriteria.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[tblDoctorSearchWeightedCriteria] (
+    [PrimaryKey]                   INT            IDENTITY (1, 1) NOT NULL,
+    [BlockTime]                    DECIMAL (8, 2) NOT NULL,
+    [AverageMargin]                DECIMAL (8, 2) NOT NULL,
+    [CaseCount]                    DECIMAL (8, 2) NOT NULL,
+    [SchedulePriority]             DECIMAL (8, 2) NOT NULL,
+    [ReceiveMedRecsElectronically] DECIMAL (8, 2) NOT NULL,
+    CONSTRAINT [PK_tblDoctorSearchWeightedCriteria] PRIMARY KEY CLUSTERED ([PrimaryKey] ASC)
+);
+
