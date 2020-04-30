496d1e4a8de2234a3039556212eebb69fa0c58a2
David Rose <david.rose@examworks.com>
2020-04-30 10:25:29 -0400

    DB Release 3.67

---
+++ 3.67/CurrentRelease/Views/vwCasePanel.sql
@@ -0,0 +1,21 @@
+﻿
+CREATE VIEW vwCasePanel
+AS
+    SELECT  tblSpecialty.description AS SpecialtyDesc ,
+            tblDoctor.lastName + ', ' + ISNULL(tblDoctor.firstName, '') AS DoctorName ,
+            tblDoctor.firstName ,
+            tblDoctor.Credentials ,
+            tblCasePanel.Panelnbr ,
+            tblCasePanel.DoctorCode ,
+            tblCasePanel.Panelnote ,
+            tblCasePanel.SpecialtyCode ,
+            tblCasePanel.SchedCode ,
+            tblCasePanel.DateAdded ,
+            tblCasePanel.UserIDAdded ,
+            tblCasePanel.DateEdited ,
+            tblCasePanel.UserIDEdited ,
+			tblCasePanel.DoctorReason
+    FROM    tblCasePanel
+            INNER JOIN tblDoctor ON tblCasePanel.DoctorCode = tblDoctor.DoctorCode
+            LEFT OUTER JOIN tblSpecialty ON tblCasePanel.SpecialtyCode = tblSpecialty.SpecialtyCode
+
---
+++ 3.67/CurrentRelease/Views/vwRptDoctorScheduleSlot.sql
@@ -0,0 +1,56 @@
+﻿CREATE VIEW vwRptDoctorScheduleSlot
+AS
+     SELECT CA.SchedCode AS RecID,
+            CA.DoctorCode,            
+			CA.LocationCode,
+            CAST(CAST(CA.Date AS DATE) AS DATETIME) AS Date,
+            CA.StartTime AS StartTime, 
+
+            NULL AS CaseNbr , 
+			NULL AS ExtCaseNbr , 
+            '' AS SpecialInstructions ,
+            NULL AS PhotoRqd ,
+            NULL AS PanelNbr ,
+            '' AS PanelDesc,
+            NULL AS CaseOfficeCode,
+
+            '' AS Interpreter,
+
+            CA.Status AS ScheduleDesc1,
+
+			CA.Description AS ScheduleDesc2,
+
+            '' AS Company ,
+
+            '' AS ClientName ,
+            '' AS ClientPhone ,
+
+			LO.OfficeCode as LocationOfficeCode,
+            L.Location,
+			L.Addr1,
+            L.Addr2,
+            L.City,
+            L.State,
+            L.Zip,
+            L.Phone AS LocationPhone ,
+            L.Fax AS LocationFax ,
+
+            EWF.LegalName AS CompanyName ,
+
+            ISNULL(DR.FirstName, '') + ' ' + ISNULL(DR.LastName, '') + ', ' + ISNULL(DR.Credentials, '') AS DoctorName,
+
+			'' AS Problem
+
+    FROM    tblDoctorSchedule AS CA
+
+				INNER JOIN tblDoctor AS DR ON DR.DoctorCode = CA.DoctorCode
+				INNER JOIN tblLocation AS L ON CA.LocationCode = L.LocationCode
+
+				INNER JOIN tblDoctorOffice AS DRO ON DR.DoctorCode = DRO.DoctorCode
+				INNER JOIN tblLocationOffice AS LO ON LO.OfficeCode = DRO.OfficeCode AND LO.LocationCode = L.LocationCode
+
+				INNER JOIN tblOffice AS O ON DRO.OfficeCode = O.OfficeCode
+				INNER JOIN tblEWFacility AS EWF on O.EWFacilityID = EWF.EWFacilityID
+
+
+	WHERE CA.Status IN ('Open', 'Hold', 'Held')
