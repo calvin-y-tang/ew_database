23e209d82309129758733e10734c37118a1f9004
David Rose <david.rose@examworks.com>
2020-05-11 09:45:00 -0400

    DB Release 3.69

---
+++ 3.69/Data/OneTime.sql
@@ -0,0 +1,6 @@
+﻿use IMECentricPOPB
+GO
+DELETE FROM tblPOPBVoucher WHERE CaseDoctorCode IS NULL
+
+UPDATE tblPOPBVoucher SET Percentage=10, DoctorCode2=1, Percentage2=10
+GO
---
+++ 3.69/Tables/tblEmployerAddress.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE [dbo].[tblEmployerAddress]
+(
+	[EmployerAddressID] INT IDENTITY(1, 1) NOT NULL , 
+    [EmployerID]        INT                NOT NULL, 
+    [Address1]          VARCHAR(50)        NULL, 
+    [Address2]          VARCHAR(50)        NULL, 
+    [City]              VARCHAR(50)        NULL, 
+    [State]             VARCHAR(2)         NULL, 
+    [Zip]               VARCHAR(10)        NULL, 
+    [ContactFirst]      VARCHAR(50)        NULL, 
+    [ContactLast]       VARCHAR(50)        NULL, 
+    [Phone]             VARCHAR(15)        NULL, 
+    [PhoneExt]          VARCHAR(10)        NULL, 
+    [Fax]               VARCHAR(15)        NULL, 
+    [Email]             VARCHAR(70)        NULL, 
+    [DateAdded]         DATETIME           NULL, 
+    [UserIDAdded]       VARCHAR(15)        NULL, 
+    [DateEdited]        DATETIME           NULL, 
+    [UserIDEdited]      VARCHAR(15)        NULL
+	CONSTRAINT [PK_tblEmployerAddress] PRIMARY KEY CLUSTERED ([EmployerAddressID] ASC)
+)
