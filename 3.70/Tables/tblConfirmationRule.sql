5340281b94970ed81e89cbe21299e6ec9d88f81f
David Rose <david.rose@examworks.com>
2020-06-01 10:05:32 -0400

    DB Release 3.70

---
+++ 3.70/Tables/tblConfirmationRule.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [dbo].[tblConfirmationRule]
+(
+	[ConfirmationRuleID] INT IDENTITY (1, 1) NOT NULL, 
+    [ProcessOrder] INT NULL, 
+    [OfficeCode] INT NULL, 
+    [CaseType] INT NULL, 
+    [ServiceCode] INT NULL, 
+    [Jurisdiction] VARCHAR(2) NULL, 
+    [EmployerID] INT NULL, 
+    [CompanyCode] INT NULL, 
+    [ClientCode] INT NULL, 
+    [DateAdded] DATETIME NULL, 
+    [UserIDAdded] VARCHAR(15) NULL, 
+    [DateEdited] DATETIME NULL, 
+    [UserIDEdited] VARCHAR(15) NULL,
+	[ParentCompanyID] INT NULL,
+	[ExcludeFromList] BIT CONSTRAINT [DF_tblConfirmationRule_ExcludeFromList] DEFAULT ((0)) NOT NULL,
+	CONSTRAINT [PK_tblConfirmationRule] PRIMARY KEY CLUSTERED ([ConfirmationRuleID] ASC)
+)
---
+++ 3.70/Tables/tblDoctorNetwork.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[tblDoctorNetwork] (
+    [DoctorCode]  INT          NOT NULL,
+    [EWNetworkID] INT          NOT NULL,
+    [UserIDAdded] VARCHAR (15) NULL,
+    [DateAdded]   DATETIME     NULL,
+    CONSTRAINT [PK_tblDoctorNetwork] PRIMARY KEY CLUSTERED ([DoctorCode] ASC, [EWNetworkID] ASC)
+);
+
