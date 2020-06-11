ea8d448fcb62fb571f3a7cb6174341d34acc3382
Calvin Tang <calvin.tang@examworks.com>
2020-06-11 14:44:03 -0400

    Issue 11663 - New column to sort DPS Sort Model combobox

--- IMECentricDB/trunk/Tables/tblDPSSortModel.sql
+++ IMECentricDB/trunk/Tables/tblDPSSortModel.sql
@@ -4,6 +4,7 @@
     [ExtSortModelCode] VARCHAR (30) NULL,
     [Explanation] VARCHAR(100) NULL, 
     [PublishOnWeb] BIT NULL, 
+    [DisplayOrder] INT NULL, 
     CONSTRAINT [PK_tblDPSSortModel] PRIMARY KEY CLUSTERED ([SortModelID] ASC)
 );
 
