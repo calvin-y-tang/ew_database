9587fa008c1a788b30067aabb98a6fb0c4cbf648
David Rose <david.rose@examworks.com>
2020-06-24 07:56:14 -0400

    DB Release 3.71

---
+++ 3.71/Tables/tblUserSecurity.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[tblUserSecurity] (
+    [UserID]    VARCHAR (15) NOT NULL,
+    [GroupCode] VARCHAR (15) NOT NULL,
+    CONSTRAINT [PK_tblUserSecurity] PRIMARY KEY CLUSTERED ([UserID] ASC, [GroupCode] ASC)
+);
+
