da3fa896c363235e69a24b3f06efe2f056e07ef8
Jose Pais <jose.pais@examworks.com>
2020-07-02 12:55:35 -0400

    Issue 11686 - new secruity token for Open Doctor Schedule menu item

--- IMECentricDB/trunk/Data/NextRelease.sql
+++ IMECentricDB/trunk/Data/NextRelease.sql
@@ -72,3 +72,10 @@ INSERT INTO [dbo].[tblCaseApptRequestStatus] ([CaseApptRequestStatusID], [Name])
 INSERT INTO [dbo].[tblCaseApptRequestStatus] ([CaseApptRequestStatusID], [Name]) VALUES (1, 'Accepted')
 INSERT INTO [dbo].[tblCaseApptRequestStatus] ([CaseApptRequestStatusID], [Name]) VALUES (2, 'Rejected')
 GO
+
+-- Issue 11686 - new security token for show/hide Open Doctor Schedule Menu Item
+INSERT INTO tblUserFunction (FunctionCode, FunctionDesc, DateAdded)
+VALUES ('MenuOpenDrSched', 'Menu - Open Doctor Schedule', '2020-07-02')
+GO
+
+
