c6bd5e91b3884d99d98070b3fb9d2083a6171756
Calvin Tang <calvin.tang@examworks.com>
2020-07-15 09:16:25 -0400

    Prepare DB Project for Build Script

--- IMECentricDB/branches/CurrentRelease/Data/NextRelease.sql
+++ IMECentricDB/branches/CurrentRelease/Data/NextRelease.sql
@@ -1,40 +1,7 @@
--- Issue 11654 - add new token for claim number extension only
-INSERT INTO [dbo].[tblMessageToken] ([Name], [Description]) VALUES ('@ClaimNbrExt@ ', '')
+
+-- Issue 11686 - new security token for show/hide Open Doctor Schedule Menu Item
+INSERT INTO tblUserFunction (FunctionCode, FunctionDesc, DateAdded)
+VALUES ('MenuOpenDrSched', 'Menu - Open Doctor Schedule', '2020-07-02')
 GO
 
 
-UPDATE U SET U.DisplayOrder=SM.RowNbr
-FROM tblDPSSortModel AS U
-INNER JOIN
-(SELECT SortModelID, Description, ROW_NUMBER() OVER (ORDER BY Description) AS RowNbr
-FROM tblDPSSortModel) AS SM ON U.SortModelID = SM.SortModelID
-GO
-
--- Issue 11137 - data patch fot acctquotefee to link in prodcode
-     UPDATE AQF
-        SET AQF.QuoteFeeConfigID = QFC.QuoteFeeConfigID
-     FROM tblAcctQuoteFee AS AQF
-               INNER JOIN tblQuoteFeeConfig as QFC ON QFC.FeeValueName = AQF.FeeValueName
-GO 
-
--- Issue 11137 - patch tblAcctDetail to set existing row to FeeCodeSource where FeeCode is present
-UPDATE tblAcctDetail SET FeeCodeSource = 1 WHERE FeeCode IS NOT NULL AND FeeCodeSource IS NULL
-GO 
-
--- Issue 11137 - patch tblAcctHeader to set existing rows to FeeCodeSource where FeeCode is present
-UPDATE tblAcctHeader SET FeeCodeSource = 1 WHERE FeeCode IS NOT NULL AND FeeCodeSource IS NULL
-GO 
-
--- Issue 11137 - patch tblAcctQuoteFee to set value for QuoteFeeConfigID
-UPDATE AQ
-   SET AQ.QuoteFeeConfigID = QF.QuoteFeeConfigID
-FROM tblAcctQuoteFee AS AQ
-          INNER JOIN tblQuoteFeeConfig AS QF ON QF.FeeValueName = AQ.FeeValueName
-WHERE AQ.QuoteFeeConfigID IS NULL
-GO
-
-DELETE FROM [tblCaseApptRequestStatus]
-INSERT INTO [dbo].[tblCaseApptRequestStatus] ([CaseApptRequestStatusID], [Name]) VALUES (0, 'Pending Acceptance')
-INSERT INTO [dbo].[tblCaseApptRequestStatus] ([CaseApptRequestStatusID], [Name]) VALUES (1, 'Accepted')
-INSERT INTO [dbo].[tblCaseApptRequestStatus] ([CaseApptRequestStatusID], [Name]) VALUES (2, 'Rejected')
-GO
