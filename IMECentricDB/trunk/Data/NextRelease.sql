335347944ec32b5df56446f3e79d0f76027fa27a
Jose Pais <jose.pais@examworks.com>
2020-07-20 13:13:59 -0400

    Issue 11702 - populate new DPSImportConfig table with configuration details

--- IMECentricDB/trunk/Data/NextRelease.sql
+++ IMECentricDB/trunk/Data/NextRelease.sql
@@ -2,5 +2,20 @@
 -- Issue 11718 - Add business rule for Hartford quotes - BCC email
 INSERT INTO tblBusinessRuleCondition(EntityType, EntityID, BillingEntity, ProcessOrder, BusinessRuleID, DateAdded, UserIDAdded, DateEdited, UserIDEdited, OfficeCode, EWBusLineID, EWServiceTypeID, Jurisdiction, Param1, Param2, Param3, Param4, Param5)
 VALUES('PC', 30, 2, 1, 109, GetDate(), 'Admin', NULL, NULL, NULL, NULL, NULL, NULL, 0, NULL, 'HartfordQuotes@ExamWorks.com', NULL, NULL)
+GO
 
+-- Issue 11702 - populate new DPSImportConfig table with configuration details
+INSERT INTO tblDPSResultFileConfig (EWServiceTypeID, FileIndex, Description, CombinedFileName, CombineFileSeqNo)
+VALUES(-1, 1, 'Sorted Document 1', 'MedIndex - Final', 1),
+      (-1, 2, 'Exclusion Document', 'MedIndex - Final', 99),
+      (-1, 3, 'Sorted Document 2', 'MedIndex - Final', 2),
+      (-1, 4, 'Sorted Document 3', 'MedIndex - Final', 3),
+      (-1, 5, 'Sorted Document 4', 'MedIndex - Final', 4),
+      (-1, 6, 'Sorted Document 5', 'MedIndex - Final', 5),
+      (-1, 7, 'Sorted Document 6', 'MedIndex - Final', 6),
+      (-1, 8, 'Sorted Document 7', 'MedIndex - Final', 7),
+      (-1, 9, 'Sorted Document 8', 'MedIndex - Final', 8),
+      (-1, 10, 'Sorted Document 9', 'MedIndex - Final', 9), 
+	  (11, 1, 'MedIndex - Final', '', 0)
+GO
 
