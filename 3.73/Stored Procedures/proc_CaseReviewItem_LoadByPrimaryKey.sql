968c150dc645316272fe515bc1450bfb0d6cdb98
David Rose <david.rose@examworks.com>
2020-08-05 08:18:49 -0400

    DB Release 3.73

---
+++ 3.73/Stored Procedures/proc_CaseReviewItem_LoadByPrimaryKey.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE [proc_CaseReviewItem_LoadByPrimaryKey]
+(
+	@CaseReviewItemID int
+)
+AS
+BEGIN
+	SET NOCOUNT ON
+	DECLARE @Err int
+
+	SELECT *
+
+	FROM [tblCaseReviewItem]
+	WHERE
+		([CaseReviewItemID] = @CaseReviewItemID)
+
+	SET @Err = @@Error
+
+	RETURN @Err
+END
