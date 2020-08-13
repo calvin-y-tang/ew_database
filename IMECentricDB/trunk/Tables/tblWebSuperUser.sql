6a20798346fd1a0ac9aa0444587fb3423928600a
gary.ritzenthaler <gary.ritzenthaler@examworks.com>
2020-08-13 20:16:17 +0000

    IMECWM-218 - Admin Portal Conversion to Angular - API Development - added tblWebSuperUser

---
+++ IMECentricDB/trunk/Tables/tblWebSuperUser.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[tblWebSuperUser](
+	[SuperUserID] [int] IDENTITY(1,1) NOT NULL,
+	[WebUserID] [int] NULL,
+	[EWBusLineID] [int] NULL,
+	[ParentCompanyID] [int] NULL,
+	[CompanyCode] [int] NULL,
+	[DateAdded] [smalldatetime] NULL,
+	[UserIDAdded] [varchar](255) NULL,
+	[DateEdited] [smalldatetime] NULL,
+	[UserIDEdited] [varchar](255) NULL,
+ CONSTRAINT [PK_tblWebSuperUser] PRIMARY KEY CLUSTERED 
+(
+	[SuperUserID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
