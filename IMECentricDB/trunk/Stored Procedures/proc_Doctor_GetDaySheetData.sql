2c620177ba56ee84da5b47387c615e5d0069a6b0
Terri Lyde <terri.lyde@examworks.com>
2020-08-31 11:22:49 -0400

    Issue 11758 - changed to use tblCaseAppt instead of tblDoctorSchedule

--- IMECentricDB/trunk/Stored Procedures/proc_Doctor_GetDaySheetData.sql
+++ IMECentricDB/trunk/Stored Procedures/proc_Doctor_GetDaySheetData.sql
@@ -12,7 +12,9 @@ CREATE proc [dbo].[proc_Doctor_GetDaySheetData]
 as
 begin
 	
-
+	----------------------------------------------------------------------------------------
+	--Paramter preparation
+	----------------------------------------------------------------------------------------
 	if @alloffices is null or @alloffices < 0
 	begin
 		set @alloffices = -1;
@@ -30,60 +32,29 @@ begin
 
 	if object_id('tempdb..#daysheetdata') is not null drop table #daysheetdata;   
 
-	if object_id('tempdb..#daysheetdatanew') is not null drop table #daysheetdatanew;   
 
-
-	select 
-		distinct LocationCode, LocationOffice, DoctorOffice 
-		into #daysheetdatanew 
-	from (
-		select 
-			ca.CaseApptID as SchedCode,
-			ca.DoctorCode ,            
-			ca.LocationCode ,
-			c.CaseNbr, 
-			c.ExtCaseNbr, 
-			c.OfficeCode,
-			lo.OfficeCode as LocationOffice,
-			dro.OfficeCode as DoctorOffice
-
-		from tblcaseappt as ca with (nolock)
-			inner join tblcase as c  with (nolock)on ca.caseapptid = c.caseapptid			
-			left join tblcaseapptpanel as cap  with (nolock)on cap.caseapptid = c.caseapptid
-			inner join tbldoctor as dr  with (nolock)on dr.doctorcode = isnull(ca.doctorcode, cap.doctorcode)
-			inner join tbllocation as l  with (nolock)on ca.locationcode = l.locationcode
-			inner join tbldoctoroffice as dro  with (nolock)on dr.doctorcode = dro.doctorcode
-			inner join tbllocationoffice as lo  with (nolock)on lo.officecode = dro.officecode and lo.locationcode = l.locationcode			
-			inner join tblapptstatus as aps  with (nolock)on aps.apptstatusid = ca.apptstatusid and ca.apptstatusid = 10
-
-		where
-			(cast(ca.ApptTime as date) >= @fromDate and cast(ca.ApptTime as date) <= @toDate)	
-			and aps.Name = 'Scheduled'
-			and (c.CaseType = (COALESCE(NULLIF(@casetype, '-1'), c.CaseType)) or c.CaseType is null)
-			and ca.DoctorCode = (COALESCE(NULLIF(@doctor, '-1'), ca.DoctorCode))
-			and ca.LocationCode = (COALESCE(NULLIF(@location, '-1'), ca.LocationCode))
-	) x
-	;
-
+	----------------------------------------------------------------------------------------
+	--Primary dataset using tblDoctorSchedule the old way into temp table
+	----------------------------------------------------------------------------------------
 	select 
 		x.*, 
 		isnull(cpc.OfficeCode, c.OfficeCode) as OfficeCode, 
 		isnull(cpc.CaseNbr, c.CaseNbr) as CaseNbr, 
 		isnull(cpc.ExtCaseNbr, c.ExtCaseNbr) as ExtCaseNbr
-		
-		into #daysheetdata
+
+		into #daysheetdata		
 	from (
 
 		select 
 
-			tblDoctorSchedule.SchedCode,		
-			tblDoctorSchedule.DoctorCode,
-			tblDoctorSchedule.LocationCode,		
+			tblCaseAppt.CaseApptID,		
+			tblCaseAppt.DoctorCode,
+			tblCaseAppt.LocationCode,		
 			tblLocation.Location,
 			(rtrim(tblLocation.Addr1 + ' ' + isnull(tblLocation.Addr2, '')) +  ', ' + tblLocation.City + ' ' + tblLocation.State + ' ' + tblLocation.Zip) as DoctorAddress,				
-			tblDoctorSchedule.date as ApptDate,
-			tblDoctorSchedule.StartTime as ApptDateTime,
-			stuff(replace(right(convert(varchar(19), tblDoctorSchedule.StartTime, 0), 7), ' ', '0'), 6, 0, ' ') as ApptTime,		
+			CAST(CAST(tblCaseAppt.ApptTime AS DATE) AS DATETIME) as ApptDate,
+			tblCaseAppt.ApptTime as ApptDateTime,
+			stuff(replace(right(convert(varchar(19), tblCaseAppt.ApptTime, 0), 7), ' ', '0'), 6, 0, ' ') as ApptTime,		
 			ISNULL(tblDoctor.FirstName, '') + ' ' + ISNULL(tblDoctor.LastName, '') + ', ' + ISNULL(tblDoctor.Credentials, '') as DoctorName,		
 			(case 
 				when ltrim(rtrim(isnull(tblDoctor.DaysheetEmailAddr, ''))) <> '' then ltrim(rtrim(tblDoctor.DaysheetEmailAddr)) 
@@ -99,40 +70,38 @@ begin
 			tblLocation.ExtName as LocationExtName,
 			tblLocationOffice.OfficeCode as LocationOffice, 
 			tblDoctorOffice.OfficeCode as DoctorOffice,
-			tblDoctorSchedule.CaseNbr1,
-			tblDoctorSchedule.CaseNbr2,
-			tblDoctorSchedule.CaseNbr3,
-			tblDoctorSchedule.CaseNbr4,
-			tblDoctorSchedule.CaseNbr5,
-			tblDoctorSchedule.CaseNbr6
+			tblCaseAppt.CaseNbr as caCaseNbr
 
-		FROM tblDoctorSchedule with (nolock)
-			inner join tblDoctor with (nolock) on tblDoctorSchedule.DoctorCode = tblDoctor.DoctorCode	
-			inner join tblLocation with (nolock) on tblDoctorSchedule.LocationCode = tblLocation.LocationCode 
+		FROM tblCaseAppt with (nolock)
+			inner join tblDoctor with (nolock) on tblCaseAppt.DoctorCode = tblDoctor.DoctorCode	
+			inner join tblLocation with (nolock) on tblCaseAppt.LocationCode = tblLocation.LocationCode 
 			inner join tblDoctorOffice with (nolock) on tblDoctor.DoctorCode = tblDoctorOffice.DoctorCode
 			inner join tblLocationOffice with (nolock) on tblLocationOffice.OfficeCode = tblDoctorOffice.OfficeCode AND tblLocationOffice.LocationCode = tblLocation.LocationCode	
 
 		WHERE 					
-			(tblDoctorSchedule.date >= @fromDate and tblDoctorSchedule.date <= @toDate)			
-			and tblDoctorSchedule.Status = 'Scheduled'		
-			and tblDoctorSchedule.DoctorCode = (COALESCE(NULLIF(@doctor, '-1'), tblDoctorSchedule.DoctorCode))
-			and tblDoctorSchedule.LocationCode = (COALESCE(NULLIF(@location, '-1'), tblDoctorSchedule.LocationCode))
-
+			(tblCaseAppt.ApptTime >= @fromDate and tblCaseAppt.ApptTime <= @toDate)			
+			and tblCaseAppt.ApptStatusID = 10	
+			and tblCaseAppt.DoctorCode = (COALESCE(NULLIF(@doctor, '-1'), tblCaseAppt.DoctorCode))
+			and tblCaseAppt.LocationCode = (COALESCE(NULLIF(@location, '-1'), tblCaseAppt.LocationCode))
+			
 	) x
-		left outer join tblCasePanel cp with (nolock) 
-			join tblCase cpc with (nolock) on cp.PanelNbr = cpc.PanelNbr
-			on cp.SchedCode = x.SchedCode and cpc.CaseNbr in (x.CaseNbr1, x.CaseNbr2, x.CaseNbr3, x.CaseNbr4, x.CaseNbr5, x.CaseNbr6)
+		--left outer join tblCasePanel cp with (nolock) 
+		left outer join tblCaseApptPanel cp with (nolock)
+			join tblCase cpc with (nolock) on cp.CaseApptID = cpc.CaseApptID
+			on cp.CaseApptID = x.CaseApptID and cpc.CaseNbr = x.caCaseNbr
 
-		left outer join tblCase c with (nolock) on c.SchedCode = x.SchedCode and c.CaseNbr in (x.CaseNbr1, x.CaseNbr2, x.CaseNbr3, x.CaseNbr4, x.CaseNbr5, x.CaseNbr6)
-
+		left outer join tblCase c with (nolock) on c.CaseApptID = x.CaseApptID and c.CaseNbr = x.caCaseNbr
 	;
 
+	----------------------------------------------------------------------------------------
+	--Filter the temp table from above base on Office selection in CTE
+	----------------------------------------------------------------------------------------
 	with
 	ddata as (
 
 		select 
 			distinct 
-			SchedCode, OfficeCode, DoctorCode, LocationCode, Location, DoctorAddress, CaseNbr, ExtCaseNbr, ApptDate, ApptDateTime, ApptTime,  
+			CaseApptID, OfficeCode, DoctorCode, LocationCode, Location, DoctorAddress, CaseNbr, ExtCaseNbr, ApptDate, ApptDateTime, ApptTime,  
 			DoctorName, DoctorEmail, LocationPhone, LocationFax, ContactLast, ContactFirst, LocationExtName
 		from #daysheetdata
 		where 
@@ -165,6 +134,9 @@ begin
 
 	)
 
+	----------------------------------------------------------------------------------------
+	--Execute the actual query by joining the CTE (from temp table) to other tables for all the column outputs
+	----------------------------------------------------------------------------------------
 	select 
 		d.*,
 		tblOffice.ShortDesc as OfficeName,
@@ -192,10 +164,10 @@ begin
 			left outer join tblLanguage with (nolock) on tblLanguage.LanguageID = tblcase.LanguageID
 	;
 
+	----------------------------------------------------------------------------------------
+	--Cleanup
+	----------------------------------------------------------------------------------------
 	if object_id('tempdb..#daysheetdata') is not null drop table #daysheetdata;   
 
-	if object_id('tempdb..#daysheetdatanew') is not null drop table #daysheetdatanew; 
-
-
 end
-go
+GO
