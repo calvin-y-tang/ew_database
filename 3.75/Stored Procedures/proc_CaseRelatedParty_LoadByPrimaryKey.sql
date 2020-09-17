02af381a97f6dfa56a19ce0a24b0c98372729b10
David Rose <david.rose@examworks.com>
2020-09-17 12:07:20 -0400

    DB Release 3.75

---
+++ 3.75/Stored Procedures/proc_CaseRelatedParty_LoadByPrimaryKey.sql
@@ -0,0 +1,23 @@
+﻿
+CREATE PROCEDURE [proc_CaseRelatedParty_LoadByPrimaryKey]
+(
+ @CaseNbr int,
+ @RPCode int
+)
+AS
+BEGIN
+ SET NOCOUNT ON
+ DECLARE @Err int
+
+ SELECT *
+
+ FROM [tblCaseRelatedParty]
+ WHERE
+  ([CaseNbr] = @CaseNbr)
+ AND
+  ([RPCode] = @RPCode)
+
+ SET @Err = @@Error
+
+ RETURN @Err
+END
---
+++ 3.75/Stored Procedures/proc_GetSpecialtyByCaseNbr.sql
@@ -0,0 +1,18 @@
+﻿
+CREATE PROCEDURE [proc_GetSpecialtyByCaseNbr]
+(
+	@casenbr int
+)
+AS
+BEGIN
+	SET NOCOUNT ON
+	DECLARE @Err int
+
+	SELECT * FROM tblCaseSpecialty 
+		INNER JOIN tblSpecialty ON tblCaseSpecialty.specialtyCode = tblSpecialty.specialtyCode 
+		WHERE caseNbr = @casenbr
+
+	SET @Err = @@Error
+
+	RETURN @Err
+END
