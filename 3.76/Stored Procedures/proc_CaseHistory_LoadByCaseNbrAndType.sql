7bc20f2738d473ea52af08ebdca058512a18bd79
David Rose <david.rose@examworks.com>
2020-10-13 05:39:39 -0400

    DB Release 3.76

---
+++ 3.76/Stored Procedures/proc_CaseHistory_LoadByCaseNbrAndType.sql
@@ -0,0 +1,26 @@
+﻿
+
+CREATE PROCEDURE [proc_CaseHistory_LoadByCaseNbrAndType]
+(
+ @casenbr int,
+ @histType varchar(20)
+)
+AS
+BEGIN
+ SET NOCOUNT ON
+ DECLARE @Err int
+
+ SELECT *
+
+ FROM [tblCaseHistory]
+ WHERE
+  ([casenbr] = @casenbr)
+ AND
+  ([type] = @histType)
+
+ SET @Err = @@Error
+
+ RETURN @Err
+END
+
+
