4f1f064f481432b2252018fba857b5972e2845e8
Jose Pais <jose.pais@examworks.com>
2020-10-21 15:06:06 -0400

    Issue 11857 - modify schema for EWParentCompany

--- IMECentricDB/trunk/Data/OneTime.sql
+++ IMECentricDB/trunk/Data/OneTime.sql
@@ -1,17 +1,10 @@
-﻿-- Issue 11799 - new document tokens and Security tokens
-INSERT INTO tblMessageToken(Name, Description)
-VALUES ('@CurrentDate@', ''),
-       ('@CurrentDateTime@', '')
-GO
-INSERT INTO tblUserFunction (FunctionCode, FunctionDesc, DateAdded)
-VALUES ('DocEditElecTransfer', 'Documents - Modify Electronic Transfer Settings', '2020-09-30')
-GO
-
-
--- Issue 11806 – increased the size of the client email fields in 2 DBs
+﻿-- Issue 11806 – increased the size of the client email fields in 2 DBs
 
 ALTER TABLE EWDataRepository.dbo.Client ALTER COLUMN Email VARCHAR (150) NULL
 
 ALTER TABLE IMECentricMaster.dbo.GPClient ALTER COLUMN Email VARCHAR (150) NULL
 
+-- Issue 11857 - modify schema for EWParentCompany
+ALTER TABLE IMECentricMaster.dbo.EWParentCompany DROP CONSTRAINT DF_EWParentCompany_RecordRetrievalMethod
+ALTER TABLE IMECentricMaster.dbo.EWParentCompany  ALTER COLUMN RecordRetrievalMethod INT NULL
 
