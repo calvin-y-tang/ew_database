fb69339e023527e5c817aec8dc4cf83887fa2472
Sam.Chiang <Sam.Chiang@examworks.com>
2020-10-22 17:13:01 +0000

    Issue 11828 - Added insert scripted for new token "Jurisdiction"

--- IMECentricDB/trunk/Data/OneTime.sql
+++ IMECentricDB/trunk/Data/OneTime.sql
@@ -8,3 +8,7 @@ ALTER TABLE IMECentricMaster.dbo.GPClient ALTER COLUMN Email VARCHAR (150) NULL
 ALTER TABLE IMECentricMaster.dbo.EWParentCompany DROP CONSTRAINT DF_EWParentCompany_RecordRetrievalMethod
 ALTER TABLE IMECentricMaster.dbo.EWParentCompany  ALTER COLUMN RecordRetrievalMethod INT NULL
 
+
+-- // add new token for Jurisdiction
+-- // Issue 11828 - Modified Replace_Tokens method to support new @Jurisdiction@ token.
+INSERT INTO tblMessageToken (Name) VALUES('@Jurisdiction@');
