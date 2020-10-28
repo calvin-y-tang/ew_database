b22008fc5867e48d84977b35a196f0374218132a
Sam.Chiang <Sam.Chiang@examworks.com>
2020-10-28 19:01:47 +0000

    Issue 11822 - Added in patching scripts for the fee unit on tables tblAcctQuote and tblAcctQuoteFee

--- IMECentricDB/trunk/Data/NextRelease.sql
+++ IMECentricDB/trunk/Data/NextRelease.sql
@@ -11,3 +11,39 @@ VALUES
     )
 
 GO
+
+
+
+-- // 11822 - update / patch records where the fee unit is not 'Fee','Hourly','Inch','Each'
+with
+x as (
+	select AcctQuoteID, CaseNbr, FeeUnit,
+		(case
+			when FeeUnit like '%hr%' then 'Hourly'
+			when FeeUnit like '%hour%' then 'Hourly'
+			else 'Fee'
+			end) as ChangeFeeUnit
+	from tblAcctQuote taq
+	where FeeUnit not in ('Fee','Hourly','Inch','Each')
+)
+update tblAcctQuote set FeeUnit = x.ChangeFeeUnit
+from tblAcctQuote y join x on y.AcctQuoteID = x.AcctQuoteID
+GO
+
+
+
+-- // 11822 - update / patch records where the fee unit is not 'Fee','Hourly','Inch','Each'
+with
+x as (
+	select AcctQuoteFeeID, AcctQuoteID, FeeUnit,
+		(case
+			when FeeUnit like '%hr%' then 'Hourly'
+			when FeeUnit like '%hour%' then 'Hourly'
+			else 'Fee'
+			end) as ChangeFeeUnit
+	from tblAcctQuoteFee taqf
+	where FeeUnit not in ('Fee','Hourly','Inch','Each')
+)
+update tblAcctQuoteFee set FeeUnit = x.ChangeFeeUnit
+from tblAcctQuoteFee y join x on y.AcctQuoteFeeID = x.AcctQuoteFeeID
+GO
