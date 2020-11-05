455c30348d13b737575f467644ef75aa40d8fa1d
David Rose <david.rose@examworks.com>
2020-11-05 05:46:37 -0500

    Added creation of spGetAllBusinessRules

--- Scripts/3.77.sql
+++ Scripts/3.77.sql
@@ -249,6 +249,93 @@ IF @@TRANCOUNT = 0
 
 
 GO
+PRINT N'Adding [dbo].[spGetAllBusinessRules]...';
+
+
+GO
+CREATE PROCEDURE [dbo].[spGetAllBusinessRules]
+(
+	@eventID INT,
+    @clientCode INT,
+    @billClientCode INT,
+    @officeCode INT,
+    @caseType INT,
+    @serviceCode INT,
+    @jurisdiction VARCHAR(5)
+)
+AS
+BEGIN
+
+	SET NOCOUNT ON
+
+	SELECT DISTINCT * FROM (
+	SELECT BR.BusinessRuleID, BR.Category, BR.Name,	 
+	 tmpBR.BusinessRuleConditionID,
+	 tmpBR.Param1,
+	 tmpBR.Param2,
+	 tmpBR.Param3,
+	 tmpBR.Param4,
+	 tmpBR.Param5,
+	 tmpBR.EntityType,
+	 tmpBR.ProcessOrder
+	FROM
+	(
+	
+	SELECT 1 AS GroupID, BRC.*
+	FROM tblBusinessRuleCondition AS BRC
+	LEFT OUTER JOIN tblClient AS CL ON CL.ClientCode = @billClientCode
+	LEFT OUTER JOIN tblCompany AS CO ON CO.CompanyCode = CL.CompanyCode
+	WHERE 1=1
+	AND (BRC.BillingEntity IN (0,2))
+	AND (CASE BRC.EntityType WHEN 'PC' THEN CO.ParentCompanyID WHEN 'CO' THEN CO.CompanyCode WHEN 'CL' THEN CL.ClientCode ELSE 0 END = BRC.EntityID)
+
+	UNION
+
+	SELECT 2 AS GroupID, BRC.*
+	FROM tblBusinessRuleCondition AS BRC
+	LEFT OUTER JOIN tblClient AS CL ON CL.ClientCode = @clientCode
+	LEFT OUTER JOIN tblCompany AS CO ON CO.CompanyCode = CL.CompanyCode
+	WHERE 1=1
+	AND (BRC.BillingEntity IN (1,2))
+	AND (CASE BRC.EntityType WHEN 'PC' THEN CO.ParentCompanyID WHEN 'CO' THEN CO.CompanyCode WHEN 'CL' THEN CL.ClientCode ELSE 0 END = BRC.EntityID)
+
+	UNION
+
+	SELECT 3 AS GroupID, BRC.*
+	FROM tblBusinessRuleCondition AS BRC
+	WHERE 1=1
+	AND (BRC.EntityType='SW')
+	) AS tmpBR
+	INNER JOIN tblBusinessRule AS BR ON BR.BusinessRuleID = tmpBR.BusinessRuleID
+	LEFT OUTER JOIN tblCaseType AS CT ON CT.Code = @caseType
+	LEFT OUTER JOIN tblServices AS S ON S.ServiceCode = @serviceCode
+	WHERE BR.IsActive=1
+	AND BR.EventID=@eventID
+	AND (tmpBR.OfficeCode IS NULL OR tmpBR.OfficeCode = @officeCode)
+	AND (tmpBR.EWBusLineID IS NULL OR tmpBR.EWBusLineID = CT.EWBusLineID)
+	AND (tmpBR.EWServiceTypeID IS NULL OR tmpBR.EWServiceTypeID = S.EWServiceTypeID)
+	AND (tmpBR.Jurisdiction IS NULL OR tmpBR.Jurisdiction = @jurisdiction)
+	
+	) AS sortedBR	
+	ORDER BY sortedBR.BusinessRuleID
+END
+GO
+IF @@ERROR <> 0
+   AND @@TRANCOUNT > 0
+    BEGIN
+        ROLLBACK;
+    END
+
+IF @@TRANCOUNT = 0
+    BEGIN
+        INSERT  INTO #tmpErrors (Error)
+        VALUES                 (1);
+        BEGIN TRANSACTION;
+    END
+
+
+GO
+
 PRINT N'Altering [dbo].[vwAutoProvisions]...';
 
 
