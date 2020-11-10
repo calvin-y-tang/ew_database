85d124e2834979524b1a3b2060b9e3d564477af2
Terri Lyde <terri.lyde@examworks.com>
2020-11-10 09:44:29 -0500

    Issue 11636 - added joins to tblServices and tblEWServiceType to get EWServiceTypeID

--- IMECentricDB/trunk/Views/vwAcctMonitorDetail.sql
+++ IMECentricDB/trunk/Views/vwAcctMonitorDetail.sql
@@ -16,11 +16,14 @@ AS
 			Q.FormToOpen,
 			Q.StatusDesc,
 			Q.DisplayOrder,
-			ISNULL(BillCompany.ParentCompanyID, Company.ParentCompanyID) AS ParentCompanyID
+			ISNULL(BillCompany.ParentCompanyID, Company.ParentCompanyID) AS ParentCompanyID,
+			ST.EWServiceTypeID
     FROM    tblAcctingTrans AS AT
 				INNER JOIN tblCase AS C ON AT.CaseNbr = C.CaseNbr
 				INNER JOIN tblQueues AS Q ON Q.StatusCode = AT.StatusCode
 				INNER JOIN tblCompany AS Company ON Company.CompanyCode = C.CompanyCode
 				LEFT OUTER JOIN tblClient AS BillClient ON BillClient.ClientCode = C.BillClientCode
 				LEFT OUTER JOIN tblCompany AS BillCompany ON BillCompany.CompanyCode = BillClient.CompanyCode
+				LEFT OUTER JOIN tblServices AS S ON S.ServiceCode = C.ServiceCode
+				LEFT OUTER JOIN tblEWServiceType AS ST ON ST.EWServiceTypeID = S.EWServiceTypeID
     WHERE   AT.StatusCode <> 20;
