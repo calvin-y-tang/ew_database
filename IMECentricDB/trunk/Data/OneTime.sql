a721c14e196bf763900c15b21c453e6e7541742c
Terri Lyde <terri.lyde@examworks.com>
2020-12-02 10:56:44 -0500

    Issue 11850 - add states to tblOfficeState that MCMC user are limited to

--- IMECentricDB/trunk/Data/OneTime.sql
+++ IMECentricDB/trunk/Data/OneTime.sql
@@ -5,3 +5,13 @@
 --
 update tblNotifyPreference set UserType = 'CL' where 
 WebUserID IN (select WebUserID from tblClient where UserIDAdded = 'AutoProv') and UserType IS NULL
+
+
+
+-- Issue 11850 - add states to tblOfficeState that MCMC user are limited to 
+INSERT INTO tblOfficeState VALUES (43, 'CO', 'Admin', GetDate()),
+ (43, 'AZ', 'Admin', GetDate()),
+ (43, 'NV', 'Admin', GetDate()),
+ (43, 'UT', 'Admin', GetDate())
+ GO
+
