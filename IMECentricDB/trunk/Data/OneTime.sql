1296e476b7e071e8f4a8ff12b93eaa5579d34fb2
Doug Troy <doug.troy@examworks.com>
2020-12-02 07:14:14 -0500

    Issue - 272 (Portal) data patch to fix SSO auto prov bug with Notify Preferences.

--- IMECentricDB/trunk/Data/OneTime.sql
+++ IMECentricDB/trunk/Data/OneTime.sql
@@ -1,1 +1,7 @@
-﻿
+﻿--
+-- Patch data for SSO notify preference bug. To be run against all US IMEC databases. does not have to 
+-- be run against the Canadian systems.
+-- DHT
+--
+update tblNotifyPreference set UserType = 'CL' where 
+WebUserID IN (select WebUserID from tblClient where UserIDAdded = 'AutoProv') and UserType IS NULL
