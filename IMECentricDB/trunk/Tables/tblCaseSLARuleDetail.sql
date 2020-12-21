18ecfa0b3b0e39e52cf11b91a4a083bf1f1cffa9
Calvin Tang <calvin.tang@examworks.com>
2020-12-21 13:50:31 -0500

    Issue 11940 - Store StartDate and EndDate in tblCaseSLARuleDetail

--- IMECentricDB/trunk/Tables/tblCaseSLARuleDetail.sql
+++ IMECentricDB/trunk/Tables/tblCaseSLARuleDetail.sql
@@ -1,21 +1,25 @@
 ﻿CREATE TABLE [dbo].[tblCaseSLARuleDetail] (
-    [CaseNbr]           INT          NOT NULL,
-    [SLARuleDetailID]   INT          NOT NULL,
-    [RequiredDate]      DATETIME     NULL,
-    [ToleranceDate]     DATETIME     NULL,
-    [Fullfilled]        BIT          CONSTRAINT [DF_tblCaseSLARuleDetail_Fullfilled] DEFAULT ((0)) NOT NULL,
-    [DateAdded]         DATETIME     CONSTRAINT [DF_tblCaseSLARuleDetail_DateAdded] DEFAULT (getdate()) NOT NULL,
-    [SLAExceptionID]    INT          NULL,
-    [Explanation]       VARCHAR(120) NULL, 
-    [UserIDAdded]       VARCHAR(15)  NULL, 
-    [DateEdited]        DATETIME     NULL, 
-    [UserIDEdited]      VARCHAR(15)  NULL, 
-	[SysExceptionFired] BIT          NULL,
-    [CaseSLARuleDetailID] INT NOT NULL IDENTITY, 
-    [IsResolved] BIT NOT NULL DEFAULT 0, 
-    [Responsible] VARCHAR(50) NULL, 
+    [CaseNbr]             INT           NOT NULL,
+    [SLARuleDetailID]     INT           NOT NULL,
+    [RequiredDate]        DATETIME      NULL,
+    [ToleranceDate]       DATETIME      NULL,
+    [Fullfilled]          BIT           CONSTRAINT [DF_tblCaseSLARuleDetail_Fullfilled] DEFAULT ((0)) NOT NULL,
+    [DateAdded]           DATETIME      CONSTRAINT [DF_tblCaseSLARuleDetail_DateAdded] DEFAULT (getdate()) NOT NULL,
+    [SLAExceptionID]      INT           NULL,
+    [Explanation]         VARCHAR (120) NULL,
+    [UserIDAdded]         VARCHAR (15)  NULL,
+    [DateEdited]          DATETIME      NULL,
+    [UserIDEdited]        VARCHAR (15)  NULL,
+    [SysExceptionFired]   BIT           NULL,
+    [CaseSLARuleDetailID] INT           IDENTITY (1, 1) NOT NULL,
+    [IsResolved]          BIT           DEFAULT ((0)) NOT NULL,
+    [Responsible]         VARCHAR (50)  NULL,
+    [StartDate]           DATETIME      NULL,
+    [EndDate]             DATETIME      NULL,
     CONSTRAINT [PK_tblCaseSLARuleDetail] PRIMARY KEY CLUSTERED ([CaseSLARuleDetailID] ASC)
 );
+
+
 
 
 
