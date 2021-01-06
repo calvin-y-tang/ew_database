dc04845a101673ca5a0de5c5948f7cef3809e428
Terri Lyde <terri.lyde@examworks.com>
2021-01-06 12:20:55 -0500

    Issue 11927 - rearranged order of entry into database

--- IMECentricDB/trunk/Data/NextRelease.sql
+++ IMECentricDB/trunk/Data/NextRelease.sql
@@ -15,8 +15,8 @@ GO
 
 -- Issue 11927 - parameters that will show on the custom data tab in IMECentric.  For Allstate
 INSERT INTO tblParamProperty (ParamPropertyGroupID, LabelText, FieldName, Required, AllowedValues, DateAdded, UserIDAdded)
-VALUES (2, 'Policy Company Name', 'PolicyCompanyName', 0, NULL, GETDATE(), 'Admin'),
-       (2, 'Policy Company Code', 'PolicyCompanyCode', 0, NULL, GETDATE(), 'Admin'),
+VALUES (2, 'Policy Company Code', 'PolicyCompanyCode', 0, NULL, GETDATE(), 'Admin'),
+       (2, 'Policy Company Name', 'PolicyCompanyName', 0, NULL, GETDATE(), 'Admin'),
        (2, 'NAIC Company Code', 'NAICCompanyCode', 0, NULL, GETDATE(), 'Admin'),
        (2, 'Address', 'PolicyCompanyAddress', 0, NULL, GETDATE(), 'Admin')
 
