8bc3d5886ac8fcbd9b5e18c5def2f7d57b564d62
Terri Lyde <terri.lyde@examworks.com>
2021-01-11 11:00:41 -0500

    Issue 11916 - need to add Actions to table tblSLAAction

--- IMECentricDB/trunk/Data/NextRelease.sql
+++ IMECentricDB/trunk/Data/NextRelease.sql
@@ -22,3 +22,15 @@ VALUES (2, 'Policy Company Code', 'PolicyCompanyCode', 0, NULL, GETDATE(), 'Admi
 
 GO
 
+
+-- Issue 11916 - need to add Actions to table tblSLAAction
+DELETE FROM tblSLAAction
+
+SET IDENTITY_INSERT [dbo].[tblSLAAction] ON
+INSERT INTO [dbo].[tblSLAAction] ([SLAActionID], [Name], [RequireComment], [IsResolution]) VALUES (1, 'Contacted Client', 0, 0)
+INSERT INTO [dbo].[tblSLAAction] ([SLAActionID], [Name], [RequireComment], [IsResolution]) VALUES (2, 'Contacted Doctor', 0, 0)
+INSERT INTO [dbo].[tblSLAAction] ([SLAActionID], [Name], [RequireComment], [IsResolution]) VALUES (3, 'Contacted Examinee', 0, 0)
+INSERT INTO [dbo].[tblSLAAction] ([SLAActionID], [Name], [RequireComment], [IsResolution]) VALUES (4, 'Resolve SLA', 0, 1)
+INSERT INTO [dbo].[tblSLAAction] ([SLAActionID], [Name], [RequireComment], [IsResolution]) VALUES (5, 'Other', 1, 0)
+SET IDENTITY_INSERT [dbo].[tblSLAAction] OFF
+
