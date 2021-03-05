fbdb876f6ff7063a7a9367abbc0d5581fc21c605
David Rose <david.rose@examworks.com>
2021-03-05 14:04:31 -0500

    DB Release 3.82

---
+++ 3.82/Tables/tblEWFacilityGroupDetail.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[tblEWFacilityGroupDetail] (
+    [EWFacilityGroupID] INT NOT NULL,
+    [EWFacilityID]      INT NOT NULL,
+    CONSTRAINT [PK_tblEWFacilityGroupDetail] PRIMARY KEY CLUSTERED ([EWFacilityGroupID] ASC, [EWFacilityID] ASC)
+);
+
---
+++ 3.82/Views/vwSedgwickClientInformation.sql
@@ -0,0 +1,17 @@
+﻿CREATE VIEW [dbo].[vwSedgwickClientInformation] AS 
+SELECT	SCMSClientId,
+	    ClientName,
+		ClientAddressLine1,
+		ClientAddressLine2,
+		ClientCity,
+		ClientState,
+		ClientPostalCode,
+		ClientPhoneNumber,
+		InceptionDate,
+		ExpirationDate,
+		CASE ClientContactStatus
+			WHEN 'A' THEN 'Active'
+			WHEN 'R' THEN 'Run-off'
+			WHEN 'D' THEN 'Inactive'
+		END AS 'ClientStatus'
+  FROM [CustomRepository].[dbo].[SedgwickClientInformationRecord]
