60297c5e7a76d306e9725c2782cc9d635d553444
Jose Pais <jose.pais@examworks.com>
2021-03-31 14:43:40 -0400

    IMEC-12082 - remove "time" from tblDoctorBlockTimeDay.ScheduleDate

--- IMECentricDB/trunk/Data/OneTime.sql
+++ IMECentricDB/trunk/Data/OneTime.sql
@@ -1,1 +1,9 @@
 ﻿
+-- ISSUE 12082 - remove "time" from tblDoctorBlockTimeDay.ScheduleDate
+UPDATE tblDoctorBlockTimeDay
+SET ScheduleDate = CAST(ScheduleDate AS Date)
+FROM tblDoctorBlockTimeDay
+WHERE DateAdded >= '2021-03-01'
+GO
+
+
