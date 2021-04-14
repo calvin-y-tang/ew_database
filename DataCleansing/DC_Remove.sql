52a201afdf326afbcff62a7e87f3643316fa18e7
Calvin Tang <calvin.tang@examworks.com>
2021-04-14 18:49:53 -0400

    Archive old project

--- DataCleansing/DC_Remove.sql
+++
@@ -1,7 +0,0 @@
-use [VAR_DBINSTANCE];
-
-if object_id('VAR_DBINSTANCE.dbo.DCTableFieldAction') is not null
-drop table VAR_DBINSTANCE.dbo.DCTableFieldAction;
-
-if object_id('VAR_DBINSTANCE.dbo.DCAction') is not null
-drop table VAR_DBINSTANCE.dbo.DCAction;
