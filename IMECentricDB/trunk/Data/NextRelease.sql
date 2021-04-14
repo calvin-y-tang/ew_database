ebd00292e09ffe01e05501bd2dce14d7b45e838d
Jose Pais <jose.pais@examworks.com>
2021-04-14 11:31:55 -0400

    Issue 12026 - add new entries to tblSetting & tblUserFunction for CCMSI Overview.

--- IMECentricDB/trunk/Data/NextRelease.sql
+++ IMECentricDB/trunk/Data/NextRelease.sql
@@ -1,27 +1,3 @@
--- Issue 11897 - new SLA Metrics
-INSERT INTO tblDataField(DataFieldID, TableName, FieldName, Descrip)
-VALUES (216, 'tblCase', 'TATExamSchedToQuoteSent', null),
-       (217, 'tblCase', 'TATExamSchedToApprovalSent', null),
-       (218, 'tblCase', 'TATApprovalSentToResentApproval', null),
-       (219, 'FeeQuote', 'DateClientInformed', 'Fee Quote Sent'), 
-       (220, 'FeeApproval', 'DateClientInformed', 'Fee Approval Sent'),
-       (221, 'FeeApproval', 'DateClientCommResent', 'Resent Fee Approval')
-GO
-INSERT INTO tblTATCalculationMethod(TATCalculationMethodID, StartDateFieldID, EndDateFieldID, Unit, TATDataFieldID, UseTrend)
-VALUES(21, 207, 219, 'Day', 216, 0),
-      (22, 207, 220, 'Day', 217, 0), 
-      (23, 220, 221, 'Day', 218, 0)
-GO
-INSERT INTO tblEvent (EventID, Descrip, Category)
-VALUES(1060, 'Fee Quote/Approval Saved', 'Case')
-GO
-INSERT INTO tblTATCalculationMethodEvent (TATCalculationMethodID, EventID)
-VALUES (21,1060), 
-	   (22,1060), 
-	   (23,1060)
-GO
-
-
 -- Issue 12079 - add med status options to combo
 INSERT INTO tblRecordStatus  (Description, DateAdded, UserIDAdded, PublishOnWeb)
 VALUES ('Awaiting Declaration', GETDATE(), 'TLyde', 1),
@@ -29,4 +5,12 @@ VALUES ('Awaiting Declaration', GETDATE(), 'TLyde', 1),
 
 GO
 
+-- Issue 12026 - add new security token and items to tblSetting for CCMSI
+INSERT INTO tblUserFunction (FunctionCode, FunctionDesc, DateAdded)
+VALUES('CaseOverview', 'Case - CCMSI Overview', GETDATE())
+GO
+INSERT INTO tblSetting(Name, Value)
+VALUES('CCMSIBaseAPIURL', 'https://api.terraclaim.com/connect/vendors/test/'),
+      ('CCMSIAPISecurityToken', '31031783bcb8466abfc45521a2fdcfe9')
+GO
 
