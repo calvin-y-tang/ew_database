9eade6a4f04b81e126ec92318815d1e661175e56
Jose Pais <jose.pais@examworks.com>
2021-04-14 11:35:28 -0400

    Issue 12026 - new tables for CCMSI Overview feature

---
+++ IMECentricDB/trunk/Tables/tblCaseDetermination.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[tblCaseDetermination]
+(
+	[CaseNbr]       INT          NOT NULL, 
+	[DoctorSummary] VARCHAR(MAX) NULL, 
+	[EWSummary]     VARCHAR(MAX) NULL, 
+	[UserIDAdded]   VARCHAR(15)  NOT NULL, 
+	[DateAdded]     DATETIME     NOT NULL, 
+	[UserIDEdited]  VARCHAR(15)  NULL, 
+	[DateEdited]    DATETIME     NULL, 
+	CONSTRAINT [PK_tblCaseDetermination] PRIMARY KEY CLUSTERED ([CaseNbr] ASC)
+)
---
+++ IMECentricDB/trunk/Tables/tblCaseOverviewGroupItem.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[tblCaseOverviewGroupItem]
+(
+	[CaseOverviewGroupItemID] INT          IDENTITY (1, 1) NOT NULL, 
+	[CaseOverviewGroupID]     INT          NOT NULL,
+	[ItemNumber]              INT          NOT NULL, 
+	[ResponseValue]           VARCHAR(8)   NOT NULL, 
+	[Description]             VARCHAR(MAX) NOT NULL, 
+	[IsSelected]              BIT          NULL, 
+	[UserIDAdded]             VARCHAR(15)  NOT NULL, 
+	[DateAdded]               DATETIME     NOT NULL, 
+	[UserIDEdited]            VARCHAR(15)  NULL, 
+	[DateEdited]              DATETIME     NULL, 
+	CONSTRAINT [PK_tblCaseOverviewGroupItem] PRIMARY KEY CLUSTERED ([CaseOverviewGroupItemID] ASC)
+)
