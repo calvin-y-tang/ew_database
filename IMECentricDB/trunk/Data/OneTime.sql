9b50687c5b45d1b75b5e88a03c2986eedf757df7
Terri Lyde <terri.lyde@examworks.com>
2021-04-20 00:12:17 -0400

    Issue 10778 - add additional WHERE clause for CMS 1500 query

--- IMECentricDB/trunk/Data/OneTime.sql
+++ IMECentricDB/trunk/Data/OneTime.sql
@@ -9,10 +9,10 @@ GO
 
 -- Issue 10778 - set CMS1500 Boxes 19 and 28 for past behavior
 UPDATE tblDocument SET CMSBox19 = '%Blank%' 
-WHERE Document LIKE 'CMS1500%' AND CMSBox19 IS NULL
+WHERE Document LIKE 'CMS%' AND Type = 'Invoice' AND CMSBox19 IS NULL
 
 UPDATE tblDocument SET CMSBox28Dollars = '%InvAmtDetailDollars%', CMSBox28Cents='%InvAmtDetailCents'
-WHERE Document LIKE 'CMS1500%' AND CMSBox28Cents IS NULL AND CMSBox28Dollars IS NULL
+WHERE Document LIKE 'CMS%' AND Type = 'Invoice' AND CMSBox28Cents IS NULL AND CMSBox28Dollars IS NULL
 
 
 
