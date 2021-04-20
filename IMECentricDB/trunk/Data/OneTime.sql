6e1cb85d805db1f3a5255d3bb914027ed99e74e8
Terri Lyde <terri.lyde@examworks.com>
2021-04-19 23:45:35 -0400

    Issue 10778 - set data for tblDocument CMS boxes 19 and 28 for past behavior

--- IMECentricDB/trunk/Data/OneTime.sql
+++ IMECentricDB/trunk/Data/OneTime.sql
@@ -7,3 +7,12 @@ WHERE DateAdded >= '2021-03-01'
 GO
 
 
+-- Issue 10778 - set CMS1500 Boxes 19 and 28 for past behavior
+UPDATE tblDocument SET CMSBox19 = '%Blank%' 
+WHERE Document LIKE 'CMS1500%' AND CMSBox19 IS NULL
+
+UPDATE tblDocument SET CMSBox28Dollars = '%InvAmtDetailDollars%', CMSBox28Cents='%InvAmtDetailCents'
+WHERE Document LIKE 'CMS1500%' AND CMSBox28Cents IS NULL AND CMSBox28Dollars IS NULL
+
+
+
