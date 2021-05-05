60a0f38d482d36e79ec64f0654cc05481a0caabd
Jose Pais <jose.pais@examworks.com>
2021-05-05 16:17:43 -0400

    Issue 12131 - schema changes to allow doctor/city for network FS.

--- IMECentricDB/trunk/Tables/tblFSDetailCondition.sql
+++ IMECentricDB/trunk/Tables/tblFSDetailCondition.sql
@@ -3,7 +3,8 @@
 	[FSDetailConditionID] INT IDENTITY (1, 1) NOT NULL,
 	[FSDetailID]          INT NOT NULL,
 	[ConditionTable]      VARCHAR(30) NOT NULL,
-	[ConditionKey]        INT         NOT NULL,
+	[ConditionKey]        INT         NULL,
+	[ConditionValue]      VARCHAR(50) NULL, 
 	CONSTRAINT [PK_tblFSDetailCondition] PRIMARY KEY CLUSTERED ([FSDetailConditionID] ASC)
 )
 
