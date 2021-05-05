333b697f9055802fa33d1805442eb888183af437
Jose Pais <jose.pais@examworks.com>
2021-05-05 16:22:10 -0400

    Issue 12109 - Default Dr  to "All" for network fee schedules

--- IMECentricDB/trunk/Data/OneTime.sql
+++ IMECentricDB/trunk/Data/OneTime.sql
@@ -20,12 +20,19 @@ GO
 -- Issue 10778 - set CMS1500 Boxes 19 and 28 for past behavior
 UPDATE tblDocument SET CMSBox19 = '%Blank%' 
 WHERE Document LIKE 'CMS%' AND Type = 'Invoice' AND CMSBox19 IS NULL
-
+GO
 UPDATE tblDocument SET CMSBox28Dollars = '%InvoiceAmtDollars%', CMSBox28Cents='%InvoiceAmtCents%'
 WHERE Document LIKE 'CMS%' AND Type = 'Invoice' AND CMSBox28Cents IS NULL AND CMSBox28Dollars IS NULL
-
+GO
 
 -- Issue 12030 - sprint 65 Branding for file link - set MCMC offices WebCompanyID to the MCMC Web compay ID
 UPDATE IMECentricEW.dbo.tblOffice SET WebCompanyID = 62 WHERE OfficeCode = 43
+GO
 
-
+-- Issue 12109 - Default Dr  to "All" for network fee schedules
+UPDATE dtl
+SET Doctor = -1
+FROM tblFSHeaderSetup AS hdr
+          INNER JOIN tblFSDetailSetup AS dtl ON dtl.FSHeaderSetupID = hdr.FSHeaderSetupID
+WHERE hdr.EntityType = 'NW'
+GO
