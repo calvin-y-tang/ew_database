77597a1f897dd5fc12e1a0fda26c20d6d85a1b7b
David Rose <david.rose@examworks.com>
2021-05-13 11:15:53 -0400

    Removed another insert where data already exists

--- Scripts/3.85.sql
+++ Scripts/3.85.sql
@@ -487,12 +487,14 @@ PRINT N'Update complete.';
 
 
 GO
--- Issue 12079 - add med status options to combo
-INSERT INTO tblRecordStatus  (Description, DateAdded, UserIDAdded, PublishOnWeb)
-VALUES ('Awaiting Declaration', GETDATE(), 'TLyde', 1),
-       ('Declaration Received', GETDATE(), 'TLyde', 1)
 
-GO
+--The following seems to exist in all databases.  Not including this
+-- Issue 12079 - add med status options to combo
+--INSERT INTO tblRecordStatus  (Description, DateAdded, UserIDAdded, PublishOnWeb)
+--VALUES ('Awaiting Declaration', GETDATE(), 'TLyde', 1),
+--       ('Declaration Received', GETDATE(), 'TLyde', 1)
+--
+--GO
 
 -- Issue 12026 - add new security token and items to tblSetting for CCMSI
 INSERT INTO tblUserFunction (FunctionCode, FunctionDesc, DateAdded)
