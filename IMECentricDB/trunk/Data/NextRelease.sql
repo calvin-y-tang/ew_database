fd36c180a6deddd944651604b0fdad77cc2e4c72
Terri Lyde <terri.lyde@examworks.com>
2021-05-17 11:20:54 -0400

    Issue 12097 - add new business rule and BR conditions for checking Exam time fields before finalizing reports

--- IMECentricDB/trunk/Data/NextRelease.sql
+++ IMECentricDB/trunk/Data/NextRelease.sql
@@ -14,3 +14,14 @@ VALUES('CCMSIBaseAPIURL', 'https://api.terraclaim.com/connect/vendors/test/'),
       ('CCMSIAPISecurityToken', '31031783bcb8466abfc45521a2fdcfe9')
 GO
 
+
+-- Issue 12097 - add new business rules and BR conditions to check the Exam Time fields before finalizing a report
+INSERT INTO tblBusinessRule (BusinessRuleID, Name, Category, Descrip, IsActive, EventID, AllowOverride, Param1Desc, Param2Desc, Param3Desc, Param4Desc, Param5Desc, BrokenRuleAction)
+VALUES(140, 'FinalizeRptCaptureExamTimes', 'Case', 'Exam times required when finalizing report', 1, 1320, 0, NULL, NULL, NULL, NULL, NULL, 0)
+
+INSERT INTO tblBusinessRuleCondition(EntityType, EntityID, BillingEntity, ProcessOrder, BusinessRuleID, DateAdded, UserIDAdded, DateEdited, UserIDEdited, OfficeCode, EWBusLineID, EWServiceTypeID, Jurisdiction, Param1, Param2, Param3, Param4, Param5)
+VALUES('SW', NULL, 0, 1, 140, GetDate(), 'Admin', NULL, NULL, NULL, 3, 1, 'NY', NULL, NULL, NULL, NULL, NULL)
+
+INSERT INTO tblBusinessRuleCondition(EntityType, EntityID, BillingEntity, ProcessOrder, BusinessRuleID, DateAdded, UserIDAdded, DateEdited, UserIDEdited, OfficeCode, EWBusLineID, EWServiceTypeID, Jurisdiction, Param1, Param2, Param3, Param4, Param5)
+VALUES('SW', NULL, 0, 1, 140, GetDate(), 'Admin', NULL, NULL, NULL, 3, 3, 'NY', NULL, NULL, NULL, NULL, NULL)
+
