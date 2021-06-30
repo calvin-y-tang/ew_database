b65be7a43000a0ab40c5bfc50a5004ac2cf1a613
David Rose <david.rose@examworks.com>
2021-06-30 12:54:21 -0400

    DB Release 3.87

---
+++ 3.87/Tables/tblSLAAction.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[tblSLAAction] (
+    [SLAActionID]    INT          IDENTITY (1, 1) NOT NULL,
+    [Name]           VARCHAR (25) NULL,
+    [RequireComment] BIT          CONSTRAINT [DF_tblSLAAction_RequireComment] DEFAULT ((0)) NOT NULL,
+    [IsResolution]   BIT          CONSTRAINT [DF_tblSLAAction_IsResolution] DEFAULT ((0)) NOT NULL,
+    CONSTRAINT [PK_tblSLAAction] PRIMARY KEY CLUSTERED ([SLAActionID] ASC)
+);
+
+
