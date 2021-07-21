2aa4efb73d73e14af22e740ee1ed3f84fe7fccd3
David Rose <david.rose@examworks.com>
2021-07-21 10:35:02 -0400

    Added script to insert a security token that was not initially included.

--- Scripts/3.88.sql
+++ Scripts/3.88.sql
@@ -158,3 +158,10 @@ GO
 INSERT INTO tblBusinessRule (BusinessRuleID, Name, Category, Descrip, IsActive, EventID, AllowOverride, Param1Desc, Param2Desc, 
 Param3Desc, Param4Desc, Param5Desc, BrokenRuleAction)
 VALUES(160, 'GenDocDefaultOtherEmail', 'Case', 'Set Default Email for Other Party when generating document', 1, 1201, 0, 'OtherEmail', NULL, NULL, NULL, NULL, 0)
+
+-- Insert a security token
+  INSERT INTO tblUserFunction
+( FunctionCode, FunctionDesc )
+VALUES
+( 'AllstateApprovedQuoteOverride', 'Accounting - Create Allstate IN WO Apprvd Quote')
+GO
