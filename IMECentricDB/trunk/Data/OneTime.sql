be2e5a9251e377e99bf18bd25c8350add72f7f90
Jose Pais <jose.pais@examworks.com>
2021-07-29 12:58:27 -0400

    Issue 12222 - schema change for EWParentCompany

--- IMECentricDB/trunk/Data/OneTime.sql
+++ IMECentricDB/trunk/Data/OneTime.sql
@@ -1,1 +1,8 @@
 ﻿
+-- Issue 12222 - new setting for require out of network reason on quote form.
+ALTER TABLE EWParentCompany ADD [RequireOutofNetworkReason] BIT CONSTRAINT [DF_tblEWParentCompany_RequireOutofNetworkReason] DEFAULT (0)
+GO
+UPDATE tblEWParentCompany SET RequireOutofNetworkReason = 0
+GO
+UPDATE tblEWParentCompany SET RequireOutofNetworkReason = 1 WHERE ParentCompanyID = 9
+GO
