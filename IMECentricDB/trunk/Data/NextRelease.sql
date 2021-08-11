736b798d2a46fa3553d508d901269a4a0c15fbcd
Jose Pais <jose.pais@examworks.com>
2021-08-11 14:16:20 -0400

    Issue 12222 - DB Changes to support new functionality for Quotes.

--- IMECentricDB/trunk/Data/NextRelease.sql
+++ IMECentricDB/trunk/Data/NextRelease.sql
@@ -17,3 +17,16 @@ UPDATE tblEWParentCompany SET RequireOutofNetworkReason = 0
 GO
 UPDATE tblEWParentCompany SET RequireOutofNetworkReason = 1 WHERE ParentCompanyID = 9
 GO
+
+-- Issue 12222 - Enhanced Quote functionality
+INSERT INTO tblEvent (EventID, Descrip, Category)
+VALUES(1061, 'Access Fee Quote', 'Case')
+GO
+INSERT INTO tblBusinessRule (BusinessRuleID, Name, Category, Descrip, IsActive, EventID, AllowOverride, Param1Desc, Param2Desc, Param3Desc, Param4Desc, Param5Desc, BrokenRuleAction)
+VALUES (152, 'UseQuoteSpecialColumns', 'Case', 'Allow the use of the Med Rec Pages field on Quote Form', 1, 1061, 0, 'MedRecsPages', NULL, NULL, NULL, NULL, 0)
+GO
+INSERT INTO tblBusinessRuleCondition(BusinessRuleID, EntityType, EntityID, BillingEntity, ProcessOrder, DateAdded, UserIDAdded, DateEdited, UserIDEdited, OfficeCode, EWBusLineID, EWServiceTypeID, Jurisdiction, Param1, Param2, Param3, Param4, Param5)
+VALUES(152, 'PC', 9, 2, 1, GETDATE(), 'Admin', GETDATE(), 'Admin', NULL, NULL, NULL, NULL, 'True', NULL, NULL, NULL, NULL)
+GO
+
+
