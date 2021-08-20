0961f66c3cab54c53c9651f11929fd9e4b927fdd
Jose Pais <jose.pais@examworks.com>
2021-08-20 16:50:58 -0400

    Issue 12215 - removed code from previous sprint; added statements to create new business rule & conditions for the Duplicate Case customer data option.

--- IMECentricDB/trunk/Data/NextRelease.sql
+++ IMECentricDB/trunk/Data/NextRelease.sql
@@ -1,41 +1,13 @@
--- Issue 12199 - Amtrust Parent Company stuff
-INSERT INTO tblBusinessRule (BusinessRuleID, Name, Category, Descrip, IsActive, EventID, AllowOverride, Param1Desc, Param2Desc, Param3Desc, Param4Desc, Param5Desc, BrokenRuleAction)
-VALUES (150, 'ApplyAmtrustGuardRailsQuote', 'Case', 'Check if the Amtrust guardrails need to be applied when saving a quote.', 1, 1060, 0, NULL, NULL, NULL, NULL, NULL, 0), 
-       (151, 'ApplyAmtrustGuardRailsInvoice', 'Case', 'Check if the Amtrust guardrails need to be applied when generating an invoice.', 1, 1811, 0, NULL, NULL, NULL, NULL, NULL, 0)
-GO
-INSERT INTO tblBusinessRuleCondition(BusinessRuleID, EntityType, EntityID, BillingEntity, ProcessOrder, DateAdded, UserIDAdded, DateEdited, UserIDEdited, 
-                                     OfficeCode, EWBusLineID, EWServiceTypeID, Jurisdiction, Param1, Param2, Param3, Param4, Param5)
-VALUES(150, 'PC', 9, 2, 1, GETDATE(), 'Admin', GETDATE(), 'Admin', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-      (151, 'PC', 9, 2, 1, GETDATE(), 'Admin', GETDATE(), 'Admin', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)
-GO
-
--- Issue 12202/12203 - new product item for quote additional fees
-INSERT INTO tblQuoteFeeConfig (FeeValueName, DisplayOrder, DateAdded, UserIDAdded, ProdCode)
-VALUES('Med Recs > 250pgs', 45, GETDATE(), 'Admin', 385)
-GO
-UPDATE tblEWParentCompany SET RequireOutofNetworkReason = 0
-GO
-UPDATE tblEWParentCompany SET RequireOutofNetworkReason = 1 WHERE ParentCompanyID = 9
-GO
-
--- Issue 12222 - Enhanced Quote functionality
-INSERT INTO tblEvent (EventID, Descrip, Category)
-VALUES(1061, 'Access Fee Quote', 'Case')
-GO
-INSERT INTO tblBusinessRule (BusinessRuleID, Name, Category, Descrip, IsActive, EventID, AllowOverride, Param1Desc, Param2Desc, Param3Desc, Param4Desc, Param5Desc, BrokenRuleAction)
-VALUES (152, 'UseQuoteSpecialColumns', 'Case', 'Allow the use of the Med Rec Pages field on Quote Form', 1, 1061, 0, 'MedRecsPages', NULL, NULL, NULL, NULL, 0)
-GO
-INSERT INTO tblBusinessRuleCondition(BusinessRuleID, EntityType, EntityID, BillingEntity, ProcessOrder, DateAdded, UserIDAdded, DateEdited, UserIDEdited, OfficeCode, EWBusLineID, EWServiceTypeID, Jurisdiction, Param1, Param2, Param3, Param4, Param5)
-VALUES(152, 'PC', 9, 2, 1, GETDATE(), 'Admin', GETDATE(), 'Admin', NULL, NULL, NULL, NULL, 'True', NULL, NULL, NULL, NULL)
-GO
-
--- Issue 12231 - new override setting for creating line items from quote additional fees.
-INSERT INTO tblSetting (Name, Value)
-VALUES('DoNotCreateQuoteLineItems', '')
-GO
-
 -- Issue 12229 - Parent company bookmark request - add token also
   INSERT INTO tblMessageToken (Name, Description)
   VALUES ('@ParentCompany@', '')
+GO 
 
-
+-- Issue  12215 - add business rules for additional refinement of "Customer Data" option for duplicate/new sub case
+INSERT INTO tblBusinessRule (BusinessRuleID, Name, Category, Descrip, IsActive, EventID, AllowOverride, Param1Desc, Param2Desc, Param3Desc, Param4Desc, Param5Desc, BrokenRuleAction)
+VALUES (153, 'CustomerDataForDupCase', 'Case', 'Data Handling setting for tblCustomerData when duplicate/new sub case', 1, 1001, 0, 'DataHandlingValue', 'InputSourceID', NULL, NULL, NULL, 0)
+GO
+INSERT INTO tblBusinessRuleCondition(BusinessRuleID, EntityType, EntityID, BillingEntity, ProcessOrder, DateAdded, UserIDAdded, DateEdited, UserIDEdited, OfficeCode, EWBusLineID, EWServiceTypeID, Jurisdiction, Param1, Param2, Param3, Param4, Param5)
+VALUES(153, 'PC', 4, 2, 1, GETDATE(), 'Admin', GETDATE(), 'Admin', NULL, NULL, NULL, NULL, 'Never', NULL, NULL, NULL, NULL), 
+      (153, 'PC', 44, 2, 1, GETDATE(), 'Admin', GETDATE(), 'Admin', NULL, NULL, NULL, NULL, 'Always', '7', NULL, NULL, NULL)
+GO
