a6e0dc396e9272ac404a552faab101513d67b0ec
Jose Pais <jose.pais@examworks.com>
2021-09-01 12:34:16 -0400

    Issue 12269 - changes to business rules that were previously added for this issue.

--- IMECentricDB/trunk/Data/NextRelease.sql
+++ IMECentricDB/trunk/Data/NextRelease.sql
@@ -14,11 +14,10 @@ GO
 
 -- Issue 12269 - add business rules for Quote Resend Email time calculation
 INSERT INTO tblBusinessRule (BusinessRuleID, Name, Category, Descrip, IsActive, EventID, AllowOverride, Param1Desc, Param2Desc, Param3Desc, Param4Desc, Param5Desc, BrokenRuleAction)
-VALUES (154, 'AutoResendQuoteDoc', 'Case', 'Determine the Resent Date for Quote Document.', 1, 1201, 0, 'InOutNetwork', 'NbrHrsAddToOrigDate', NULL, NULL, NULL, 0)
+VALUES (154, 'AutoResendQuoteDoc', 'Case', 'Determine the Resent Date for Quote Document.', 1, 1201, 0, 'NbrHrsAddToOrigDate', NULL, NULL, NULL, NULL, 0)
 GO
 INSERT INTO tblBusinessRuleCondition(BusinessRuleID, EntityType, EntityID, BillingEntity, ProcessOrder, DateAdded, UserIDAdded, DateEdited, UserIDEdited, OfficeCode, EWBusLineID, EWServiceTypeID, Jurisdiction, Param1, Param2, Param3, Param4, Param5)
-VALUES(154, 'PC', 4, 2, 1, GETDATE(), 'Admin', GETDATE(), 'Admin', NULL, NULL, 1, NULL, 'Out', '16', NULL, NULL, NULL), 
-      (154, 'PC', 4, 2, 1, GETDATE(), 'Admin', GETDATE(), 'Admin', NULL, NULL, 2, NULL, 'Out', '16', NULL, NULL, NULL), 
-      (154, 'PC', 4, 2, 1, GETDATE(), 'Admin', GETDATE(), 'Admin', NULL, NULL, 3, NULL, 'Out', '16', NULL, NULL, NULL)
+VALUES(154, 'PC', 4, 2, 1, GETDATE(), 'Admin', GETDATE(), 'Admin', NULL, NULL, 1, NULL, '16', NULL, NULL, NULL, NULL), 
+      (154, 'PC', 4, 2, 1, GETDATE(), 'Admin', GETDATE(), 'Admin', NULL, NULL, 2, NULL, '16', NULL, NULL, NULL, NULL), 
+      (154, 'PC', 4, 2, 1, GETDATE(), 'Admin', GETDATE(), 'Admin', NULL, NULL, 3, NULL, '16', NULL, NULL, NULL, NULL)
 GO
-
