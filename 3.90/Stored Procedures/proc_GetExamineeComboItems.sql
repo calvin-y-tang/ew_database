047c5291f28fd9ccd2b2d75556a98389d04dc021
David Rose <david.rose@examworks.com>
2021-09-02 04:51:27 -0400

    DB Release 3.90

---
+++ 3.90/Stored Procedures/proc_GetExamineeComboItems.sql
@@ -0,0 +1,15 @@
+﻿
+
+CREATE PROCEDURE [proc_GetExamineeComboItems]
+
+AS
+
+SELECT DISTINCT chartnbr, lastname, firstname, lastname + ', ' + firstname AS ExamineeName 
+FROM tblExaminee 
+
+WHERE (NOT (tblExaminee.lastname IS NULL)) AND (NOT (tblExaminee.firstname IS NULL)) 
+ AND (NOT (tblExaminee.lastname = '')) AND (NOT (tblExaminee.firstname = '')) 
+ORDER BY tblExaminee.lastname
+
+
+
---
+++ 3.90/Views/vwAssocCases.sql
@@ -0,0 +1,36 @@
+﻿CREATE VIEW vwAssocCases
+AS
+    SELECT
+        tblDoctor.LastName,
+        tblDoctor.FirstName,
+        tblDoctor.Credentials AS degree,
+        tblSpecialty.Description AS Specialty,
+        tblCase.ApptDate,
+        tblCase.ApptTime,
+        tblCase.CaseNbr,
+        tblCase.MasterSubCase,
+        tblCase.MasterCaseNbr,
+        tblDoctor.Prefix,
+        tblSpecialty.SpecialtyCode,
+        tblDoctor.DoctorCode,
+        tblDoctor.WCNbr,
+        tblDoctor.UnRegNbr,
+        tblServices.Description AS Service,
+        tblLocation.Addr1,
+        tblLocation.Addr2,
+        tblLocation.City,
+        tblLocation.State,
+        tblLocation.Zip,
+        tblLocation.Phone,
+        tblLocation.Fax,
+        tblCaseAppt.Duration,
+        tblCase.Status,
+        tblProviderType.Description AS DoctorProviderType
+    FROM
+        tblServices
+    INNER JOIN tblCase ON tblServices.ServiceCode=tblCase.ServiceCode
+    LEFT OUTER JOIN tblDoctor ON tblCase.DoctorCode=tblDoctor.DoctorCode
+    LEFT OUTER JOIN tblLocation ON tblCase.DoctorLocation=tblLocation.LocationCode
+    LEFT OUTER JOIN tblProviderType ON tblDoctor.ProvTypeCode=tblProviderType.ProvTypeCode
+    LEFT OUTER JOIN tblCaseAppt ON tblCase.CaseApptID = tblCaseAppt.CaseApptID
+    LEFT OUTER JOIN tblSpecialty ON tblCase.DoctorSpecialty=tblSpecialty.SpecialtyCode
