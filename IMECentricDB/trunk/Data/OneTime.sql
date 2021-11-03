48322182d39036b614fbf6c27235276474e8a2ec
Terri Lyde <terri.lyde@examworks.com>
2021-11-03 12:37:45 -0400

    IMEC-12367 - make sure all offices are set to use the OCR process: set tblOffice.OCRSystemID = 1

--- IMECentricDB/trunk/Data/OneTime.sql
+++ IMECentricDB/trunk/Data/OneTime.sql
@@ -4,6 +4,10 @@ UPDATE tblEWParentCompany SET RequireExamineeDOB = 0
 GO
 
 
+-- IMEC-12367 - make sure all offices are set to use the OCR process
+UPDATE tblOffice SET OCRSystemID = 1
+GO
+
 
 -- ******************** FOR IMECentricMASTER DB **********************
 ALTER TABLE EWParentCompany ADD [RequireExamineeDOB] BIT CONSTRAINT [DF_EWParentCompany_RequireExamineeDOB] DEFAULT (0)
