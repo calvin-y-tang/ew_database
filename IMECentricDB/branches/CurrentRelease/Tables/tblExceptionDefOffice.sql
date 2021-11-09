5780afedf6363107cdae1ad121159489849b4f7d
Calvin Tang <calvin.tang@examworks.com>
2021-11-09 14:18:23 -0500

    IMECDO-94 - Remove branch no longer used

--- IMECentricDB/branches/CurrentRelease/Tables/tblExceptionDefOffice.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[tblExceptionDefOffice] (
-    [ExceptionDefID] INT NOT NULL,
-    [OfficeCode]     INT NOT NULL,
-    CONSTRAINT [PK_tblExceptionDefOffice] PRIMARY KEY CLUSTERED ([ExceptionDefID] ASC, [OfficeCode] ASC)
-);
-
