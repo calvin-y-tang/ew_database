ca2b2597f641b4c42bf1bcfa2b202cb1bd790a0f
Terri Lyde <terri.lyde@examworks.com>
2021-11-12 13:55:34 -0500

    EWIS 12427 - added field Tags to tblCase - varchar(1000), NULL

--- IMECentricDB/trunk/Tables/tblCase.sql
+++ IMECentricDB/trunk/Tables/tblCase.sql
@@ -283,6 +283,7 @@ CREATE TABLE [dbo].[tblCase] (
     [ExamStartTime]                    DATETIME         NULL,
     [ExamEndTime]                      DATETIME         NULL,
     [TimeReviewingRecords]             VARCHAR (20)     NULL,
+    [Tags]                             VARCHAR (1000)   NULL,
     CONSTRAINT [PK_tblCase] PRIMARY KEY CLUSTERED ([CaseNbr] ASC),
     CONSTRAINT [FK_tblCase_tblClient] FOREIGN KEY ([ClientCode]) REFERENCES [dbo].[tblClient] ([ClientCode]),
     CONSTRAINT [FK_tblCase_tblExaminee] FOREIGN KEY ([ChartNbr]) REFERENCES [dbo].[tblExaminee] ([ChartNbr])
