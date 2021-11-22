ee993a5941fdd338a2561c1ea28207a377af70e7
Terri Lyde <terri.lyde@examworks.com>
2021-11-22 10:34:52 -0500

    IMEC 12418 - added business rules for Hartford resend quote approvals

--- IMECentricDB/Data/OneTime.sql
+++ IMECentricDB/Data/OneTime.sql
@@ -9,10 +9,18 @@ VALUES (3020,'LibertyXML NY-MCMC', 'LibertyXML', 1, '\\dev4.ew.domain.local\ISIn
 
  GO
 
-INSERT INTO ISSchedule
- (Name, Task, Type, Interval, Weekdays, Time, StartDate, Param)
-VALUES ('LibertyXML NY-MCMC', 'LibertyXML', 'm', 2, '1111111', '1900-01-01 06:00:00', '2020-11-01 00:00:00', 
- 'DBID=23;UnknownClientCode=791347;ParentCompanyID=31;DefaultOfficeCode=28;Tags=UseMCMCOffice')
+ -- Issue 12418 - Hartford resend quote approvals a 2nd time then approve - adding business rules to add resend date
+ INSERT INTO tblBusinessRuleCondition(EntityType, EntityID, BillingEntity, ProcessOrder, BusinessRuleID, DateAdded, 
+UserIDAdded, DateEdited, UserIDEdited,   OfficeCode, EWBusLineID, EWServiceTypeID, Jurisdiction, Param1, Param2, Param3, Param4, Param5, Skip)
+VALUES('PC', 30, 2, 1, 154, GetDate(), 'Admin', NULL, NULL,    NULL, 3, NULL, 'CA', '16', NULL, NULL, NULL, NULL, 1)
 
- GO
+INSERT INTO tblBusinessRuleCondition(EntityType, EntityID, BillingEntity, ProcessOrder, BusinessRuleID, DateAdded, 
+UserIDAdded, DateEdited, UserIDEdited,   OfficeCode, EWBusLineID, EWServiceTypeID, Jurisdiction, Param1, Param2, Param3, Param4, Param5, Skip)
+VALUES('PC', 30, 2, 1, 154, GetDate(), 'Admin', NULL, NULL,    NULL, 3, NULL, 'TX', '16', NULL, NULL, NULL, NULL, 1)
+
+INSERT INTO tblBusinessRuleCondition(EntityType, EntityID, BillingEntity, ProcessOrder, BusinessRuleID, DateAdded, 
+UserIDAdded, DateEdited, UserIDEdited,   OfficeCode, EWBusLineID, EWServiceTypeID, Jurisdiction, Param1, Param2, Param3, Param4, Param5, Skip)
+VALUES('PC', 30, 2, 2, 154, GetDate(), 'Admin', NULL, NULL,    NULL, NULL, NULL, NULL, '16', NULL, NULL, NULL, NULL, 0)
+
+GO
 
