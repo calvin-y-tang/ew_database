372d610497978425528eb1b24d98e1e2fed5baff
Terri Lyde <terri.lyde@examworks.com>
2021-11-30 14:48:38 -0500

    IMEC 12441 - updates to new business rules for fee quote distributions - changed Param1 from 16 to 8 for business hours

--- IMECentricDB/Data/OneTime.sql
+++ IMECentricDB/Data/OneTime.sql
@@ -10,3 +10,16 @@ VALUES (3020,'LibertyXML NY-MCMC', 'LibertyXML', 1, '\\dev4.ew.domain.local\ISIn
  GO
 
 
+ -- Issue 12441 - changes to ExtIntegrations for automatic fee quote distributions - new input parameters
+  update ISExtIntegration set Param = 'DBID=23;PCID=4;AttachQuote=true;ActiveCasesOnly=true;InNetworkQuotes=false;Subject=Second Request - %ClaimNbr% - %ExamineeName% IME Quote;Body=Dear %ClientFirstName%,<br /><br />This is a second request for the quote approval originally sent to you on %CaseDocDateAdded%. <br /><br />This request will serve as acknowledgement and approval of the quote (attached). <br /><br />If you have any questions, please contact our office at %OfficePhone%'
+  where ExtIntegrationID = 6000
+
+  GO
+
+  insert into ISExtIntegration (ExtIntegrationID, Name, Type, Active, NotifyEmail, Param)
+  Values (6001, 'Hartford Fee Quote Dist', 'FeeQuoteDist', 1, 'terri.lyde@examworks.com',
+  'DBID=23;PCID=30;AttachQuote=true;ActiveCasesOnly=true;Subject=Second Request - %ClaimNbr% - %ExamineeName% IME Quote;Body=Dear %ClientFirstName%,<br /><br />This is a second request for the quote approval originally sent to you on %CaseDocDateAdded%. <br /><br />This request will serve as acknowledgement and approval of the quote (attached). <br /><br />If you have any questions, please contact our office at %OfficePhone%')
+
+  GO
+
+
