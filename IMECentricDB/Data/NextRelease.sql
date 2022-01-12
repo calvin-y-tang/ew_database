5112645d048a87dd8f32aa7df7cd6eeb407c2f03
jose.pais <jose.pais@examworks.com>
2022-01-12 14:43:03 -0500

    IMEC-12320 - add new column to tblCaseAppt for Show/No Show letter; new business rule for Appt Confirmation/show/no show letters; remove tblSetting for appt confirmation letter that is no longer used

--- IMECentricDB/Data/NextRelease.sql
+++ IMECentricDB/Data/NextRelease.sql
@@ -10,4 +10,18 @@ SET RequireInOutNetworkInvoice = ISNULL(RequireInOutNetworkInvoice, 0),
     RequireOutofNetworkReason = ISNULL(RequireOutofNetworkReason, 0)
 GO
 
+-- IMEC-12320 - Add new business rule for capture date appt conf/show/no show letters are generated
+--      remove old tblSetting for appt conf letter
+    INSERT INTO tblBusinessRule (BusinessRuleID, Name, Category, Descrip, IsActive, EventID, AllowOverride, Param1Desc, Param2Desc, Param3Desc, Param4Desc, Param5Desc, BrokenRuleAction)
+VALUES (141, 'SaveApptLetterDateToCaseAppt', 'Case', 'Save Date Doc Created to tblCaseAppt', 1, 1201, 0, 'ApptStatusID', 'DocContentType', 'CaseApptColumnName', NULL, NULL, 0)
+GO
+INSERT INTO tblBusinessRuleCondition(BusinessRuleID, EntityType, EntityID, BillingEntity, ProcessOrder, DateAdded, UserIDAdded, DateEdited, UserIDEdited, OfficeCode, EWBusLineID, EWServiceTypeID, Jurisdiction, Param1, Param2, Param3, Param4, Param5, Skip)
+VALUES(141, 'SW', NULL, 2, 1, GETDATE(), 'Admin', GETDATE(), 'Admin', NULL, NULL, NULL, NULL, '10', 'Appointment Confirmation', 'DateApptLetterSent', NULL, NULL, 0),
+      (141, 'SW', NULL, 2, 1, GETDATE(), 'Admin', GETDATE(), 'Admin', NULL, NULL, NULL, NULL, '101', 'No Show Notice', 'DateShowNoShowLetterSent', NULL, NULL, 0), 
+      (141, 'SW', NULL, 2, 1, GETDATE(), 'Admin', GETDATE(), 'Admin', NULL, NULL, NULL, NULL, '100', 'Attendance Confirmation', 'DateShowNoShowLetterSent', NULL, NULL, 0)
+GO
+DELETE 
+  FROM tblSetting 
+ WHERE Name = 'ApptLetterContentType'
+GO 
 
