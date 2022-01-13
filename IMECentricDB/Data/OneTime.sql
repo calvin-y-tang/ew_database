625dffdfb5e63518921f37b06a731f42abfdf9f4
Terri Lyde <terri.lyde@examworks.com>
2022-01-13 10:41:06 -0500

    IMEC 12293 - add Visible column to tblParamProperty; add records to tblParamProperty to display Senior Manager and Policy Company Code

--- IMECentricDB/Data/OneTime.sql
+++ IMECentricDB/Data/OneTime.sql
@@ -39,3 +39,13 @@ GO
 
 
 
+-- ISSUE 12293 - Allstate VAT ERP Import Senior Manager Field From JSON
+  -- Making Policy Company Code field visible
+INSERT INTO tblParamProperty (ParamPropertyGroupID, LabelText, FieldName, Required, AllowedValues, DateAdded, UserIDAdded, Visible)
+VALUES (2, 'Senior Manager', 'SeniorManager', 0, NULL, GETDATE(), 'TLyde', 1)
+GO
+
+INSERT INTO tblParamProperty (ParamPropertyGroupID, LabelText, FieldName, Required, AllowedValues, DateAdded, UserIDAdded, Visible)
+VALUES (2, 'Policy Company Code', 'PolicyCompanyCode', 0, NULL, GETDATE(), 'TLyde', 1)
+GO
+
