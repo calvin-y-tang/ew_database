852823033aa520e47ab6f3b764fa7c53326c6cb8
jose.pais <jose.pais@examworks.com>
2022-02-02 09:59:22 -0500

    IMEC-12523 - modified business stored procs to include Param6 in result set.

--- IMECentricDB/Stored Procedures/spGetAllBusinessRules.sql
+++ IMECentricDB/Stored Procedures/spGetAllBusinessRules.sql
@@ -13,62 +13,60 @@ BEGIN
 
 	SET NOCOUNT ON
 
-
-	SELECT distinct * INTO #tmp_GetAllBusinessRules
+	SELECT DISTINCT * 
+	INTO #tmp_GetAllBusinessRules
 	FROM (
-	SELECT BR.BusinessRuleID, BR.Category, BR.Name,	 
-	 tmpBR.BusinessRuleConditionID,
-	 tmpBR.Param1,
-	 tmpBR.Param2,
-	 tmpBR.Param3,
-	 tmpBR.Param4,
-	 tmpBR.Param5,
-	 tmpBR.EntityType,
-	 tmpBR.ProcessOrder,
-	 tmpBR.Skip
-	FROM
-	(
-		SELECT 1 AS GroupID, BRC.*
-		FROM tblBusinessRuleCondition AS BRC
-			LEFT OUTER JOIN tblClient AS CL ON CL.ClientCode = @billClientCode
-			LEFT OUTER JOIN tblCompany AS CO ON CO.CompanyCode = CL.CompanyCode
-		WHERE 1=1
-			AND (BRC.BillingEntity IN (0,2))
-			AND (CASE BRC.EntityType WHEN 'PC' THEN CO.ParentCompanyID WHEN 'CO' THEN CO.CompanyCode WHEN 'CL' THEN CL.ClientCode ELSE 0 END = BRC.EntityID)
+			SELECT BR.BusinessRuleID, BR.Category, BR.Name,	 
+			 tmpBR.BusinessRuleConditionID,
+			 tmpBR.Param1,
+			 tmpBR.Param2,
+			 tmpBR.Param3,
+			 tmpBR.Param4,
+			 tmpBR.Param5,
+			 tmpBR.EntityType,
+			 tmpBR.ProcessOrder,
+			 tmpBR.Skip, 
+			 tmpBR.Param6
+			FROM
+				 (SELECT 1 AS GroupID, BRC.*
+					FROM tblBusinessRuleCondition AS BRC
+						LEFT OUTER JOIN tblClient AS CL ON CL.ClientCode = @billClientCode
+						LEFT OUTER JOIN tblCompany AS CO ON CO.CompanyCode = CL.CompanyCode
+					WHERE 1=1
+						AND (BRC.BillingEntity IN (0,2))
+						AND (CASE BRC.EntityType WHEN 'PC' THEN CO.ParentCompanyID WHEN 'CO' THEN CO.CompanyCode WHEN 'CL' THEN CL.ClientCode ELSE 0 END = BRC.EntityID)
 
-		UNION
+					UNION
 
-		SELECT 2 AS GroupID, BRC.*
-		FROM tblBusinessRuleCondition AS BRC
-			LEFT OUTER JOIN tblClient AS CL ON CL.ClientCode = @clientCode
-			LEFT OUTER JOIN tblCompany AS CO ON CO.CompanyCode = CL.CompanyCode
-		WHERE 1=1
-			AND (BRC.BillingEntity IN (1,2))
-			AND (CASE BRC.EntityType WHEN 'PC' THEN CO.ParentCompanyID WHEN 'CO' THEN CO.CompanyCode WHEN 'CL' THEN CL.ClientCode ELSE 0 END = BRC.EntityID)
+					SELECT 2 AS GroupID, BRC.*
+					FROM tblBusinessRuleCondition AS BRC
+						LEFT OUTER JOIN tblClient AS CL ON CL.ClientCode = @clientCode
+						LEFT OUTER JOIN tblCompany AS CO ON CO.CompanyCode = CL.CompanyCode
+					WHERE 1=1
+						AND (BRC.BillingEntity IN (1,2))
+						AND (CASE BRC.EntityType WHEN 'PC' THEN CO.ParentCompanyID WHEN 'CO' THEN CO.CompanyCode WHEN 'CL' THEN CL.ClientCode ELSE 0 END = BRC.EntityID)
 
-		UNION
+					UNION
 
-		SELECT 3 AS GroupID, BRC.*
-		FROM tblBusinessRuleCondition AS BRC
-		WHERE 1=1
-			AND (BRC.EntityType='SW')
-	) AS tmpBR
-		INNER JOIN tblBusinessRule AS BR ON BR.BusinessRuleID = tmpBR.BusinessRuleID
-		LEFT OUTER JOIN tblCaseType AS CT ON CT.Code = @caseType
-		LEFT OUTER JOIN tblServices AS S ON S.ServiceCode = @serviceCode
-	WHERE BR.IsActive=1
-		and BR.EventID=@eventID
-		AND (tmpBR.OfficeCode IS NULL OR tmpBR.OfficeCode = @officeCode)
-		AND (tmpBR.EWBusLineID IS NULL OR tmpBR.EWBusLineID = CT.EWBusLineID)
-		AND (tmpBR.EWServiceTypeID IS NULL OR tmpBR.EWServiceTypeID = S.EWServiceTypeID)
-		AND (tmpBR.Jurisdiction Is Null Or tmpBR.Jurisdiction = @jurisdiction)
-	
+					SELECT 3 AS GroupID, BRC.*
+					FROM tblBusinessRuleCondition AS BRC
+					WHERE 1=1
+						AND (BRC.EntityType='SW')
+				) AS tmpBR
+						INNER JOIN tblBusinessRule AS BR ON BR.BusinessRuleID = tmpBR.BusinessRuleID
+						LEFT OUTER JOIN tblCaseType AS CT ON CT.Code = @caseType
+						LEFT OUTER JOIN tblServices AS S ON S.ServiceCode = @serviceCode
+			WHERE BR.IsActive = 1
+				and BR.EventID = @eventID
+				AND (tmpBR.OfficeCode IS NULL OR tmpBR.OfficeCode = @officeCode)
+				AND (tmpBR.EWBusLineID IS NULL OR tmpBR.EWBusLineID = CT.EWBusLineID)
+				AND (tmpBR.EWServiceTypeID IS NULL OR tmpBR.EWServiceTypeID = S.EWServiceTypeID)
+				AND (tmpBR.Jurisdiction Is Null Or tmpBR.Jurisdiction = @jurisdiction)
 	) AS sortedBR	
 	ORDER BY sortedBR.BusinessRuleID, sortedBR.ProcessOrder
 
 	DELETE FROM #tmp_GetAllBusinessRules WHERE Skip = 1
 
 	SELECT * FROM #tmp_GetAllBusinessRules
-
 
 END
--- IMECentricDB/Stored Procedures/spGetBusinessRules.sql
+++ IMECentricDB/Stored Procedures/spGetBusinessRules.sql
@@ -13,60 +13,59 @@ BEGIN
 
 	SET NOCOUNT ON
 
-
 	DECLARE @groupDigits INT
-
 	SET @groupDigits = 10000000
 
-	SELECT * INTO #tmp_GetBusinessRules
+	SELECT * 
+	INTO #tmp_GetBusinessRules
 	FROM (
-	SELECT BR.BusinessRuleID, BR.Category, BR.Name,
-	 ROW_NUMBER() OVER (PARTITION BY BR.BusinessRuleID ORDER BY tmpBR.GroupID*@groupDigits+(CASE tmpBR.EntityType WHEN 'SW' THEN 4 WHEN 'PC' THEN 3 WHEN 'CO' THEN 2 WHEN 'CL' THEN 1 ELSE 9 END)*1000000+tmpBR.ProcessOrder) AS RowNbr,
-	 tmpBR.BusinessRuleConditionID,
-	 tmpBR.Param1,
-	 tmpBR.Param2,
-	 tmpBR.Param3,
-	 tmpBR.Param4,
-	 tmpBR.Param5,
-	 tmpBR.EntityType,
-	 tmpBR.ProcessOrder,
-	 tmpBR.Skip
-	FROM
-	(
-	SELECT 1 AS GroupID, BRC.*
-	FROM tblBusinessRuleCondition AS BRC
-	LEFT OUTER JOIN tblClient AS CL ON CL.ClientCode = @billClientCode
-	LEFT OUTER JOIN tblCompany AS CO ON CO.CompanyCode = CL.CompanyCode
-	WHERE 1=1
-	AND (BRC.BillingEntity IN (0,2))
-	AND (CASE BRC.EntityType WHEN 'PC' THEN CO.ParentCompanyID WHEN 'CO' THEN CO.CompanyCode WHEN 'CL' THEN CL.ClientCode ELSE 0 END = BRC.EntityID)
+		SELECT BR.BusinessRuleID, BR.Category, BR.Name,
+		 ROW_NUMBER() OVER (PARTITION BY BR.BusinessRuleID ORDER BY tmpBR.GroupID*@groupDigits+(CASE tmpBR.EntityType WHEN 'SW' THEN 4 WHEN 'PC' THEN 3 WHEN 'CO' THEN 2 WHEN 'CL' THEN 1 ELSE 9 END)*1000000+tmpBR.ProcessOrder) AS RowNbr,
+		 tmpBR.BusinessRuleConditionID,
+		 tmpBR.Param1,
+		 tmpBR.Param2,
+		 tmpBR.Param3,
+		 tmpBR.Param4,
+		 tmpBR.Param5,
+		 tmpBR.EntityType,
+		 tmpBR.ProcessOrder,
+		 tmpBR.Skip, 
+		 tmpBR.Param6
+		FROM
+			(SELECT 1 AS GroupID, BRC.*
+			   FROM tblBusinessRuleCondition AS BRC
+					LEFT OUTER JOIN tblClient AS CL ON CL.ClientCode = @billClientCode
+					LEFT OUTER JOIN tblCompany AS CO ON CO.CompanyCode = CL.CompanyCode
+			  WHERE 1=1
+			    AND (BRC.BillingEntity IN (0,2))
+			    AND (CASE BRC.EntityType WHEN 'PC' THEN CO.ParentCompanyID WHEN 'CO' THEN CO.CompanyCode WHEN 'CL' THEN CL.ClientCode ELSE 0 END = BRC.EntityID)
 
-	UNION
+			UNION
 
-	SELECT 2 AS GroupID, BRC.*
-	FROM tblBusinessRuleCondition AS BRC
-	LEFT OUTER JOIN tblClient AS CL ON CL.ClientCode = @clientCode
-	LEFT OUTER JOIN tblCompany AS CO ON CO.CompanyCode = CL.CompanyCode
-	WHERE 1=1
-	AND (BRC.BillingEntity IN (1,2))
-	AND (CASE BRC.EntityType WHEN 'PC' THEN CO.ParentCompanyID WHEN 'CO' THEN CO.CompanyCode WHEN 'CL' THEN CL.ClientCode ELSE 0 END = BRC.EntityID)
+			SELECT 2 AS GroupID, BRC.*
+			  FROM tblBusinessRuleCondition AS BRC
+					LEFT OUTER JOIN tblClient AS CL ON CL.ClientCode = @clientCode
+					LEFT OUTER JOIN tblCompany AS CO ON CO.CompanyCode = CL.CompanyCode
+			  WHERE 1=1
+			    AND (BRC.BillingEntity IN (1,2))
+			    AND (CASE BRC.EntityType WHEN 'PC' THEN CO.ParentCompanyID WHEN 'CO' THEN CO.CompanyCode WHEN 'CL' THEN CL.ClientCode ELSE 0 END = BRC.EntityID)
 
-	UNION
+			UNION
 
-	SELECT 3 AS GroupID, BRC.*
-	FROM tblBusinessRuleCondition AS BRC
-	WHERE 1=1
-	AND (BRC.EntityType='SW')
-	) AS tmpBR
-	INNER JOIN tblBusinessRule AS BR ON BR.BusinessRuleID = tmpBR.BusinessRuleID
-	LEFT OUTER JOIN tblCaseType AS CT ON CT.Code = @caseType
-	LEFT OUTER JOIN tblServices AS S ON S.ServiceCode = @serviceCode
-	WHERE BR.IsActive=1
-	and BR.EventID=@eventID
-	AND (tmpBR.OfficeCode IS NULL OR tmpBR.OfficeCode = @officeCode)
-	AND (tmpBR.EWBusLineID IS NULL OR tmpBR.EWBusLineID = CT.EWBusLineID)
-	AND (tmpBR.EWServiceTypeID IS NULL OR tmpBR.EWServiceTypeID = S.EWServiceTypeID)
-	AND (tmpBR.Jurisdiction Is Null Or tmpBR.Jurisdiction = @jurisdiction)
+			SELECT 3 AS GroupID, BRC.*
+			  FROM tblBusinessRuleCondition AS BRC
+			 WHERE 1=1
+			   AND (BRC.EntityType='SW')
+		) AS tmpBR
+			INNER JOIN tblBusinessRule AS BR ON BR.BusinessRuleID = tmpBR.BusinessRuleID
+			LEFT OUTER JOIN tblCaseType AS CT ON CT.Code = @caseType
+			LEFT OUTER JOIN tblServices AS S ON S.ServiceCode = @serviceCode
+		WHERE BR.IsActive=1
+		  AND BR.EventID=@eventID
+		  AND (tmpBR.OfficeCode IS NULL OR tmpBR.OfficeCode = @officeCode)
+		  AND (tmpBR.EWBusLineID IS NULL OR tmpBR.EWBusLineID = CT.EWBusLineID)
+		  AND (tmpBR.EWServiceTypeID IS NULL OR tmpBR.EWServiceTypeID = S.EWServiceTypeID)
+		  AND (tmpBR.Jurisdiction Is Null Or tmpBR.Jurisdiction = @jurisdiction)
 	) AS sortedBR
 	WHERE sortedBR.RowNbr=1
 	ORDER BY sortedBR.BusinessRuleID
