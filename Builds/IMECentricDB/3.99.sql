53693157150bde4ea01a5a62d605c75a4b95908b
Calvin Tang <calvin.tang@examworks.com>
2022-02-03 00:18:11 -0500

    Revised script for Sprint 77 DB Release

--- Builds/IMECentricDB/3.99.sql
+++ Builds/IMECentricDB/3.99.sql
@@ -695,11 +695,6 @@ PRINT N'Update complete.';
 GO
 
 -- IMEC-12318 - parent company patch existing data
-UPDATE IMECentricMaster.dbo.EWParentCompany
-SET RequireInOutNetworkInvoice = ISNULL(RequireInOutNetworkInvoice, 0), 
-    RequireOutofNetworkReason = ISNULL(RequireOutofNetworkReason, 0)
-GO
-
 UPDATE tblEWParentCompany
 SET RequireInOutNetworkInvoice = ISNULL(RequireInOutNetworkInvoice, 0), 
     RequireOutofNetworkReason = ISNULL(RequireOutofNetworkReason, 0)
