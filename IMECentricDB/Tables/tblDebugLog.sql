ec824b4a3b2e418db119051e9295cb4b8428db5a
Terri Lyde <terri.lyde@examworks.com>
2022-02-03 20:32:33 -0500

    IMEC-12467 - remove tabe tblDebugLog to use tblLog

--- IMECentricDB/Tables/tblDebugLog.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[tblDebugLog] (
-    [SessionID]      VARCHAR (50)  NOT NULL,
-    [ModuleName]     VARCHAR (50)  NULL,
-    [DateAdded]      DATETIME      NULL,
-    [UserID]         VARCHAR (50)  NULL,
-    [DebugMessage]   VARCHAR (200) NULL,
-    [PrimaryKey]     INT           IDENTITY (1, 1) NOT NULL,
-    CONSTRAINT [PK_tblDebugLog] PRIMARY KEY CLUSTERED ([PrimaryKey] ASC)
-);
