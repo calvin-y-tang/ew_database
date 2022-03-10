fafe4b6113b516076e9b8304c1e5f041c423b46c
jose.pais <jose.pais@examworks.com>
2022-03-10 15:06:50 -0500

    IMEC-12558 - add new security token for doc/rpt delete

---
+++ IMECentricDB/Data/NextRelease.sql
@@ -0,0 +1,8 @@
+
+-- IMEC-12558 - add new security token
+INSERT INTO tblUserFunction(FunctionCode, FunctionDesc, DateAdded)
+VALUES('CaseDocRptDelete', 'Case - Delete Doc/Rpt', GetDate())
+GO
+
+
+
