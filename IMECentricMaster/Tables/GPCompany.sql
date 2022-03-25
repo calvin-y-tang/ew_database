08bcc9ee36cadd9fa878e65bea7a00b002776917
Sam Chiang <scdev@icloud.com>
2022-03-25 16:13:18 -0400

    Revert "Merge branch 'scdev' into develop" for IMEC-12652
    This reverts commit 06a83cfe0213c9cdced86397e242531a680458fc.

--- IMECentricMaster/Tables/GPCompany.sql
+++ IMECentricMaster/Tables/GPCompany.sql
@@ -19,7 +19,7 @@
     [ParentCompanyID] INT           NULL,
     [SourceID]        INT           NULL,
     [AcctingEmail]    VARCHAR (70)  NULL,
-    [EWCompanyTypeID] INT           NULL,
+    [EWCompanyTypeID] VARCHAR (70)  NULL,
     CONSTRAINT [PK_GPCompany] PRIMARY KEY CLUSTERED ([PrimaryKey] ASC)
 );
 
