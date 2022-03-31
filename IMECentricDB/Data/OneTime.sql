c17eb4772f448beece4571ef1a510f6b35a496df
william.cecil <William.Cecil@examworks.com>
2022-03-31 11:48:17 -0400

    Sprint 81 General Release

--- IMECentricDB/Data/OneTime.sql
+++ IMECentricDB/Data/OneTime.sql
@@ -1,12 +1,4 @@
-﻿-- Database: EW_IME_CENTRIC
--- Catalog : EWDataRepository
--- Table   : AcctHeader
--- Issue   : IMEC-12600 Add new column for CaseDocID
---
-USE EWDataRepository
-ALTER TABLE AcctHeader ADD CaseDocID INT NULL
-GO
-
+﻿--Sprint 81 DB Script Changes
 
 -- Database: EW_IME_CENTRIC
 -- Catalog : IMECentricMaster
