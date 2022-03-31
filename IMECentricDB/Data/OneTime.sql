a8ef224e3d76fb340f523002b8f9a97092691fdd
Sam Chiang <sam.chiang@examworks.com>
2022-03-31 16:27:32 -0400

    Clearing out OneTime script for new sprint

--- IMECentricDB/Data/OneTime.sql
+++ IMECentricDB/Data/OneTime.sql
@@ -1,9 +1,1 @@
-﻿-- Database: EW_IME_CENTRIC
--- Catalog : EWDataRepository
--- Table   : AcctHeader
--- Issue   : IMEC-12600 Add new column for CaseDocID
---
-USE EWDataRepository
-ALTER TABLE AcctHeader ADD CaseDocID INT NULL
-GO
-
+﻿
