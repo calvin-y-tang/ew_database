19487020882d20730a8a326336fac27c0f9202c9
jose.pais <jose.pais@examworks.com>
2022-04-05 10:13:04 -0400

    IMEC-12659 - new tblTaxExemptCompanyEmployer table added.

---
+++ IMECentricDB/Tables/tblTaxExemptCompanyEmployer.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[tblTaxExemptCompanyEmployer]
+(
+    [TaxExemptCompanyEmployerID] INT IDENTITY (1, 1) NOT NULL,
+    [ParentCompanyID]            INT                 NOT NULL,
+    [EWParentEmployerID]         INT                 NOT NULL, 
+    [StateCode]                  VARCHAR(2)          NULL,
+    [DateAdded]                  DATETIME            NOT NULL,
+    [UserIDAdded]                VARCHAR (15)        NOT NULL,
+    CONSTRAINT [PK_tblTaxExemptCompanyEmployer] PRIMARY KEY CLUSTERED ([TaxExemptCompanyEmployerID] ASC)
+)
+GO
+
+CREATE UNIQUE INDEX [IX_U_tblTaxExemptCompanyEmployer_ParentCompanyID_EWParentEmployerID_StateCode] 
+	ON [dbo].[tblTaxExemptCompanyEmployer] ([ParentCompanyID], [EWParentEmployerID], [StateCode])
+GO
