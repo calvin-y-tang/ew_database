aad67b4dfb187e5b70e92bb29b20f42b99992f68
Sam Chiang <sam.chiang@examworks.com>
2022-04-06 05:54:09 -0400

    IMEC-12653 - Changes to support EWCompanyTypeID
    IMEC-12653 changes
    
    
    IMEC-12652 - Fixed data type on new EWCompanyTypeID column

--- IMECentricMaster/Tables/GPCompany.sql
+++ IMECentricMaster/Tables/GPCompany.sql
@@ -19,6 +19,7 @@
     [ParentCompanyID] INT           NULL,
     [SourceID]        INT           NULL,
     [AcctingEmail]    VARCHAR (70)  NULL,
+    [EWCompanyTypeID] INT           NULL,
     CONSTRAINT [PK_GPCompany] PRIMARY KEY CLUSTERED ([PrimaryKey] ASC)
 );
 
