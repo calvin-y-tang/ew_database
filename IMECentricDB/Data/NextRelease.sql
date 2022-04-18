2e32eac91bc38e4f7288282a9a66702983e51b6b
jose.pais <jose.pais@examworks.com>
2022-04-18 14:46:13 -0400

    Sprint 83 Reset

--- IMECentricDB/Data/NextRelease.sql
+++ IMECentricDB/Data/NextRelease.sql
@@ -1,10 +1,2 @@
 
--- Sprint 82
-
--- IMEC-11749 - TeamMembers security token and set URL for WebCompany
-INSERT INTO tblUserFunction
-VALUES('TeamMembers','Web Portal - Maintain Team Members', getDate())
-GO
--- IMEC-11749
-UPDATE tblWebCompany SET TeamMembersURL = 'https://portaladmin.examworks.com/login/tokenlogin?token=' 
-GO
+-- Sprint 83
