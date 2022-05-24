7936fb185893a3763e96debc73d0adc57f6e025b
Doug Troy <doug.troy@examworks.com>
2022-05-24 09:35:47 -0400

    IMEC-12527 - added update statement for mapping 5 in EWFlashCategory table

--- IMECentricDB/Data/OneTime.sql
+++ IMECentricDB/Data/OneTime.sql
@@ -1,2 +1,13 @@
 ﻿
 -- Sprint 85
+
+-- Database: EW_IME_CENTRIC
+-- Catalog : IMECentricMaster
+-- Table   : EWFlashCategory 
+-- Issue   : IMEC-12527 - Add milege to Hartford Mgt. Report
+
+USE [IMECentricMaster]
+GO
+
+UPDATE EWFlashCategory SET Mapping5 = 'Mileage' WHERE EWFlashCategoryID = 290 and Category = 'Mileage';
+GO
