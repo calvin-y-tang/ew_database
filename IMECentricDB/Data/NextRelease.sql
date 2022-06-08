797a58e52d9cf7fdbc7549352678739d3fa55c7a
jose.pais <jose.pais@examworks.com>
2022-06-08 15:06:21 -0400

    IMEC=12084 - new setting for PDF Printing/Merging

--- IMECentricDB/Data/NextRelease.sql
+++ IMECentricDB/Data/NextRelease.sql
@@ -5,4 +5,8 @@
 DELETE FROM tblSetting WHERE Name = 'UseARCHClaimInfo'
 GO
 
-
+-- IMEC=12084 - new setting for PDF Printing/Merging
+INSERT INTO tblSetting(Name, Value)
+VALUES('PDFProcessingTimeout', '45'), 
+      ('PDFPrintMethod','QuickPDF')
+GO
