e3050f5d95b615f9aedf518549a9c53763fd0c57
Calvin Tang <calvin.tang@examworks.com>
2022-06-20 12:05:20 -0400

    IMEC-12525 - New security token and folder path

--- IMECentricDB/Data/NextRelease.sql
+++ IMECentricDB/Data/NextRelease.sql
@@ -1,12 +1,34 @@
 
--- Sprint 86
+-- Sprint 87
 
--- IMEC-12772 remove arch claim setting
-DELETE FROM tblSetting WHERE Name = 'UseARCHClaimInfo'
+INSERT INTO tblUserFunction
+(
+    FunctionCode,
+    FunctionDesc,
+    DateAdded
+)
+VALUES
+(   'DicomExtractor',
+    'DicomExtractor - Run',
+    GETDATE()
+    )
 GO
 
--- IMEC=12084 - new setting for PDF Printing/Merging
-INSERT INTO tblSetting(Name, Value)
-VALUES('PDFProcessingTimeout', '45'), 
-      ('PDFPrintMethod','QuickPDF')
+
+INSERT tblSetting
+(
+    Name,
+    Value
+)
+VALUES
+(   'UseRibbonDefault',
+    'False'
+    )
 GO
+
+UPDATE tblControl SET DirIcon='\\IMECDocs5.ew.domain.local\IMECentricDocs\Icon\'
+UPDATE tblControl SET DirDicomExtractor='\\EWISApp1.ew.domain.local\Deploy\DicomExtractor\'
+GO
+
+
+
