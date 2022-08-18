f83acc822c757be0aaa1b565c9b0ef304e943dd2
jose.pais <jose.pais@examworks.com>
2022-08-18 17:11:11 -0400

    IMEC-12913 - move from nextrelease to onetime

--- IMECentricDB/Data/NextRelease.sql
+++ IMECentricDB/Data/NextRelease.sql
@@ -12,7 +12,3 @@ INSERT INTO tblUserFunction (FunctionCode, FunctionDesc, DateAdded)
 VALUES('DoctorCRNView','Doctor - View in CRN', GETDATE()), 
       ('DoctorCRNAdd', 'Doctor - Add CRN Reviewer', GETDATE())
 GO
--- IMEC-12913 - configuration to access CRN system (TEST SYSTEM ONLY)
-INSERT INTO tblConfiguration (ConfigurationID, Name, Active, ConfigurationType, DateEdited, UserIDEdited, Param)
-VALUES(11, 'CRN', 1, 'CRN', GETDATE(), 'JPais', 'BU=EW;URLView=https://crn-alpha-develop-staff.examworks.com/master-reviewer-detail/<MasterReviewerID>;URLAdd=https://crn-alpha-develop-staff.examworks.com/new-external-request?sn=<BUCode>')
-GO
