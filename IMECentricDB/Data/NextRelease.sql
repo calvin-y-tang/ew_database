10966867be2ac13a0aa5f06f61f441a3410c4e2f
jose.pais <jose.pais@examworks.com>
2022-08-18 14:34:03 -0400

    IMEC-12913 - tblSetting, tblUserFunction, tblConfiguration items for CRN Integration.

--- IMECentricDB/Data/NextRelease.sql
+++ IMECentricDB/Data/NextRelease.sql
@@ -2,3 +2,17 @@
 
 -- IMEC-12959 - Add status to tblOutOfNetworkReason, set all existing to active
 update tblOutOfNetworkReason set Status = 'Active'
+
+-- IMEC-12913 - new setting that makes CRN Active
+INSERT INTO tblSetting
+VALUES ('CRNIsActive', 'False')
+GO
+-- IMEC-12913 - new CRN security settings
+INSERT INTO tblUserFunction (FunctionCode, FunctionDesc, DateAdded)
+VALUES('DoctorCRNView','Doctor - View in CRN', GETDATE()), 
+      ('DoctorCRNAdd', 'Doctor - Add CRN Reviewer', GETDATE())
+GO
+-- IMEC-12913 - configuration to access CRN system (TEST SYSTEM ONLY)
+INSERT INTO tblConfiguration (ConfigurationID, Name, Active, ConfigurationType, DateEdited, UserIDEdited, Param)
+VALUES(11, 'CRN', 1, 'CRN', GETDATE(), 'JPais', 'BU=EW;URLView=https://crn-alpha-develop-staff.examworks.com/master-reviewer-detail/<MasterReviewerID>;URLAdd=https://crn-alpha-develop-staff.examworks.com/new-external-request?sn=<BUCode>')
+GO
