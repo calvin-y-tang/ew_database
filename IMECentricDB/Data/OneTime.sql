66164a9900e4abe84c0db3b448173f45c26a93bb
Sam Chiang <sam.chiang@examworks.com>
2022-08-30 12:42:46 -0400

    IMEC-12984 - Added new setting in tblSetting table for extended CommandTimeout

--- IMECentricDB/Data/OneTime.sql
+++ IMECentricDB/Data/OneTime.sql
@@ -1,4 +1,15 @@
 ﻿
 -- Sprint 92
 
+-- IMEC-12984
+-- Adding new setting to tblSetting to handle database context CommandTimeout value
+-- Dev: Sam Chiang
+USE [IMECentricEW]
+GO
 
+INSERT INTO dbo.tblSetting (Name, Value)
+VALUES (
+'DBConnCommandTimeoutExtended', 
+'120' 
+)
+GO
