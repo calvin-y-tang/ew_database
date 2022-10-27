e3e0f810ab3eb0ae7c5d22e874ca2465d2ad949c
jose.pais <jose.pais@examworks.com>
2022-10-27 13:28:13 -0400

    Revert "IMEC-13145 Data patching for CalcForExam."
    This reverts commit 5442efd478b8634e0d5aa04486250fa20242f382.

--- IMECentricDB/Data/NextRelease.sql
+++ IMECentricDB/Data/NextRelease.sql
@@ -7,11 +7,10 @@ GO
 -- IMEC-13145  - create new tblCodes Entry for SLA Calc For Exam values
 INSERT INTO tblCodes(Category, SubCategory, Value)
 VALUES('SLARuleAppt', 'First Scheduled Exam', '10'),
-	  ('SLARuleAppt', 'Subsequent Scheduled Exams', '20'),
-	  ('SLARuleAppt', 'All Exams', '30'),
-	  ('SLARuleAppt', 'Not Applicable', '40')
+	    ('SLARuleAppt', 'Subsequent Scheduled Exams', '20'),
+	    ('SLARuleAppt', 'All Exams', '30'),
+	    ('SLARuleAppt', 'Not Applicable', '40')
 GO
-
 -- IMEC-13145 patch existing data
 UPDATE TCM
 	   SET isApptBased = 1
@@ -25,9 +24,3 @@ UPDATE TCM
 FROM tblTATCalculationMethod AS TCM  
 WHERE isApptBased is null
 GO 
-UPDATE SLA 
-   SET CalculateForExam = IIF(TAT.IsApptBased = 1, 30, 40)
-FROM tblSLARuleDetail AS SLA
-          INNER JOIN tblTATCalculationMethod AS TAT ON TAT.TATCalculationMethodID = SLA.TATCalculationMethodID
-WHERE SLA.CalculateForExam IS NULL
-GO
