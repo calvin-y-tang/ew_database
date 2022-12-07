ba318e92fef34ff6631f2d92b624c593ee967813
jose.pais <jose.pais@examworks.com>
2022-12-07 11:51:34 -0500

    IMEC-13222 - modify business rule 155 to allow for EWFacility matching and override

--- IMECentricDB/Data/NextRelease.sql
+++ IMECentricDB/Data/NextRelease.sql
@@ -1,1 +1,11 @@
 -- Sprint 99
+
+-- IMEC-13222 - modify business rule to allow for EWFacility matching and override
+UPDATE tblBusinessRule
+   SET Param3Desc = 'EWFacIDNormTaxes'
+ WHERE BusinessRuleID = 155
+ GO
+UPDATE tblBusinessRuleCondition
+   SET Param3 = ';5;'
+ WHERE BusinessRuleID = 155
+ GO
