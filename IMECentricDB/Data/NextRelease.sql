119f874c0ad1d4519c13ece87754122b52bd139e
jose.pais <jose.pais@examworks.com>
2023-01-18 08:15:22 -0500

    IMEC-13314 - correct conditions on new business rule

--- IMECentricDB/Data/NextRelease.sql
+++ IMECentricDB/Data/NextRelease.sql
@@ -19,7 +19,7 @@
       GO
      -- add new business rules
      INSERT INTO tblBusinessRuleCondition(BusinessRuleID, EntityType, EntityID, BillingEntity, ProcessOrder, DateAdded, UserIDAdded, DateEdited, UserIDEdited, OfficeCode, EWBusLineID, EWServiceTypeID, Jurisdiction, Param1, Param2, Param3, Param4, Param5, Skip, Param6)
-     VALUES (104, 'PC', 25, 2, 2, GETDATE(), 'Admin', GETDATE(), 'Admin', NULL, 3, NULL, NULL, '002456', NULL, '55453, 59557', 'First Transit or First Student', NULL, 0, NULL)
+     VALUES (104, 'PC', 25, 2, 2, GETDATE(), 'Admin', GETDATE(), 'Admin', NULL, 3, NULL, NULL, '002456', NULL, '55453,59557', 'First Transit or First Student', NULL, 0, NULL)
      GO
 
 
