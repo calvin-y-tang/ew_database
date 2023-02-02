1beb8ab1bc31d9851ffedc42809ddd32e2e36a91
jose.pais <jose.pais@examworks.com>
2023-02-02 15:40:13 -0500

    IMEC-13360 -- need to remove security token to ensure it is not going to be used.

--- IMECentricDB/Data/NextRelease.sql
+++ IMECentricDB/Data/NextRelease.sql
@@ -1,4 +1,8 @@
 -- Sprint 103
 
-
+-- IMEC-13360 -- need to remove security token to ensure it is not going to be used.
+DELETE FROM tblUserFunction WHERE FunctionCode = 'DoctorSpecialtyEdit'
+GO
+DELETE FROM tblGroupFunction WHERE FunctionCode = 'DoctorSpecialtyEdit'
+GO
 
