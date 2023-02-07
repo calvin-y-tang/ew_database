9a7b6287a62d5accf17204ab92cb4c2f68e031ad
jose.pais <jose.pais@examworks.com>
2023-02-07 13:18:36 -0500

    Sprint 104 reset

--- IMECentricDB/Data/NextRelease.sql
+++ IMECentricDB/Data/NextRelease.sql
@@ -1,8 +1,1 @@
--- Sprint 103
-
--- IMEC-13360 -- need to remove security token to ensure it is not going to be used.
-DELETE FROM tblUserFunction WHERE FunctionCode = 'DoctorSpecialtyEdit'
-GO
-DELETE FROM tblGroupFunction WHERE FunctionCode = 'DoctorSpecialtyEdit'
-GO
-
+-- Sprint 104
