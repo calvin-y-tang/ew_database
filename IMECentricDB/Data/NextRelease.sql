a77a07626cbe1a32133ad2883b2806ea6116a416
jose.pais <jose.pais@examworks.com>
2023-02-10 14:23:03 -0500

    IMEC-13365 - update AllowToPublishOnWeb values.

--- IMECentricDB/Data/NextRelease.sql
+++ IMECentricDB/Data/NextRelease.sql
@@ -1,1 +1,7 @@
 -- Sprint 104
+
+-- IMEC-13365 - only allow CV Dr Document Type to be set for Publish to Web
+UPDATE tblEWDrDocType
+   SET AllowPublishOnWeb = 0
+ WHERE EWDrDocTypeID <> 5
+GO
--- IMECentricDB/Data/OneTime.sql
+++ IMECentricDB/Data/OneTime.sql
@@ -1,1 +1,9 @@
 ﻿-- Sprint 104
+
+-- IMEC-13365 - only allow CV Dr Document Type to be set for Publish to Web
+USE IMECentricMaster 
+GO
+ UPDATE EWDrDocType
+   SET AllowPublishOnWeb = 0
+ WHERE EWDrDocTypeID <> 5
+GO
