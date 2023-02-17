7880ee15d4dc3ec23e0423b682c3dc7d99fc81db
jose.pais <jose.pais@examworks.com>
2023-02-17 09:37:01 -0500

    -- IMEC-13402 - add new Doctor Document Type and update EWDrDocType Schema

--- IMECentricDB/Data/OneTime.sql
+++ IMECentricDB/Data/OneTime.sql
@@ -4,8 +4,19 @@
 USE IMECentricMaster 
 GO
  UPDATE EWDrDocType
+   SET AllowPublishOnWeb = 1
+ WHERE EWDrDocTypeID = 5
+GO
+ UPDATE EWDrDocType
    SET AllowPublishOnWeb = 0
  WHERE EWDrDocTypeID <> 5
+GO
+
+-- IMEC-13402 - add new Doctor Document Type and update EWDrDocType Schema
+USE IMECentricMaster 
+GO
+INSERT INTO EWDrDocType(Name, Confidential, AllowEdit, SeqNo, AllowState, AllowExpireDate, AllowSpecialty, AllowLicenseNbr, AllowAccreditation, AllowPublishOnWeb)
+VALUES('CV Redacted', 0, 1, 21, 2, 1, 2, 2, 2, 1)
 GO
 
 -- IMEC-13363 - CRN - need to increase SpecialtyCode (and Description) to VARCHAR(500)
@@ -13,6 +24,7 @@ USE IMECentricMaster
 GO
 ALTER TABLE EWDoctorDocument ALTER COLUMN SpecialtyCode VARCHAR (500) NULL
 GO
+
 ALTER TABLE EWSpecialty ALTER COLUMN SpecialtyCode VARCHAR (500) NULL
 GO
 ALTER TABLE EWSpecialty ALTER COLUMN  Description VARCHAR (500) NULL
@@ -28,5 +40,6 @@ ALTER TABLE NDBSpecialtyMapping ALTER COLUMN FromSpecialtyCode VARCHAR(500) NOT 
 GO
 ALTER TABLE NDBSpecialtyMapping ALTER COLUMN ToSpecialtyCode VARCHAR(500) NULL 
 GO
+
 ALTER TABLE EWDoctorExt ALTER COLUMN Specialties VARCHAR(3000) NULL
 GO
