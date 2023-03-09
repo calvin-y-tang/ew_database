b8b1d433c67bb863208398b06e4c545f44deb0b4
jose.pais <jose.pais@examworks.com>
2023-03-09 10:01:45 -0500

    IMEC-13441 - moved from OneTime to here, where it belongs...

--- IMECentricDB/Data/NextRelease.sql
+++ IMECentricDB/Data/NextRelease.sql
@@ -1,1 +1,6 @@
 -- Sprint 106
+
+-- IMEC-13441 - creae new business rule condition for Claim Nbr requires Employer 
+INSERT INTO tblBusinessRuleCondition(BusinessRuleID, EntityType, EntityID, BillingEntity, ProcessOrder, DateAdded, UserIDAdded, DateEdited, UserIDEdited, OfficeCode, EWBusLineID, EWServiceTypeID, Jurisdiction, Param1, Param2, Param3, Param4, Param5, Skip, Param6)
+VALUES (104, 'PC', 25, 2, 2, GETDATE(), 'Admin', GETDATE(), 'Admin', NULL, 3, NULL, NULL, '015557', NULL, '55453,59557', 'First Transit or First Student', NULL, 0, NULL)
+GO
