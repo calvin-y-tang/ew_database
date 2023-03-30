d5bc2d1d4e80457e84c26e0708fe65cfadc57c25
jose.pais <jose.pais@examworks.com>
2023-03-30 16:05:12 -0400

    IMEC-13437 patch existing tblDoctor data for new columns

--- IMECentricDB/Data/OneTime.sql
+++ IMECentricDB/Data/OneTime.sql
@@ -1,2 +1,31 @@
 ﻿-- Sprint 107
 
+-- IMEC-13437 patch existing tblDoctor data for new columns
+USE IMECentricEW
+GO
+UPDATE dr
+  SET CRNStatus = IIF(activeCnt.cnt IS NULL, dr.status, 'Active'),
+      SavedStatus = dr.Status
+ FROM tblDoctor as dr
+          LEFT OUTER JOIN (SELECT COUNT(*) AS cnt, EWParentDocID, Status
+                             FROM tblDoctor 
+                             WHERE EWParentDocID IS NOT NULL
+                               AND Status = 'Active'
+                            GROUP BY EWParentDocID, Status) AS activeCnt ON activeCnt.EWParentDocID = dr.EWParentDocID
+WHERE OPType = 'DR' 
+GO
+
+USE IMECentricFCE 
+GO 
+UPDATE dr
+  SET CRNStatus = IIF(activeCnt.cnt IS NULL, dr.status, 'Active'),
+      SavedStatus = dr.Status
+ FROM tblDoctor as dr
+          LEFT OUTER JOIN (SELECT COUNT(*) AS cnt, EWParentDocID, Status
+                             FROM tblDoctor 
+                             WHERE EWParentDocID IS NOT NULL
+                               AND Status = 'Active'
+                            GROUP BY EWParentDocID, Status) AS activeCnt ON activeCnt.EWParentDocID = dr.EWParentDocID
+WHERE OPType = 'DR' 
+GO
+
