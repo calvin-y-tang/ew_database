b07bbea0a9a38f9e4d07c07b316a28ac1e3b3c5c
jose.pais <jose.pais@examworks.com>
2023-04-06 16:22:50 -0400

    IMEC-11792 - corrected schema to match defined data model.

--- IMECentricDB/Tables/tblIntegrationBatchDetail.sql
+++ IMECentricDB/Tables/tblIntegrationBatchDetail.sql
@@ -1,11 +1,12 @@
 CREATE TABLE [dbo].[tblIntegrationBatchDetail] (
     [PrimaryKey]             INT             IDENTITY (1, 1) NOT NULL,
-	[ProcessName]            VARCHAR(50)     NULL,
+	[ProcessName]            VARCHAR(25)     NULL,
     [BatchNbr]               INT             NOT NULL,
+	[TableType]              VARCHAR(50)     NULL,
+    [TableKey]               INT             NOT NULL,
+	[Param]                  VARCHAR(200)    NULL,
     [DateAdded]              DATETIME        NULL,
-    [UserIDAdded]            VARCHAR (50)    NULL,
-    [DateProcessed]          DATETIME        NULL,
-    [BatchStatus]            VARCHAR (50)    NULL,
+    [UserIDAdded]            VARCHAR (15)    NULL,
     CONSTRAINT [PK_tblIntegrationBatchDetail] PRIMARY KEY CLUSTERED ([PrimaryKey] ASC)
 );
 
