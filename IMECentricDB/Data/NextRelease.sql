82be2d947c08a00d83433fb790996922f56fccbf
jose.pais <jose.pais@examworks.com>
2023-04-11 08:39:20 -0400

    IMEC-13482 - add DoctorSpecialtyEdit secutiry token

--- IMECentricDB/Data/NextRelease.sql
+++ IMECentricDB/Data/NextRelease.sql
@@ -1,1 +1,8 @@
 -- Sprint 108
+
+
+-- IMEC- 13482new security token
+INSERT INTO tblUserFunction (FunctionCode, FunctionDesc, DateAdded)
+VALUES('DoctorSpecialtyEdit','Doctor - Modify Attached Specialties', GETDATE())
+GO
+
