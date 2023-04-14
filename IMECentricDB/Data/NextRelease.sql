d717c121e5ac0af8e30ffc7c66092434ef630216
jose.pais <jose.pais@examworks.com>
2023-04-14 19:39:29 -0400

    IMEC-13482 - hold off on deploying new security token

--- IMECentricDB/Data/NextRelease.sql
+++ IMECentricDB/Data/NextRelease.sql
@@ -1,8 +1,4 @@
 -- Sprint 108
 
 
--- IMEC- 13482new security token
-INSERT INTO tblUserFunction (FunctionCode, FunctionDesc, DateAdded)
-VALUES('DoctorSpecialtyEdit','Doctor - Modify Attached Specialties', GETDATE())
-GO
 
