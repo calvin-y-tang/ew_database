7702d8c433df824a801990b9a62947ed6c0f5e3e
Sam Chiang <sam.chiang@examworks.com>
2023-04-21 10:11:15 -0400

    IMEC-13461,13463 - add table create script to the onetime script file to be use for deployment

--- IMECentricDB/Data/OneTime.sql
+++ IMECentricDB/Data/OneTime.sql
@@ -1,1 +1,66 @@
 ﻿-- Sprint 109
+
+
+-- // new table in EWData Repository - to handle the InvoiceUpdate.txt file for the DR process
+USE [EWDataRepository]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[AcctInvoiceUpdate](
+	[AcctInvoiceUpdateID] [INT] IDENTITY(1,1) NOT NULL,
+	[FormatVersion] [INT] NULL,
+	[ExportDate] [DATETIME] NULL,
+	[SourceID] [INT] NULL,
+	[InvoiceNo] [VARCHAR](15) NULL,
+	[EWFacilityID] [INT] NULL,
+	[CompanyID] [VARCHAR](11) NULL,
+	[OldClientID] [VARCHAR](15) NULL,
+	[NewClientID] [VARCHAR](15) NULL,
+	[ClaimNo] [VARCHAR](50) NULL,
+	[CaseNo] [VARCHAR](15) NULL,
+	[BatchNo] [INT] NULL,
+	[EventDate] [DATETIME] NULL,
+	[EventType] [INT] NULL,
+	[GPExportStatus] [INT] NULL,
+	CONSTRAINT [PK_AcctInvoiceUpdate] PRIMARY KEY CLUSTERED ([AcctInvoiceUpdateID] ASC)
+)
+GO
+
+
+
+-- // new table in IMECentricMaster - to handle the data from AcctInvoiceUpdate for the DR-GP process
+USE [IMECentricMaster]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[GPInvoiceUpdate](
+	[PrimaryKey] [int] IDENTITY(1,1) NOT NULL,
+	[FormatVersion] [int] NULL,
+	[ProcessedFlag] [BIT] NOT NULL,
+	[ExportDate] [datetime] NULL,
+	[SourceID] [int] NULL,
+	[InvoiceNo] [varchar](15) NULL,
+	[EWFacilityID] [int] NULL,
+	[GPFacilityID] [varchar](3) NULL,
+	[CompanyID] [varchar](11) NULL,
+	[GPCustomerID] [varchar](15) NULL,
+	[OldClientID] [varchar](15) NULL,
+	[NewClientID] [varchar](15) NULL,
+	[ClaimNo] [varchar](50) NULL,
+	[CaseNo] [varchar](15) NULL,
+	[BatchNo] [varchar](15) NULL,
+	[EventDate] [datetime] NULL,
+	[EventType] [int] NULL,
+	CONSTRAINT [PK_GPInvoiceUpdate] PRIMARY KEY CLUSTERED ([PrimaryKey] ASC)
+)
+GO
