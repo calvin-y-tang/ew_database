7626679e2ee76eed4d7759250f29e88ec5d25ce5
Sam Chiang <sam.chiang@examworks.com>
2023-04-21 09:38:20 -0400

    IMEC-13463 - create new table in IMECentric master to handle data for InvoiceUpdate

---
+++ IMECentricMaster/Tables/GPInvoiceUpdate.sql
@@ -0,0 +1,22 @@
+﻿CREATE TABLE [dbo].[GPInvoiceUpdate] (
+	[PrimaryKey]		INT			IDENTITY(1,1) NOT NULL,
+	[FormatVersion]		INT			NULL,
+	[ProcessedFlag]		BIT			NOT NULL,
+	[ExportDate]		DATETIME	NULL,
+	[SourceID]			INT			NULL,
+	[InvoiceNo]			VARCHAR(15)	NULL,
+	[EWFacilityID]		INT			NULL,
+	[GPFacilityID]		VARCHAR(3)	NULL,
+	[CompanyID]			VARCHAR(11)	NULL,
+	[GPCustomerID]		VARCHAR(15)	NULL,
+	[OldClientID]		VARCHAR(15)	NULL,	
+	[NewClientID]		VARCHAR(15)	NULL,	
+	[ClaimNo]			VARCHAR(50)	NULL,
+	[CaseNo]			VARCHAR(15)	NULL,
+	[BatchNo]			VARCHAR(15)	NULL,
+	[EventDate]			DATETIME	NULL,
+	[EventType]			INT			NULL,
+    CONSTRAINT [PK_GPInvoiceUpdate] PRIMARY KEY CLUSTERED ([PrimaryKey] ASC)
+);
+GO
+
