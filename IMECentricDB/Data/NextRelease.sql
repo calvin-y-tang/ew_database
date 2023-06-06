d98d88b289ed93b9fa5d22681364d04071b40461
jose.pais <jose.pais@examworks.com>
2023-06-06 11:00:20 -0400

    IMEC-13475 - initialize value for new column to -1 (<All>)

--- IMECentricDB/Data/NextRelease.sql
+++ IMECentricDB/Data/NextRelease.sql
@@ -1,1 +1,8 @@
 -- Sprint 112
+
+-- IMEC-13475 - initialize value for new column to -1 (<All>)
+UPDATE tblSLAException
+   SET TATCalculationMethodID = -1
+ WHERE TATCalculationMethodID IS NULL
+ GO 
+
