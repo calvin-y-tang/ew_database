da5837661a31402deb8647676821e3ade106531a
jose.pais <jose.pais@examworks.com>
2023-09-13 15:07:44 -0400

    IMEC-13634 - business rules and new stored procedure for External Communications (Automatic Referral Acknowledgement for Amtrust)

--- IMECentricDB/Data/NextRelease.sql
+++ IMECentricDB/Data/NextRelease.sql
@@ -1,1 +1,15 @@
 -- Sprint 119
+
+-- IMEC-13817 - Create business rules for Automated Referral Acknowledgement
+DELETE 
+  FROM tblExternalCommunications
+GO
+INSERT INTO tblEvent (EventID, Descrip, Category)
+VALUES(1070, 'CaseHistoryAdded', 'Case')
+GO
+INSERT INTO tblBusinessRule (BusinessRuleID, Name, Category, Descrip, IsActive, EventID, AllowOverride, Param1Desc, Param2Desc, Param3Desc, Param4Desc, Param5Desc, BrokenRuleAction, Param6Desc)
+VALUES (116, 'CreateExtComm', 'Case', 'Create entry in tblExternalCommunications', 1, 1070, 0, 'ExtCommTypesAllowed', 'ExtCommEntity', NULL, NULL, NULL, 0, NULL)
+GO
+INSERT INTO tblBusinessRuleCondition(BusinessRuleID, EntityType, EntityID, BillingEntity, ProcessOrder, DateAdded, UserIDAdded, DateEdited, UserIDEdited, OfficeCode, EWBusLineID, EWServiceTypeID, Jurisdiction, Param1, Param2, Param3, Param4, Param5, Skip, Param6)
+VALUES (116, 'PC', 9, 2, 3, GETDATE(), 'Admin', GETDATE(), 'Admin', NULL, NULL, NULL, NULL, 'NewCase;', 'EntityType=PC;EntityID=9', NULL, NULL, NULL, 0, NULL)
+GO
