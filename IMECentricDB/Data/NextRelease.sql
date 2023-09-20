dedbfab5c65a6263ccc59a005eb9fd9471b94649
jose.pais <jose.pais@examworks.com>
2023-09-20 09:21:32 -0400

    -- IMEC-13847 patch tblWebUser.MFAEmailAddr column using email address from source table

--- IMECentricDB/Data/NextRelease.sql
+++ IMECentricDB/Data/NextRelease.sql
@@ -18,3 +18,37 @@ GO
 INSERT INTO tblUserFunction(FunctionCode, FunctionDesc, DateAdded)
 VALUES('DoctorEditNotes', 'Doctor - Edit Notes (Notes/QANotes/RecordReqNotes)', GETDATE())
 GO
+
+-- IMEC-13847 patch tblWebUser.MFAEmailAddr column using email address from source table
+UPDATE wu
+   SET MFAEmailAddr = CASE
+                         WHEN cl.Email IS NOT NULL THEN SUBSTRING(cl.Email, 1, CASE CHARINDEX(';', cl.Email)
+                                              WHEN 0 THEN NULL 
+                                              ELSE CHARINDEX(';', cl.Email) - 1
+                                              END) 
+                         WHEN dr.EmailAddr IS NOT NULL THEN SUBSTRING(dr.EmailAddr, 1, CASE CHARINDEX(';', dr.EmailAddr)
+                                              WHEN 0 THEN NULL 
+                                              ELSE CHARINDEX(';', dr.EmailAddr) - 1
+                                              END)
+                         WHEN atty.Email IS NOT NULL THEN SUBSTRING(atty.Email, 1, CASE CHARINDEX(';', atty.Email)
+                                              WHEN 0 THEN NULL 
+                                              ELSE CHARINDEX(';', atty.Email) - 1
+                                              END)
+                         WHEN tr.Email IS NOT NULL THEN SUBSTRING(tr.Email, 1, CASE CHARINDEX(';', tr.Email)
+                                              WHEN 0 THEN NULL 
+                                              ELSE CHARINDEX(';', tr.Email) - 1
+                                              END)
+                         WHEN drA.Email IS NOT NULL THEN SUBSTRING(drA.Email, 1, CASE CHARINDEX(';', drA.Email)
+                                              WHEN 0 THEN NULL 
+                                              ELSE CHARINDEX(';', drA.Email) - 1
+                                              END) 
+                         ELSE NULL
+                    END
+FROM tblWebUser AS wu
+          LEFT OUTER JOIN tblClient AS cl ON cl.ClientCode = wu.IMECentricCode AND wu.UserType = 'CL'
+          LEFT OUTER JOIN tblDoctor AS dr ON dr.DoctorCode = wu.IMECentricCode AND wu.UserType IN ('DR', 'OP')
+          LEFT OUTER JOIN tblCCAddress AS atty ON atty.ccCode = wu.IMECentricCode AND wu.UserType = 'AT'
+          LEFT OUTER JOIN tblTranscription AS tr ON tr.TransCode = wu.IMECentricCode AND wu.UserType = 'TR'
+          LEFT OUTER JOIN tblDrAssistant AS drA ON drA.DrAssistantID = wu.IMECentricCode AND wu.UserType = 'DA'
+GO 
+
