33189fd388e94b8b0c2c173aabc3542744979bbc
NWL5CD3083L0R\Terri.Lyde <terri.lyde@examworks.com>
2023-10-17 15:10:54 -0400

    IMEC-13804 - added tokens for defense and plantiff attorneys

--- IMECentricDB/Data/OneTime.sql
+++ IMECentricDB/Data/OneTime.sql
@@ -1,3 +1,22 @@
 ﻿-- Sprint 121
 
+-- IMEC-13804 - Add new tokens to tblMessageToken
+USE IMECentricEW
+GO
+  INSERT INTO tblMessageToken (Name) 
+  VALUES ('@DAttorneyName@'),
+         ('@DAttorneyCompany@'),
+         ('@DAttorneyAddr1@'),
+         ('@DAttorneyAddr2@'),
+         ('@DAttorneyAddr3@'),
+         ('@DAttorneyPhone@'),
+         ('@PAttorneyName@'),
+         ('@PAttorneyCompany@'),
+         ('@PAttorneyAddr1@'),
+         ('@PAttorneyAddr2@'),
+         ('@PAttorneyAddr3@'),
+         ('@PAttorneyPhone@')
+
+GO
+
 
