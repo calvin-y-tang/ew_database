29819477068f0b583ae1902ed73eae87a1856358
jose.pais <jose.pais@examworks.com>
2023-11-01 11:39:35 -0400

    IMEC-13839 - new setting to configure a delay that is applied when inserting logo into header of an Invoice

--- IMECentricDB/Data/NextRelease.sql
+++ IMECentricDB/Data/NextRelease.sql
@@ -1,1 +1,6 @@
 -- Sprint 122
+
+-- IMEC-13839 - new setting to configure a delay that is applied when inserting logo into header of an Invoice 
+INSERT INTO tblSetting (Name, Value)
+VALUES('WordAutomationLogoInsertDelay', '500')
+GO
