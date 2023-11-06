79828101b67a38f6f902a18365ca9eae2889c496
jose.pais <jose.pais@examworks.com>
2023-11-06 14:06:57 -0500

    Sprint 123 reset

--- IMECentricDB/Data/NextRelease.sql
+++ IMECentricDB/Data/NextRelease.sql
@@ -1,6 +1,1 @@
--- Sprint 122
-
--- IMEC-13839 - new setting to configure a delay that is applied when inserting logo into header of an Invoice 
-INSERT INTO tblSetting (Name, Value)
-VALUES('WordAutomationLogoInsertDelay', '500')
-GO
+-- Sprint 123
