064e36b66500083f8f3ee6e12e45efb47edab363
NWL5CD3083L0R\Terri.Lyde <terri.lyde@examworks.com>
2023-11-10 11:14:46 -0500

    IMEC-13942 - Added column [InvTaxCalcMethod] to [tblAcctHeader] to hold value for which tax calculation method to use for Canada.

--- IMECentricDB/Data/OneTime.sql
+++ IMECentricDB/Data/OneTime.sql
@@ -1,1 +1,16 @@
 ﻿-- Sprint 123
+
+-- Issue 13942 - adding new column to indicate which method will be used to calculate canadian taxes - company state or
+               -- office state.  Svaes Office code for Office State and 0 for company state
+USE [IMECentricEW]
+GO
+ALTER TABLE tblAcctHeader ADD InvTaxCalcMethod INT NULL
+GO
+
+ALTER TABLE tblAcctHeader ADD CONSTRAINT DF_tblAcctHeader_InvTaxCalcMethod DEFAULT (0) FOR InvTaxCalcMethod
+GO
+
+-- set values in table to default of 0
+UPDATE tblAcctHeader SET InvTaxCalcMethod = 0
+GO
+
