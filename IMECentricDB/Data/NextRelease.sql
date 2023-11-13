247b5eee0f6747be63ea238f2bc79cdf01589f4b
jose.pais <jose.pais@examworks.com>
2023-11-13 14:55:06 -0500

    IMEC-13930 - implement changes for O365 faxing

--- IMECentricDB/Data/NextRelease.sql
+++ IMECentricDB/Data/NextRelease.sql
@@ -1,1 +1,8 @@
 -- Sprint 123
+
+
+-- IMEC-13930 - O365 Changes for Xmedius Faxing feature.
+INSERT INTO tblSetting(Name, Value)
+VALUES('FaxSysFormat', 'Default'),
+      ('FaxPhonePrefix','91')
+GO
