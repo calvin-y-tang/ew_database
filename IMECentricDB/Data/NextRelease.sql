b5173eb0d7846edb012079bfffadcc4ebeaaba3e
NWL5CD3083L0R\Terri.Lyde <terri.lyde@examworks.com>
2023-11-21 11:15:49 -0500

    IMEC-13942 - update default values for column InvTaxCalcMethod in tblAcctHeader

--- IMECentricDB/Data/NextRelease.sql
+++ IMECentricDB/Data/NextRelease.sql
@@ -6,3 +6,10 @@ INSERT INTO tblSetting(Name, Value)
 VALUES('FaxSysFormat', 'Default'),
       ('FaxPhonePrefix','91')
 GO
+
+
+-- IMEC-13942 - set default values for column [InvTaxCalcMethod] in tblAcctHeader
+UPDATE tblAcctHeader SET InvTaxCalcMethod = 0
+GO
+
+
