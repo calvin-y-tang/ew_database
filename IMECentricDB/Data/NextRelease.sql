d833551d89c54607c4322eac667b318efa031daa
NWL5CD3083L0R\Terri.Lyde <terri.lyde@examworks.com>
2023-11-30 09:24:20 -0500

    IMEC-13893 - add value to tblConfiguration for Texas Tax - default timeout, IS email address, and boolean to update expiration dates on taxes or not

--- IMECentricDB/Data/NextRelease.sql
+++ IMECentricDB/Data/NextRelease.sql
@@ -1,3 +1,7 @@
 -- Sprint 124
 
+-- IMEC-13893 - Texas Tax configuration information for default time out, update expiration date, and IS email address for problems
+INSERT INTO tblConfiguration (ConfigurationID, Name, Active, ConfigurationType, DateEdited, UserIDEdited, Param)
+VALUES (19, 'Tax Web Service', 1, 'Texas', GETDATE(), 'TLyde', 'DefaultTimeOutmsec=5000;NoResponseUpdateExpDate=1;EmailIS="william.cecil@examworks.com"')
+GO
 
