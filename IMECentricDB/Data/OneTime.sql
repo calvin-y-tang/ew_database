bf796821de71d5e137d1fd1d0e9026b2dc2d587a
NWL5CD3083L0R\Terri.Lyde <terri.lyde@examworks.com>
2023-12-01 13:00:28 -0500

    IMEC-13955 - adding additional mapping values to ISMapping for RPA medical record files to handle file name possibilities

--- IMECentricDB/Data/OneTime.sql
+++ IMECentricDB/Data/OneTime.sql
@@ -1,2 +1,12 @@
 ﻿-- Sprint 124
 
+--IMEC-13955 - RPA Update Meds Type when medical records received - adding more mappins for possible file name variations
+USE IMECentricMaster
+
+INSERT INTO ISMapping (MappingType, MappingName, SrcValue, SrcDescrip, DstValue, DstDescrip)
+VALUES 
+  ('ExtDocIntake', 'CaseDocType', 0, 'Med Records', 7, 'Medical Record'),
+  ('ExtDocIntake', 'CaseDocType', 0, 'MedRecords', 7, 'Medical Record'),
+  ('ExtDocIntake', 'CaseDocType', 0, 'Med Record', 7, 'Medical Record')
+GO
+
