cff2ff810f00a7ee386fb3ca5413d3588cc8ec4a
NWL5CD3083L0R\Terri.Lyde <terri.lyde@examworks.com>
2023-12-01 08:38:48 -0500

    IMEC-13893 - adding in email for Doug Troy - format has a comma inbetween email addresses

--- IMECentricDB/Data/NextRelease.sql
+++ IMECentricDB/Data/NextRelease.sql
@@ -2,6 +2,6 @@
 
 -- IMEC-13893 - Texas Tax configuration information for default time out, update expiration date, and IS email address for problems
 INSERT INTO tblConfiguration (ConfigurationID, Name, Active, ConfigurationType, DateEdited, UserIDEdited, Param)
-VALUES (19, 'Tax Web Service', 1, 'Texas', GETDATE(), 'TLyde', 'DefaultTimeOutmsec=5000;NoResponseUpdateExpDate=1;EmailIS="william.cecil@examworks.com"')
+VALUES (19, 'Tax Web Service', 1, 'Texas', GETDATE(), 'TLyde', 'DefaultTimeOutmsec=5000;NoResponseUpdateExpDate=1;EmailIS="william.cecil@examworks.com,Doug.Troy@examworks.com"')
 GO
 
