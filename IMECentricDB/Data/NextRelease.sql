ee2ecf12e28309ec8cc499b10ad2d10fdb12afa3
NWL5CD3083L0R\Terri.Lyde <terri.lyde@examworks.com>
2023-12-05 01:46:01 -0500

    IMEc-13883 - reset NextRelease for sprint 125 and add tblSetting value to show transcriptionist subform

--- IMECentricDB/Data/NextRelease.sql
+++ IMECentricDB/Data/NextRelease.sql
@@ -1,7 +1,8 @@
--- Sprint 124
+-- Sprint 125
 
--- IMEC-13893 - Texas Tax configuration information for default time out, update expiration date, and IS email address for problems
-INSERT INTO tblConfiguration (ConfigurationID, Name, Active, ConfigurationType, DateEdited, UserIDEdited, Param)
-VALUES (19, 'Tax Web Service', 1, 'Texas', GETDATE(), 'TLyde', 'DefaultTimeOutmsec=5000;NoResponseUpdateExpDate=1;EmailIS="william.cecil@examworks.com,Doug.Troy@examworks.com"')
+
+-- IMEC-13883 - Allow multiple transcriptionist - setting to set subform visibility
+INSERT INTO tblSetting (Name, Value)
+VALUES ('ShowTranscriptionists', 'True')
 GO
 
