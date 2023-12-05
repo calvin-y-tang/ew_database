df938d9880297c7789dd3dc697236883341b8742
NWL5CD3083L0R\Terri.Lyde <terri.lyde@examworks.com>
2023-12-05 16:20:40 -0500

    IMEC-13883 - add data patch to copy trans companies to transcriptionist

--- IMECentricDB/Data/NextRelease.sql
+++ IMECentricDB/Data/NextRelease.sql
@@ -6,3 +6,12 @@ INSERT INTO tblSetting (Name, Value)
 VALUES ('ShowTranscriptionists', 'True')
 GO
 
+-- IMEC-13883 - Data patch to copy trans companies to transcriptionist
+SET IDENTITY_INSERT tblTranscriptionist ON
+INSERT INTO tblTranscriptionist (TranscriptionistID, FirstName, LastName, Email, Status, DateAdded, UserIDAdded, TransCode, WebUserID)
+SELECT TransCode, '.', TransCompany, email, Status, GETDATE(), 'Admin', TransCode,  T.WebUserID 
+FROM tblTranscription AS T 
+LEFT JOIN tblWebUser AS W ON T.WebUserID = W.WebUserID
+SET IDENTITY_INSERT tblTranscriptionist OFF
+GO
+
