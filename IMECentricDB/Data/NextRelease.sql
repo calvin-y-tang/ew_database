cbc0c3b98a2ec03b63fff19a013fd5fac36d6dfa
NWL5CD3083L0R\Terri.Lyde <terri.lyde@examworks.com>
2024-01-05 15:33:59 -0500

    IMEC-13994 - DB changes for RPA LibertyiCase documents - add index too tblCase for sInternalCaseNbr, add a setting to tblSetting for the email address for errors, add the LibertyiCase RPA task to ISSchedule

--- IMECentricDB/Data/NextRelease.sql
+++ IMECentricDB/Data/NextRelease.sql
@@ -1,2 +1,7 @@
 -- Sprint 127
 
+-- IMEC-13994 - Add email address for when RPA document intake errors occur with referral numbers
+INSERT INTO tblSetting (Name, Value)
+VALUES ('EmailForRefrlErr_RPA_Liberty', 'william.cecil@examworks.com')
+GO
+
--- IMECentricDB/Data/OneTime.sql
+++ IMECentricDB/Data/OneTime.sql
@@ -1,1 +1,14 @@
 ﻿-- Sprint 127
+
+-- IMEC-13994 - RPA LibertyiCase updates - add entry for documents
+USE [IMECentricMaster]
+UPDATE IMECentricMaster.dbo.ISSchedule SET SeqNo = 6 WHERE ScheduleID = 381
+UPDATE IMECentricMaster.dbo.ISSchedule SET SeqNo = 5 WHERE ScheduleID = 372
+
+INSERT INTO ISSchedule (Name, Task, Type, Interval, WeekDays, Time, StartDate, Param, GroupNo, SeqNo)
+VALUES ('RPA Ext Doc - Liberty', 'ExtDocIntake', 'm', 5, '1111111', '1900-01-01 01:00:00', '2024-01-01 00:00:00', 
+'IntakeFolderID=332;FileNameFormat=dbid-intrnlCaseNbr-keyword-datetime-casedoctypename-status;CreateCaseDocumentFolders=true;DefaultUserID=RPA;FileMask=*LibertyiCase*.PDF;EventDesc="Document Uploaded";CaseDocTypeID=7;CaseHistoryType="Records";DocumentDescription="Meds Uploaded";AdditionalActions="RPA=UpdateCaseForRPA;Email1=EmailForCaseNbrErr;"',
+560, 4)
+
+
+GO
