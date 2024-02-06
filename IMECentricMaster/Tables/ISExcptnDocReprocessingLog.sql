0dde9bb7dd9ce1c892bfa4048a91f6c54d1b5a95
NWL5CD3083L0R\Terri.Lyde <terri.lyde@examworks.com>
2024-02-06 09:31:10 -0500

    IMEC-14033 - added new table in Master database (ISExcptnDocReprocessingLog) to track the reprocessing of files in the exsception folder

---
+++ IMECentricMaster/Tables/ISExcptnDocReprocessingLog.sql
@@ -0,0 +1,13 @@
+﻿
+CREATE TABLE [dbo].[ISExcptnDocReprocessingLog](
+	[ReprocessingLogID] INT             IDENTITY(1, 1) NOT FOR REPLICATION NOT NULL,
+	[MaxTimesRetry]     INT             NULL,
+	[AttemptNumber]     INT             NULL,
+	[AttemptTime]       SMALLDATETIME   NULL,
+	[FileName]          VARCHAR (200)   NULL,
+	[ExceptionFolder]   VARCHAR (300)   NULL,
+	[InboxFolder]       VARCHAR (300)   NULL,
+	[InboxFolderID]     INT             NULL,
+    CONSTRAINT [PK_ISExcptnDocReprocessingLog] PRIMARY KEY CLUSTERED ([ReprocessingLogID] ASC) WITH (FILLFACTOR = 90)
+);
+
