3d539f5ef7246cbb2015a9bae4c599fabbe86bbd
DougTroy <doug.troy@examworks.com>
2024-02-12 14:46:56 -0500

    IMEC-14068 - project and script changes.

---
+++ IMECentricDB/Data/Single_IMEC_NextRelease.sql
@@ -0,0 +1,15 @@
+﻿-- --------------------------------------------------------------------------
+--	Scripts placed in here are applied against a single IMECentric database. 
+--	You must specify which database to apply with a USE statement
+
+--	Example
+--	USE [IMECentricEW]
+--	GO
+--	{script to apply}
+--	GO
+
+-- --------------------------------------------------------------------------
+
+-- Sprint 129
+
+
