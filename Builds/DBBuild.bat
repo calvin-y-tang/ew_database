1832e4789990b551a4aaeeb110a24f44b1de07a5
william.cecil <William.Cecil@examworks.com>
2024-02-23 14:14:36 -0500

    Build Script changes for New DB Database Project

--- Builds/DBBuild.bat
+++ Builds/DBBuild.bat
@@ -2,8 +2,7 @@ rem @echo off
 
 
 rem Require VS2017, SSMS
-rem Last Revision 2021/11/12
-
+rem Last Revision 2024/02/12
 
 
 rem Manually set version information here
@@ -12,21 +11,17 @@ set nextVer=4.54
 
 echo Building %nextVer%...
 
-
-
-
 rem EXE/Folder Location
 set msbEXE="C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe"
 set sqlEXE="C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\150\SqlPackage.exe"
 
-set    projFld=E:\Database\IMECentricDB
-set    baseFld=E:\Database\Builds\Temp
-set  dacPacFld=E:\Database\Builds\IMECentricDB
+set projFld=E:\Database\IMECentricDB
+set baseFld=E:\Database\Builds\Temp
+set dacPacFld=E:\Database\Builds\IMECentricDB
 set scriptsFld=E:\Database\Builds\IMECentricDB
 
 rem Create the DacPac files
 %msbEXE% "%projFld%\IMECentricDB.sqlproj" -t:rebuild
-
 
 rem Copy DacPac files into Temp folder
 if not exist "%baseFld%" mkdir "%baseFld%"
@@ -37,19 +32,18 @@ copy "%dacPacFld%\Release %lastVer%.dacpac" "%baseFld%\lastVer.dacpac"
 
 rem Generate schema and data script
 %sqlEXE% /a:Script /sf:"%baseFld%\nextVer.dacpac" /tf:"%baseFld%\lastVer.dacpac" /tdn:sTargetDB /op:"%baseFld%\Schema.sql" /p:BackupDatabaseBeforeChanges=False /p:BlockOnPossibleDataLoss=False /p:CommentOutSetVarDeclarations=True /p:IgnoreColumnOrder=True /p:IgnoreComments=True /p:ScriptRefreshModule=False /p:IncludeTransactionalScripts=True
-copy "%projFld%\Data\NextRelease.sql" "%baseFld%\Data.sql"
+copy "%projFld%\Data\All_IMEC_NextRelease.sql" "%baseFld%\Data.sql"
 copy "%baseFld%\Schema.sql" + "%baseFld%\Data.sql" "%scriptsFld%\%nextVer%.sql"
 
-
+rem Copy the Master and Single IMEC scripts and version them
+copy "%projFld%\Data\MasterNextRelease.sql" "%baseFld%\IMEC_Master_%nextVer%.sql"
+copy "%projFld%\Data\Single_IMEC_NextRelease.sql" "%baseFld%\Single_IMEC_%nextVer%.sql"
 
 rem copy final DapPac into release folder
 copy "%baseFld%\nextVer.dacpac" "%dacPacFld%\Release %nextVer%.dacpac"
 
-
-
 rem Clean up Temp folder
 del "%baseFld%\*.*" /q
-
 
 rem Done
 pause
