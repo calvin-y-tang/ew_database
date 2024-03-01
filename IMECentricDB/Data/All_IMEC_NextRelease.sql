8502258bdb9ed3fbb0d79b9204930486c1ea2f11
NWL5CD3083L0R\Terri.Lyde <terri.lyde@examworks.com>
2024-03-01 14:08:33 -0500

    IMEC-14081 - adding new task entries to ISSchedule and ISExtIntegration - task = AutomatedEmailLateDocs

--- IMECentricDB/Data/All_IMEC_NextRelease.sql
+++ IMECentricDB/Data/All_IMEC_NextRelease.sql
@@ -4,3 +4,22 @@
 -- --------------------------------------------------------------------------
 
 -- Sprint 131
+
+-- IMEC-14081 - data patch for Nationwide cases that sets a date for emails sent for cases that apply so they will not be sent again
+UPDATE tblCase SET DateEmailSentForLateReport = '2024-03-01 00:00:00.000' 
+WHERE CaseNbr IN
+(
+select C.CaseNbr
+from tblCase AS C
+left join tblclient as CL on C.ClientCode = CL.ClientCode
+left join tblclient AS CLB on C.BillClientCode = CLB.ClientCode
+left join tblCompany AS CO on CL.CompanyCode = CO.CompanyCode
+left join tblCompany AS COB on CLB.CompanyCode = COB.CompanyCode
+left join tblServices AS S on c.ServiceCode = s.ServiceCode
+left join tblEWServiceType AS ST on S.EWServiceTypeID = ST.EWServiceTypeID
+where C.casetype = 10 and C.status not in (8,9) and (CO.ParentCompanyID = 34 or COB.ParentCompanyID = 34)  and c.RptSentDate is null
+and (C.ApptDate <= '2024-02-29 00:00:00.000' OR C.DateMedsRecd <= '2024-02-29 00:00:00.000')
+)
+
+GO
+
--- IMECentricDB/Data/MasterNextRelease.sql
+++ IMECentricDB/Data/MasterNextRelease.sql
@@ -6,3 +6,15 @@ GO
 
 -- Sprint 131
 
+-- IMEC-14081 - adding new task to ISExtIntegration and ISSchedule - task to auto send emails for late reports
+INSERT INTO ISExtIntegration (ExtIntegrationID, Name, Type, Active, NotifyEmail, Param)
+VALUES (6100, 'Nationwide Late Reports', 'AutomatedEmailLateDocs', 1, 'William.Cecil@examworks.com', 
+'DBID=23;PCID=34;DaysLate=10;CaseType=10;TaskName=NationwideEmailLateReport;Subject="Claim number:  %ClaimNbr%, Examworks report delay notification";Body="Claim number:  %ClaimNbr% <br /> Examinee:  %ExamineeName% <br /> <br /> Please be advised that the report is delayed. We have been in touch with the provider and they are working to complete. As soon as the report is available, we will forward it to you. Thanks for your patience in this matter. <br /> <br /> Examworks"')
+
+GO
+
+INSERT INTO ISSchedule (Name, Task, Type, Interval, WeekDays, Time, StartDate)
+VALUES ('Nationwide Late Reports', 'AutomatedEmailLateDocs', 'm', 60, '0111110', '1900-01-01 06:00:00', GetDate())
+
+GO
+
