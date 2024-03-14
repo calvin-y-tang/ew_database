9131079eb21ab519b37aa095310e8789cee8db86
jose.pais <jose.pais@examworks.com>
2024-03-14 09:10:43 -0400

    IMEC-14109 - add new secuirty token that will lock down doctor accounting fields.

--- IMECentricDB/Data/All_IMEC_NextRelease.sql
+++ IMECentricDB/Data/All_IMEC_NextRelease.sql
@@ -4,3 +4,8 @@
 -- --------------------------------------------------------------------------
 
 -- Sprint 132
+
+-- IMEC-14109 - new security token to lock down Doctor Accounting fields
+INSERT INTO tblUserFunction(FunctionCode, FunctionDesc, DateAdded)
+VALUES ('DoctorEditAcctngFields', 'Doctor - Edit Accounting Fields', GETDATE())
+GO
