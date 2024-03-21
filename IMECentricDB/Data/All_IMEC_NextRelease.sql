5176117422434c39902016553d12d5567dd0714b
jose.pais <jose.pais@examworks.com>
2024-03-21 15:15:56 -0400

    IMEC-14076 - Business Rules to drive new "WCCaseTypeAddressee" bookmark; DEV NOTE: This was undone in a rollback and needs to be re-applied

--- IMECentricDB/Data/All_IMEC_NextRelease.sql
+++ IMECentricDB/Data/All_IMEC_NextRelease.sql
@@ -10,8 +10,14 @@ INSERT INTO tblUserFunction(FunctionCode, FunctionDesc, DateAdded)
 VALUES ('DoctorEditAcctngFields', 'Doctor - Edit Accounting Fields', GETDATE())
 GO
 
-
 -- IMEC-14110 - new security tokem to enable changes for Specialty Do Not Use setting
 INSERT INTO tblUserFunction(FunctionCode, FunctionDesc, DateAdded)
 VALUES ('DoctorEditSpecialtyDoNotUse', 'Doctor - Update Specialty Do Not Use', GETDATE())
+GO
+
+-- IMEC-14076 - Business Rules to drive new "WCCaseTypeAddressee" bookmark 
+-- **** DEV NOTE: This was undone in a rollback and needs to be re-applied
+UPDATE tblBusinessRule
+  SET EventID = 1201
+WHERE Name = 'DynamicBookmarks'
 GO
