0bfcfe5aa1a688e94e49cfa7c19e6d2597cf2ab3
NWL5CD3083L0R\Terri.Lyde <terri.lyde@examworks.com>
2024-04-11 11:06:22 -0400

    IMEC-14048 - adding timout value to tblSetting to give time for Helper to reduce image, save it and write toi tblTempData

--- IMECentricDB/Data/All_IMEC_NextRelease.sql
+++ IMECentricDB/Data/All_IMEC_NextRelease.sql
@@ -5,3 +5,6 @@
 
 -- Sprint 133
 
+-- IMEC-14048 - add timeout to allow time for Helper to resize the image, save the file, and write to tblTempData
+INSERT INTO tblSetting ([Name], [Value]) VALUES ('TimeOutResizeImage', '9000')
+
