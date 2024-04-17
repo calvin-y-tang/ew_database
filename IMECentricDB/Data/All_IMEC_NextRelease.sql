e02fb62002363c85e881dfcfb1ef30661404b662
jose.pais <jose.pais@examworks.com>
2024-04-17 08:00:15 -0400

    IMEC-14200 - set default value for previously added DoNotUse column

--- IMECentricDB/Data/All_IMEC_NextRelease.sql
+++ IMECentricDB/Data/All_IMEC_NextRelease.sql
@@ -4,3 +4,8 @@
 -- --------------------------------------------------------------------------
 
 -- Sprint 134
+
+-- JAP - IMEC-14200 - set default value for previously added DoNotUse column
+UPDATE tblDoctorSpecialty SET DoNotUse = 0 WHERE DoNotUse IS NULL
+GO 
+
