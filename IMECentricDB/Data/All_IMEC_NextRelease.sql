6c3770ae197162b893d2d01f0f588fe019fbd79e
NWL5CD3083L0R\Terri.Lyde <terri.lyde@examworks.com>
2024-05-10 12:28:46 -0400

    IMEC-14212 - changes to business rule 170 to update description and Param1 description

--- IMECentricDB/Data/All_IMEC_NextRelease.sql
+++ IMECentricDB/Data/All_IMEC_NextRelease.sql
@@ -11,6 +11,6 @@ GO
 
 -- TL - IMEC-14212 - new business rule to allow companies and PC's to opt out of having MedIndex-Final document published on web
 INSERT INTO tblBusinessRule (BusinessRuleID, Name, Category, Descrip, IsActive, EventID, AllowOverride, Param1Desc, BrokenRuleAction)
-VALUES (170, 'OptOutPOWMedIndexFinal', 'Case', 'Entities opting out of publishing to web DPS document MedIndex-Final for client and/or billing client', 1,
-        1015, 0, 'ClientTypeToPOWTo', 0)
+VALUES (170, 'OptOutPOWMedIndexFinal', 'Case', 'IMEC-14212 - Entities opting out of publishing to web DPS document MedIndex-Final for client and/or billing client', 1,
+        1015, 0, 'ClientTypeToOptOut', 0)
 GO
