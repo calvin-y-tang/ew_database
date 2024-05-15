91cfb5f110d3aabce1fa9f06ab23a3bbdd799f53
jose.pais <jose.pais@examworks.com>
2024-05-15 09:00:27 -0400

    Sprint 136 reset

--- IMECentricDB/Data/All_IMEC_NextRelease.sql
+++ IMECentricDB/Data/All_IMEC_NextRelease.sql
@@ -3,14 +3,4 @@
 --	both US and CA
 -- --------------------------------------------------------------------------
 
--- Sprint 134
-
--- JAP - IMEC-14200 - set default value for previously added DoNotUse column
-UPDATE tblDoctorSpecialty SET DoNotUse = 0 WHERE DoNotUse IS NULL
-GO 
-
--- TL - IMEC-14212 - new business rule to allow companies and PC's to opt out of having MedIndex-Final document published on web
-INSERT INTO tblBusinessRule (BusinessRuleID, Name, Category, Descrip, IsActive, EventID, AllowOverride, Param1Desc, BrokenRuleAction)
-VALUES (170, 'OptOutPOWMedIndexFinal', 'Case', 'IMEC-14212 - Entities opting out of publishing to web DPS document MedIndex-Final for client and/or billing client', 1,
-        1015, 0, 'ClientTypeToOptOut', 0)
-GO
+-- Sprint 136
--- IMECentricDB/Data/MasterNextRelease.sql
+++ IMECentricDB/Data/MasterNextRelease.sql
@@ -4,12 +4,5 @@
 USE [IMECentricMaster]  -- DO NOT REMOVE
 GO
 
--- Sprint 134
-
--- DB Change applied on 04/24 by DHT and WC
--- // IMEC-14211 - Add ReleaseType field to Acctheader table in the data repository
--- // Dev: Sam Chiang
---alter table IMECentricMaster.dbo.GPInvoice
---add ReleaseType varchar(100) null
---go
+-- Sprint 136
 
