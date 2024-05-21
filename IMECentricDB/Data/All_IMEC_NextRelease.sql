0e886e0dd82e3fe39ceed7905bfe26eb63d0e54d
jose.pais <jose.pais@examworks.com>
2024-05-21 09:44:18 -0400

    IMEC-14227 (IMEC-14235) - add token and update bizRule details on all DBs to keep them in sync

--- IMECentricDB/Data/All_IMEC_NextRelease.sql
+++ IMECentricDB/Data/All_IMEC_NextRelease.sql
@@ -4,3 +4,15 @@
 -- --------------------------------------------------------------------------
 
 -- Sprint 136
+
+-- IMEC-14227 (IMEC-14235) - add token and update bizRule details on all DBs to keep them in sync
+UPDATE tblBusinessRule
+   SET Param5Desc = 'SecurityToken', 
+       Param6Desc = 'ServiceCode', 
+       AllowOverride = 1
+WHERE BusinessRuleID = 130
+GO
+
+INSERT INTO tblUserFunction(FunctionCode, FunctionDesc, DateAdded)
+VALUES('CaseSkipReqFieldCheck','Case -Override Required Field Validation (BusRule)', GETDATE())
+GO
