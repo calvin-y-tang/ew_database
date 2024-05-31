02d91f6af696fb4ea94129d38d10e64a2a2af9b3
Sam Chiang <sam.chiang@examworks.com>
2024-05-31 09:07:23 -0400

    IMEC-14268 - add new ISIntegration entry for Cyclone

--- IMECentricDB/Data/MasterNextRelease.sql
+++ IMECentricDB/Data/MasterNextRelease.sql
@@ -6,3 +6,34 @@ GO
 
 -- Sprint 136
 
+-- // IMEC-14268 - ISIntegration entry to handle new process for Cyclone
+USE IMECentricMaster
+GO
+
+insert into ISIntegration (
+    IntegrationID,Name,Active,Type,ToGP,FromGP,DBID,
+    LastDateEditedCompany,LastDateEditedClient,LastDateEditedDoctor,
+    DRVersion,DRInterval,DRWeekend,
+    FromGPNextBatchNo,FromGPFacilityIDs,CountryCode,GPEntityPrefix,
+    UseDataFeedIDForGP,ManageIMECentricID,NextCompanyID,NextClientID,NextDoctorID,
+    FolderPath,
+    UseSFTP,
+    SFTPPath,
+    Email,
+    OffsetHours,FromGPExcludeInvNoStart,FromGPExcludeInvNoEnd,FromGPExcludeVoNoStart,FromGPExcludeVoNoEnd,GPDBID,ExportMarketer
+) values (
+    580,'Cyclone',0,'DR',1,0,null,
+    null,null,null,
+    10,1440,null,
+    1,null,'CA',580,
+    1,1,1,1,1,
+    '\\IMECDocs5\ISIntegrations\EWRepository\580\',
+    1, 
+    '/IS/Internal/RepositoryFeeds/Cyclone/Prod/',
+    'Jim.Harris@examworks.com',
+    null,null,null,null,null,null,0
+)
+GO
+
+
+
