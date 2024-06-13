cb448bfb5543d10d37275557509431a16dabfb20
NWL5CD3083L0R\Terri.Lyde <terri.lyde@examworks.com>
2024-06-13 14:12:30 -0400

    IMEC-14254 - Farmers claim number format - business rules and business rule conditions, security token for overriding formating; tblsetting value for start date on cases added

--- IMECentricDB/Data/All_IMEC_NextRelease.sql
+++ IMECentricDB/Data/All_IMEC_NextRelease.sql
@@ -4,3 +4,28 @@
 -- --------------------------------------------------------------------------
 
 -- Sprint 137
+
+-- IMEC-14254 - business rules and business rule conditions for Farmers claim number format
+INSERT INTO tblBusinessRule (BusinessRuleID, Name, Category, Descrip,
+  IsActive, EventID, AllowOverride, Param1Desc, Param2Desc, BrokenRuleAction)
+VALUES (171, 'ClaimNbrFormat', 'Case', 'Defines claim number format on case form', 1, 1001, 1, 'Format', 'Override Sec Token', 0)
+GO
+
+INSERT INTO tblBusinessRuleCondition (EntityType, EntityID, BillingEntity, ProcessOrder, BusinessRuleID,
+   DateAdded, UserIDAdded, EWBusLineID, Param1, Param2)
+  VALUES 
+   ('PC', 24, 2, 1, 171, GETDATE(), 'Admin', 2, '##########-#-#', 'ClaimNbrFormatOverride'),
+   ('PC', 24, 2, 1, 171, GETDATE(), 'Admin', 5, '##########-#-#', 'ClaimNbrFormatOverride'),
+   ('PC', 24, 2, 1, 171, GETDATE(), 'Admin', 3, '##########-#', 'ClaimNbrFormatOverride')
+GO
+
+-- IMEC-14254 - Start date for cases added for Farmers claim number format
+INSERT INTO tblSetting ([Name], [Value])
+VALUES ('FarmersClaimNbrFormatStartDate', '2024/07/01')
+GO
+
+-- IMEC-14254 - security token for Farmers claim number format override
+INSERT INTO tblUserFunction (FunctionCode, FunctionDesc, DateAdded)
+  VALUES ('ClaimNbrFormatOverride', 'Case - Claim Nbr Format Override', GETDATE())
+GO
+
