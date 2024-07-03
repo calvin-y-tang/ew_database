87394591986c47d0b46b359a03b5e1252c9d092c
jose.pais <jose.pais@examworks.com>
2024-07-03 08:55:44 -0400

    IMEC-14237 (IMEC-14097) - allow Parent Company to specific folder locations for Accounting and Case documents to use for associated cases.

--- IMECentricDB/Data/MasterNextRelease.sql
+++ IMECentricDB/Data/MasterNextRelease.sql
@@ -5,3 +5,10 @@ USE [IMECentricMaster]  -- DO NOT REMOVE
 GO
 
 -- Sprint 138
+
+-- IMEC-14237 - add columns for parent company folderIDs for Case and Accting documents
+ALTER TABLE IMECentricMaster.dbo.EWParentCompany ADD CaseDocFolderID INT NULL
+GO
+ALTER TABLE IMECentricMaster.dbo.EWParentCompany ADD AcctDocFolderID INT NULL
+GO
+
