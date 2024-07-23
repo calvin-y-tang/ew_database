bfc00a7def218f62df8990f3978fb70330ccfaba
jose.pais <jose.pais@examworks.com>
2024-07-23 09:29:40 -0400

    Sprint 139 Reset

--- IMECentricDB/Data/MasterNextRelease.sql
+++ IMECentricDB/Data/MasterNextRelease.sql
@@ -4,15 +4,4 @@
 USE [IMECentricMaster]  -- DO NOT REMOVE
 GO
 
--- Sprint 138
-
--- IMEC-14237 - add columns for parent company folderIDs for Case and Accting documents
--- DEV NOTES: we are not going to create these columns in master and have them only
---		be part of the IMEC BU DBs. This allows us to have separate folders for each
---		DB without having to worry about having the same case number present in 
---		multiple DBs.
---ALTER TABLE IMECentricMaster.dbo.EWParentCompany ADD CaseDocFolderID INT NULL
---GO
---ALTER TABLE IMECentricMaster.dbo.EWParentCompany ADD AcctDocFolderID INT NULL
---GO
-
+-- Sprint 139
