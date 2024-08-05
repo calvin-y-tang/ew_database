5508fe10a4e5aa9588abb6b8c19a9b0f404889e2
jose.pais <jose.pais@examworks.com>
2024-08-05 15:11:15 -0400

    IMEC-14263 - new Amtrust override security token and Bizrule update

--- IMECentricDB/Data/All_IMEC_NextRelease.sql
+++ IMECentricDB/Data/All_IMEC_NextRelease.sql
@@ -24,4 +24,19 @@ INSERT INTO tblBusinessRuleCondition (EntityType, EntityID, BillingEntity, Proce
 VALUES ('CO', 76626, 2, 1, 173, GETDATE(), 'Admin', 'WBPROGAPT*', 'DoNotReply@ExamWorks.com', 'PA', 'DistDocsExtEmailSys_ProgAlbany')
 GO
 
-
+-- IMEC-14263 - new Amtrust override security token and Bizrule update 
+INSERT INTO tblUserFunction(FunctionCode, FunctionDesc, DateAdded)
+VALUES('AMTrustInvGuardrailOverride', 'Amtrust - Override Guardrails for Finalize Invoice', GetDate())
+GO
+UPDATE tblBusinessRule
+   SET Param5Desc = 'SecurityTokenOvrRide'
+  FROM tblBusinessRule 
+ WHERE BusinessRuleID = 151
+GO
+UPDATE tblBusinessRuleCondition
+   SET Param5 = 'AMTrustInvGuardrailOverride'
+  FROM tblBusinessRuleCondition
+ WHERE BusinessRuleID = 151
+   AND EntityType = 'PC' 
+   AND EntityID = 9 
+GO 
