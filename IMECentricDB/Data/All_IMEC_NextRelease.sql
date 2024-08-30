bd1e259d89fa5456f3f8cdcb1e59b52c984ac06e
NWL5CD3083L0R\Terri.Lyde <terri.lyde@examworks.com>
2024-08-30 09:02:05 -0400

    IMEC-14380 - data patch

--- IMECentricDB/Data/All_IMEC_NextRelease.sql
+++ IMECentricDB/Data/All_IMEC_NextRelease.sql
@@ -5,3 +5,8 @@
 
 -- Sprint 140
 
+-- IMEC 14380 - Data patch - sets bits to 0 so checkboxes are blank and then patch the data
+UPDATE tblCaseDocuments SET MedsIncoming = 0, MedsToDoctor = 0
+UPDATE tblCaseDocuments SET MedsIncoming = 1 WHERE CaseDocTypeID = 7 AND UserIDAdded LIKE '%@%'
+GO
+
