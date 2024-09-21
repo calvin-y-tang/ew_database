dfd7311c2bb63ed5b23dd20412dac613bc5bf4c3
DougTroy <doug.troy@examworks.com>
2024-09-21 14:31:30 -0400

    IMEC-14360 - updated all local CoverLetter tables with IDENTITY

--- IMECentricDB/Tables/tblEWCoverLetterCompanyName.sql
+++ IMECentricDB/Tables/tblEWCoverLetterCompanyName.sql
@@ -1,5 +1,5 @@
 ﻿CREATE TABLE [dbo].[tblEWCoverLetterCompanyName] (
-    [EWCoverLetterCompanyNameID] INT          NOT NULL,
+    [EWCoverLetterCompanyNameID] INT          IDENTITY (1, 1) NOT NULL,
     [EWCoverLetterID]            INT          NOT NULL,
     [CompanyName]                VARCHAR (80) NULL,
     [UserIDAdded]                VARCHAR (30) NULL,
