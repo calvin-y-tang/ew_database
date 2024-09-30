ff7f54ec3ae444564fb0629e87716f36bd157aba
jose.pais <jose.pais@examworks.com>
2024-09-30 17:09:42 -0400

    -- IMEC-14296 - set CaseDocTypeID values for DBs based on current contents of local tblCaseDocType table

--- IMECentricDB/Data/Single_IMEC_NextRelease.sql
+++ IMECentricDB/Data/Single_IMEC_NextRelease.sql
@@ -21,3 +21,186 @@ GO
             (153, 'CO', 2, 2, 1, GETDATE(), 'Admin', GETDATE(), 'Admin', NULL, NULL, NULL, NULL, 'Always', NULL, NULL, NULL, NULL, 0, NULL, 0), 
             (153, 'CO', 4, 2, 1, GETDATE(), 'Admin', GETDATE(), 'Admin', NULL, NULL, NULL, NULL, 'Always', NULL, NULL, NULL, NULL, 0, NULL, 0)
 GO
+
+-- IMEC-14296 - set CaseDocTypeID values for DBs based on current contents of local tblCaseDocType table
+USE IMECentricLandmark
+GO
+UPDATE tblSetting 
+   SET Value = ';7;'
+  FROM tblSetting 
+ WHERE Name = 'CaseDocTypeMedsIncoming_True'
+GO 
+UPDATE tblSetting 
+   SET Value = ';7;'
+  FROM tblSetting 
+ WHERE Name = 'CaseDocTypeMedsToDoctor_True'
+GO 
+
+USE IMECentricMedicolegal
+GO
+UPDATE tblSetting 
+   SET Value = ';7;'
+  FROM tblSetting 
+ WHERE Name = 'CaseDocTypeMedsIncoming_True'
+GO 
+UPDATE tblSetting 
+   SET Value = ';7;'
+  FROM tblSetting 
+ WHERE Name = 'CaseDocTypeMedsToDoctor_True'
+GO
+
+Use IMECentricFCE
+GO
+UPDATE tblSetting 
+   SET Value = ';7;22;'
+  FROM tblSetting 
+ WHERE Name = 'CaseDocTypeMedsIncoming_True'
+GO 
+UPDATE tblSetting 
+   SET Value = ';7;22;'
+  FROM tblSetting 
+ WHERE Name = 'CaseDocTypeMedsToDoctor_True'
+GO
+
+USE IMECentricSOMA
+GO
+UPDATE tblSetting 
+   SET Value = ';7;'
+  FROM tblSetting 
+ WHERE Name = 'CaseDocTypeMedsIncoming_True'
+GO 
+UPDATE tblSetting 
+   SET Value = ';7;'
+  FROM tblSetting 
+ WHERE Name = 'CaseDocTypeMedsToDoctor_True'
+GO
+
+USE IMECentricDirectIME
+GO
+UPDATE tblSetting 
+   SET Value = ';7;'
+  FROM tblSetting 
+ WHERE Name = 'CaseDocTypeMedsIncoming_True'
+GO 
+UPDATE tblSetting 
+   SET Value = ';7;'
+  FROM tblSetting 
+ WHERE Name = 'CaseDocTypeMedsToDoctor_True'
+GO
+
+USE IMECentricMatrix
+GO
+UPDATE tblSetting 
+   SET Value = ';7;'
+  FROM tblSetting 
+ WHERE Name = 'CaseDocTypeMedsIncoming_True'
+GO 
+UPDATE tblSetting 
+   SET Value = ';7;'
+  FROM tblSetting 
+ WHERE Name = 'CaseDocTypeMedsToDoctor_True'
+GO
+
+USE IMECentricCVS
+GO
+UPDATE tblSetting 
+   SET Value = ';7;'
+  FROM tblSetting 
+ WHERE Name = 'CaseDocTypeMedsIncoming_True'
+GO 
+UPDATE tblSetting 
+   SET Value = ';7;'
+  FROM tblSetting 
+ WHERE Name = 'CaseDocTypeMedsToDoctor_True'
+GO
+
+USE IMECentricMakos
+GO
+UPDATE tblSetting 
+   SET Value = ';7;'
+  FROM tblSetting 
+ WHERE Name = 'CaseDocTypeMedsIncoming_True'
+GO 
+UPDATE tblSetting 
+   SET Value = ';7;'
+  FROM tblSetting 
+ WHERE Name = 'CaseDocTypeMedsToDoctor_True'
+GO
+
+USE IMECentricNYRC
+GO
+UPDATE tblSetting 
+   SET Value = ';7;'
+  FROM tblSetting 
+ WHERE Name = 'CaseDocTypeMedsIncoming_True'
+GO 
+UPDATE tblSetting 
+   SET Value = ';7;'
+  FROM tblSetting 
+ WHERE Name = 'CaseDocTypeMedsToDoctor_True'
+GO
+
+USE IMECentricKRA
+GO
+UPDATE tblSetting 
+   SET Value = ';7;'
+  FROM tblSetting 
+ WHERE Name = 'CaseDocTypeMedsIncoming_True'
+GO 
+UPDATE tblSetting 
+   SET Value = ';7;'
+  FROM tblSetting 
+ WHERE Name = 'CaseDocTypeMedsToDoctor_True'
+GO
+
+USE IMECentricIMAS
+GO
+UPDATE tblSetting 
+   SET Value = ';7;'
+  FROM tblSetting 
+ WHERE Name = 'CaseDocTypeMedsIncoming_True'
+GO 
+UPDATE tblSetting 
+   SET Value = ';7;'
+  FROM tblSetting 
+ WHERE Name = 'CaseDocTypeMedsToDoctor_True'
+GO
+
+USE IMECentricMedylex
+GO
+UPDATE tblSetting 
+   SET Value = ';7;28;30;'
+  FROM tblSetting 
+ WHERE Name = 'CaseDocTypeMedsIncoming_True'
+GO 
+UPDATE tblSetting 
+   SET Value = ';7;28;30;'
+  FROM tblSetting 
+ WHERE Name = 'CaseDocTypeMedsToDoctor_True'
+GO
+
+USE IMECentricMedaca
+GO
+UPDATE tblSetting 
+   SET Value = ';7;'
+  FROM tblSetting 
+ WHERE Name = 'CaseDocTypeMedsIncoming_True'
+GO 
+UPDATE tblSetting 
+   SET Value = ';7;'
+  FROM tblSetting 
+ WHERE Name = 'CaseDocTypeMedsToDoctor_True'
+GO
+
+USE IMECentricEWCA
+GO
+UPDATE tblSetting 
+   SET Value = ';7;'
+  FROM tblSetting 
+ WHERE Name = 'CaseDocTypeMedsIncoming_True'
+GO 
+UPDATE tblSetting 
+   SET Value = ';7;'
+  FROM tblSetting 
+ WHERE Name = 'CaseDocTypeMedsToDoctor_True'
+GO
