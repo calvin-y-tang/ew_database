c6ba14b68039e4c08dd6f96039572fdbe525cfa9
NWL5CD3083L0R\Terri.Lyde <terri.lyde@examworks.com>
2024-10-17 16:08:49 -0400

    IMEC-14462 - added business rules for Allstate Guardrails for generating documents for invoice quotes with handling = Fee Approval.  Applies only to Examworks US databases

--- IMECentricDB/Data/Single_IMEC_NextRelease.sql
+++ IMECentricDB/Data/Single_IMEC_NextRelease.sql
@@ -12,3 +12,18 @@
 
 -- Sprint 141
 
+
+USE [IMECentricEW]
+---- Business Rule and conditions for AllState Guardrails to send quote to billing client
+INSERT INTO tblBusinessRule (BusinessRuleID, Name, Category, Descrip, IsActive, EventID, AllowOverride, Param1Desc, Param2Desc, BrokenRuleAction)
+VALUES (182, 'GenDocsSendQuoteToBillClient', 'Case', 'When generating documents for quotes, send email to billing client if there is one instead of case client', 1, 1201, 0, 'QuoteType', 'Quotehandling', 0)
+GO
+
+INSERT INTO tblBusinessRuleCondition (EntityType, EntityID, BillingEntity, ProcessOrder, BusinessRuleID, DateAdded, UserIDAdded, EWBusLineID, Param1, Param2)
+VALUES ('PC', 4, 2, 1, 182, GETDATE(), 'Admin', 2, 'IN', '2')
+GO
+
+INSERT INTO tblBusinessRuleCondition (EntityType, EntityID, BillingEntity, ProcessOrder, BusinessRuleID, DateAdded, UserIDAdded, EWBusLineID, Param1, Param2)
+VALUES ('PC', 4, 2, 1, 182, GETDATE(), 'Admin', 5, 'IN', '2')
+GO
+
