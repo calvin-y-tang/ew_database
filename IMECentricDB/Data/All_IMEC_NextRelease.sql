74a67452477dc59e19cde25e5919549eaffa2314
Kalyan Jalla <kalyan.jalla@examworks.com>
2024-10-22 13:57:40 -0700

    IMEC-14483 updated query as per Jose suggestion

--- IMECentricDB/Data/All_IMEC_NextRelease.sql
+++ IMECentricDB/Data/All_IMEC_NextRelease.sql
@@ -4,4 +4,6 @@
 -- --------------------------------------------------------------------------
 
 -- Sprint 141
+-- IMEC-14483 - Update tblSetting for CaseDocTypeMedsIncoming_True to include only CaseDocType 'Med Records' (7) and do not include 'San Med Records' (21)
+   UPDATE tblSetting Set Value = ';7;' where Name = 'CaseDocTypeMedsIncoming_True'
 
--- IMECentricDB/Data/Single_IMEC_NextRelease.sql
+++ IMECentricDB/Data/Single_IMEC_NextRelease.sql
@@ -60,10 +60,10 @@ INSERT INTO tblUserFunction (FunctionCode, FunctionDesc, DateAdded)
 VALUES (' AllStateQuoteGuardrailOverride ', 'AllState - Override Guardrails when creating quote', GETDATE())
 
 -- IMEC-14483 - Update tblSetting for CaseDocTypeMedsIncoming_True to include only CaseDocType 'Med Records' and do not include 'San Med Records'
-Use IMECentricFCE
+Use IMECentricMedylex
 GO
 UPDATE tblSetting 
-   SET Value = ';7;'
+   SET Value = '7;28;30;'
   FROM tblSetting 
  WHERE Name = 'CaseDocTypeMedsIncoming_True'
 GO
