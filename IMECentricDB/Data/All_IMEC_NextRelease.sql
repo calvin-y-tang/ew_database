b6674605a6271a52c58933cbb04849f3ed36ccf7
Christopher Smith <christopher.smith@examworks.com>
2024-11-07 19:29:43 -0500

    Update for table value function for med page sums

--- IMECentricDB/Data/All_IMEC_NextRelease.sql
+++ IMECentricDB/Data/All_IMEC_NextRelease.sql
@@ -14,3 +14,43 @@
             (137, 'PC', 4, 2, 2, GETDATE(), 'Admin', GETDATE(), 'Admin', NULL, 2, NULL, NULL, NULL, NULL, 'AllState2ndNS', NULL, NULL, 0, NULL, 0)
     GO
 
+-- imec-14490-voucher-form-changes
+/****** Object:  UserDefinedFunction [dbo].[fnGetMedsIncomingAndMedsToDoctorPageCountByCase] ******/
+DROP FUNCTION [dbo].[fnGetMedsIncomingAndMedsToDoctorPageCountByCase]
+GO
+
+/****** Object:  UserDefinedFunction [dbo].[fnGetMedsIncomingAndMedsToDoctorPageCountByCase] ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+create function [dbo].[fnGetMedsIncomingAndMedsToDoctorPageCountByCase] (@CaseNbr int)
+returns table
+as
+return (
+	select sum(u.MedsIncoming) as MedsIncoming, sum(u.MedsToDoctor) as MedsToDoctor
+	from (
+		select mc.CaseNbr, isnull(Pages, 0) as MedsIncoming, 0 as MedsToDoctor
+		from tblCase tc
+		join tblCase mc on tc.MasterCaseNbr = mc.MasterCaseNbr
+		join tblCaseDocuments tcd on mc.CaseNbr = tcd.CaseNbr
+		where (
+			tc.CaseNbr = @CaseNbr and MedsIncoming = 1
+		)
+		and (
+			(mc.CaseNbr <>  @CaseNbr and tcd.SharedDoc = 1)
+			or
+			(mc.CaseNbr = @CaseNbr)
+		)
+		union
+		select tc.CaseNbr, 0 as MedsIncoming, isnull(Pages, 0) as MedsToDoctor
+		from tblCase tc
+		join tblCaseDocuments tcd on tc.CaseNbr = tcd.CaseNbr
+		where tc.CaseNbr = @CaseNbr and MedsToDoctor = 1 and tcd.SharedDoc = 0
+	) u
+);
+GO
+
