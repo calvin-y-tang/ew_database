7661b0cd653755bd9f969ef283243a7a534afce0
Christopher Smith <christopher.smith@examworks.com>
2024-11-09 09:38:10 -0500

    Create tblFeeScheduleTiers and populate it with data

---
+++ IMECentricDB/Tables/tblFeeScheduleTiers.sql
@@ -0,0 +1,10 @@
+create table [dbo].[tblFeeScheduleTiers]
+(
+    [TierID] bigint not null identity (1, 1),
+    [Tier] varchar (128) not null,
+    [DoctorCode] int not null,
+    [ParentCompanyID] int null,
+    [DateAdded] datetime constraint [DF_Tier_DateAdded] DEFAULT (getdate()) NULL,
+    [UserIDAdded] varchar (64) null,
+    constraint [PK_tblFeeScheduleTiers] primary key ([TierID])
+);
