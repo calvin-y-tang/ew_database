f6cc548dadb044a91c57f77d49f96d65ddd1cef0
Christopher Smith <christopher.smith@examworks.com>
2024-11-11 22:41:58 -0500

    Updates from discussion w Dimitry

--- IMECentricDB/Tables/tblFeeScheduleTiers.sql
+++ IMECentricDB/Tables/tblFeeScheduleTiers.sql
@@ -4,6 +4,7 @@ create table [dbo].[tblFeeScheduleTiers]
     [Tier] varchar (128) not null,
     [DoctorCode] int not null,
     [ParentCompanyID] int null,
+	[EWBusLineID] [int] NULL,
     [DateAdded] datetime constraint [DF_Tier_DateAdded] DEFAULT (getdate()) NULL,
     [UserIDAdded] varchar (64) null,
     constraint [PK_tblFeeScheduleTiers] primary key ([TierID])
