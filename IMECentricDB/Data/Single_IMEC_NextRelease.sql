e94ed3c0e1f168f5b45553ea984c28cfe72b73e2
Kalyan Jalla <kalyan.jalla@examworks.com>
2024-12-10 12:23:27 -0800

    IMEC_14415, IMEC-14188 added new fields for CRN package

--- IMECentricDB/Data/Single_IMEC_NextRelease.sql
+++ IMECentricDB/Data/Single_IMEC_NextRelease.sql
@@ -81,3 +81,22 @@ INSERT INTO tblBusinessRuleCondition (EntityType, EntityID, BillingEntity, Proce
 VALUES ('PC', 31, 2, 1, 186, GETDATE(), 'Admin', 2, 3, 'MI', 'T1', '385')
 GO
 
+-------------IMEC-14415---and-------IMEC-14188---Add--Additional--Columns--to--map--from--CRN---------
+USE [IMECentricEW] 
+
+ALTER TABLE tblDoctor
+ADD ActiveSurgery BIT NULL,
+LastSugeryDate DATETIME NULL,
+DoctorDisciplineStatus BIT NULL,
+LicenseNotRequired BIT NULL;
+
+GO
+
+USE [IMECentricEW] 
+
+ALTER TABLE tblDoctorSpecialty
+ADD CertificationStatus VARCHAR(50) NULL,
+CertificationStatusID INT NULL,
+ExpirationDate DATETIME NULL;
+
+GO
