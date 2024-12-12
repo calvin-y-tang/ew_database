80b2f1cb08f40c0b0cd275687c3a6137a79390d0
Adam Nock <adam.nock@examworks.com>
2024-12-12 12:11:24 -0500

    Adding PreventDupInvoicesAndVouchers setting.

--- IMECentricDB/Data/All_IMEC_NextRelease.sql
+++ IMECentricDB/Data/All_IMEC_NextRelease.sql
@@ -4,3 +4,7 @@
 -- --------------------------------------------------------------------------
 
 -- Sprint 143
+IF NOT EXISTS (SELECT 1 FROM dbo.tblSetting WHERE [Name] = 'PreventDupInvoicesAndVouchers')
+BEGIN
+	INSERT INTO dbo.tblSetting ([Name], [Value]) VALUES ('PreventDupInvoicesAndVouchers', 'True')
+END
