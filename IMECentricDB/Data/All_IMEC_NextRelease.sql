7e068c4056530073ab5687a174605f031d59102d
NWL5CD3083L0R\Terri.Lyde <terri.lyde@examworks.com>
2024-12-19 14:03:09 -0500

    IMEC-14600 - new table tblCaseHistoryOverrides added and data patch to fill table

--- IMECentricDB/Data/All_IMEC_NextRelease.sql
+++ IMECentricDB/Data/All_IMEC_NextRelease.sql
@@ -8,3 +8,13 @@ IF NOT EXISTS (SELECT 1 FROM dbo.tblSetting WHERE [Name] = 'PreventDupInvoicesAn
 BEGIN
 	INSERT INTO dbo.tblSetting ([Name], [Value]) VALUES ('PreventDupInvoicesAndVouchers', 'True')
 END
+
+
+-- IMEC-14600 - data patch for populating the new table used to keep track of case history notes for use overrides
+INSERT INTO tblCaseHistoryOverrides (CasehistoryID)
+SELECT ID FROM tblCaseHistory WHERE (Eventdesc LIKE '%guardrails override%' 
+  OR Eventdesc LIKE '%QA Checklist completed with override%' 
+  OR Eventdesc LIKE '%Doctor Scheduling Discipline Override%') AND EventDate > '2024-10-01 16:36:46.000'
+ORDER BY EventDate
+GO
+
