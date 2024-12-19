3d0ccd46a8c61efd2e99b260deae84516dd598f7
NWL5CD3083L0R\Terri.Lyde <terri.lyde@examworks.com>
2024-12-19 14:51:32 -0500

    IMEC-14600 - data patch

--- IMECentricDB/Data/All_IMEC_NextRelease.sql
+++ IMECentricDB/Data/All_IMEC_NextRelease.sql
@@ -8,7 +8,26 @@ IF NOT EXISTS (SELECT 1 FROM dbo.tblSetting WHERE [Name] = 'PreventDupInvoicesAn
 BEGIN
 	INSERT INTO dbo.tblSetting ([Name], [Value]) VALUES ('PreventDupInvoicesAndVouchers', 'True')
 END
+GO
 
+-- Sprint 143
+-- IMEC- 14686 Checking CaseDate to Appointment Date
+UPDATE tblBusinessRule SET Param3Desc='DaysbetwnApptC&ApptT' WHERE BusinessRuleID=188;
+GO
+
+-- Sprint 143
+-- IMEC- 14678 Pop-up message styling updated
+UPDATE tblBusinessRuleCondition SET Param6='ExamWorks now owes Allstate the following:-
+• A $2500 penalty fee
+• A refund of the testimony/deposition fee (if prepaid)
+• A refund of the original IME/MMR service fee
+                                     Thank you.' WHERE BusinessRuleConditionID=2001;
+GO
+
+-- Sprint 143
+-- IMEC- 14680 Email language updated
+ UPDATE tblBusinessRuleCondition SET Param6='Examworks users were notified that we owe Allstate a $2500 penalty for Case Number @casenbr@.' WHERE BusinessRuleConditionID=2002;
+GO
 
 -- IMEC-14600 - data patch for populating the new table used to keep track of case history notes for use overrides
 INSERT INTO tblCaseHistoryOverrides (CasehistoryID)
