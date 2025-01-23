1bd4d76246fd4592a4a70f8dc427dc687daca614
NWL5CD3083L0R\Terri.Lyde <terri.lyde@examworks.com>
2025-01-22 22:59:55 -0500

    IMEC-14598 - removed the previous BRC's for Medylex and created new BR's and BRC's.  Needed a more generic way to add emails to generate and distribute docs

--- IMECentricDB/Data/Single_IMEC_NextRelease.sql
+++ IMECentricDB/Data/Single_IMEC_NextRelease.sql
@@ -238,44 +238,46 @@ Use [IMECentricMedylex]  -- Note, this is a Canadian DB on a different server
 BEGIN TRY
     BEGIN TRANSACTION noTxOr14598
 
-DECLARE @BusinessRuleID1 INT = (SELECT BusinessRuleID FROM tblBusinessRule WHERE Name = 'ClientGenDocsToAddtlEmail')
-DECLARE @BCCEmail VARCHAR(120) = 'appointments@medylex.com'
+INSERT INTO tblBusinessRule (BusinessRuleID, Name, Category, Descrip, IsActive, EventID, AllowOverride, Param1Desc, Param2Desc, Param3Desc, Param4Desc, BrokenRuleAction)
+VALUES (203, 'GenDocsToAddtlEmail', 'Case', 'When generating docs, CC/BCC additional email addresses', 1, 1201, 0, 'AttachOption', 'CCEmailAddress', 'BCCEmailAddress', 'MatchOnContentType', 0)
+
+INSERT INTO tblBusinessRule (BusinessRuleID, Name, Category, Descrip, IsActive, EventID, AllowOverride, Param1Desc, Param2Desc, Param3Desc, Param4Desc, BrokenRuleAction)
+VALUES (204, 'DistDocsToAddtlEmail', 'Case', 'When distributing docs, CC/BCC additional email addresses', 1, 1202, 0, 'AttachOption', 'CCEmailAddress', 'BCCEmailAddress', 'MatchOnContentType', 0)
+
+INSERT INTO tblBusinessRule (BusinessRuleID, Name, Category, Descrip, IsActive, EventID, AllowOverride, Param1Desc, Param2Desc, Param3Desc, Param4Desc, BrokenRuleAction)
+VALUES (205, 'DistRptToAddtlEmail', 'Case', 'When distributing reports, CC/BCC additional email addresses', 1, 1320, 0, 'AttachOption', 'CCEmailAddress', 'BCCEmailAddress', 'MatchOnContentType', 0)
+
+
+DECLARE @BusinessRuleID1 INT = (SELECT BusinessRuleID FROM tblBusinessRule WHERE Name = 'GenDocsToAddtlEmail')
+DECLARE @BusinessRuleID2 INT = (SELECT BusinessRuleID FROM tblBusinessRule WHERE Name = 'DistDocsToAddtlEmail')
+DECLARE @BusinessRuleID3 INT = (SELECT BusinessRuleID FROM tblBusinessRule WHERE Name = 'DistRptToAddtlEmail')
+DECLARE @BCCEmail VARCHAR(100) = 'appointments@medylex.com'
 
 IF @BusinessRuleID1 IS NOT NULL
 BEGIN
-	UPDATE tblBusinessRuleCondition SET Param3 = @BCCEmail WHERE BusinessRuleID = @BusinessRuleID1
+	INSERT INTO tblBusinessRuleCondition (EntityType, BillingEntity, ProcessOrder, BusinessRuleID, DateAdded, UserIDAdded, Param1, Param3, Param4)
+	VALUES ('SW', 2, 3, @BusinessRuleID1, GETDATE(), 'Admin', '1', @BCCEmail, 'Appointment Confirmation')
 
 	INSERT INTO tblBusinessRuleCondition (EntityType, BillingEntity, ProcessOrder, BusinessRuleID, DateAdded, UserIDAdded, Param1, Param3, Param4)
-	VALUES ('SW', 2, 2, @BusinessRuleID1, GETDATE(), 'Admin', '1', @BCCEmail, 'Appointment Confirmation')
-
-	INSERT INTO tblBusinessRuleCondition (EntityType, BillingEntity, ProcessOrder, BusinessRuleID, DateAdded, UserIDAdded, Param1, Param3, Param4)
-	VALUES ('SW', 2, 2, @BusinessRuleID1, GETDATE(), 'Admin', '1', @BCCEmail, 'Cancellation Notice')
+	VALUES ('SW', 2, 3, @BusinessRuleID1, GETDATE(), 'Admin', '1', @BCCEmail, 'Cancellation Notice')
 END
-
-DECLARE @BusinessRuleID2 INT = (SELECT BusinessRuleID FROM tblBusinessRule WHERE Name = 'ClientDistDocsToAddtlEmail')
 
 IF @BusinessRuleID2 IS NOT NULL
 BEGIN
-	UPDATE tblBusinessRuleCondition SET Param3 = @BCCEmail WHERE BusinessRuleID = @BusinessRuleID2
+	INSERT INTO tblBusinessRuleCondition (EntityType, BillingEntity, ProcessOrder, BusinessRuleID, DateAdded, UserIDAdded, Param1, Param3, Param4)
+	VALUES ('SW', 2, 3, @BusinessRuleID2, GETDATE(), 'Admin', '1', @BCCEmail, 'Appointment Confirmation')
 
 	INSERT INTO tblBusinessRuleCondition (EntityType, BillingEntity, ProcessOrder, BusinessRuleID, DateAdded, UserIDAdded, Param1, Param3, Param4)
-	VALUES ('SW', 2, 2, @BusinessRuleID2, GETDATE(), 'Admin', '1', @BCCEmail, 'Appointment Confirmation')
-
-	INSERT INTO tblBusinessRuleCondition (EntityType, BillingEntity, ProcessOrder, BusinessRuleID, DateAdded, UserIDAdded, Param1, Param3, Param4)
-	VALUES ('SW', 2, 2, @BusinessRuleID2, GETDATE(), 'Admin', '1', @BCCEmail, 'Cancellation Notice')
+	VALUES ('SW', 2, 3, @BusinessRuleID2, GETDATE(), 'Admin', '1', @BCCEmail, 'Cancellation Notice')
 END
-
-DECLARE @BusinessRuleID3 INT = (SELECT BusinessRuleID FROM tblBusinessRule WHERE Name = 'ClientDistRptToAddtlEmail')
 
 IF @BusinessRuleID3 IS NOT NULL
 BEGIN
-	UPDATE tblBusinessRuleCondition SET Param3 = @BCCEmail WHERE BusinessRuleID = @BusinessRuleID3
+	INSERT INTO tblBusinessRuleCondition (EntityType, BillingEntity, ProcessOrder, BusinessRuleID, DateAdded, UserIDAdded, Param1, Param3, Param4)
+	VALUES ('SW', 2, 3, @BusinessRuleID3, GETDATE(), 'Admin', '1', @BCCEmail, 'Appointment Confirmation')
 
 	INSERT INTO tblBusinessRuleCondition (EntityType, BillingEntity, ProcessOrder, BusinessRuleID, DateAdded, UserIDAdded, Param1, Param3, Param4)
-	VALUES ('SW', 2, 2, @BusinessRuleID3, GETDATE(), 'Admin', '1', @BCCEmail, 'Appointment Confirmation')
-
-	INSERT INTO tblBusinessRuleCondition (EntityType, BillingEntity, ProcessOrder, BusinessRuleID, DateAdded, UserIDAdded, Param1, Param3, Param4)
-	VALUES ('SW', 2, 2, @BusinessRuleID3, GETDATE(), 'Admin', '1', @BCCEmail, 'Cancellation Notice')
+	VALUES ('SW', 2, 3, @BusinessRuleID3, GETDATE(), 'Admin', '1', @BCCEmail, 'Cancellation Notice')
 END
 
     commit transaction noTxOr14598
@@ -287,6 +289,9 @@ begin catch
     print 'Rolling back transaction.'
     rollback transaction noTxOr14598
 end catch
+
+
+-- next change
 use IMECentricEW
 
 begin try
