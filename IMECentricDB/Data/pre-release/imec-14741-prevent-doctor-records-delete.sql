06e3ecb8e6810cfada04033e6c6aa1990053014f
christophersmith4 <christopher.smith@examworks.com>
2025-02-04 10:49:26 -0500

    remove existing untracked trigger and table it was using for logging

---
+++ IMECentricDB/Data/pre-release/imec-14741-prevent-doctor-records-delete.sql
@@ -0,0 +1,8 @@
+-- imec-14741-prevent-doctor-records-delete
+-- Required to run before release to remove existing untracked trigger and table it was using for logging.
+
+use [IMECentricEW]
+
+DROP TRIGGER [dbo].[tblDoctor_AfterDelete_TRG]
+DROP TABLE [dbo].[tblLogChangeTracking]
+
