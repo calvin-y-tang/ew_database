f96318aa645bef1b5814570b54d71b5fd116f8c9
umersami <umer.sami@examworks.com>
2025-02-13 14:25:07 -0500

    JurisdictionTONULL

--- IMECentricDB/Data/All_IMEC_NextRelease.sql
+++ IMECentricDB/Data/All_IMEC_NextRelease.sql
@@ -9,4 +9,9 @@
 INSERT INTO tblBusinessRule (BusinessRuleID, Name, Category, Descrip, IsActive, EventID, AllowOverride, Param1Desc, BrokenRuleAction)
 VALUES (207, ' DPSResultFileImportMethod', 'Case', ' DPS - Result File Import method: (1) info from tblDPSResultFileConfig or (2) info from JSON returned', 1, 1015, 0, ' RsltFileImportMthdID', 0)
 
+--Sprint 145
+-- IMEC 14830  Qoute Rules Processing Order 
 
+UPDATE tblQuoteRule
+SET Jurisdiction= null
+WHERE Jurisdiction = ''
