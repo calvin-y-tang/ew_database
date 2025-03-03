1e57ea0fded391c044ed082e1c503fb1c1f803a5
NWL5CD3083L0R\Terri.Lyde <terri.lyde@examworks.com>
2025-03-03 15:35:48 -0500

    IMEC-14824 - adding case priority values for DirectIMEC DB only

--- IMECentricDB/Data/Single_IMEC_NextRelease.sql
+++ IMECentricDB/Data/Single_IMEC_NextRelease.sql
@@ -10,3 +10,16 @@
 
 -- --------------------------------------------------------------------------
 -- sprint 146
+
+
+-- **** Canadian DB change ***************
+-- IMEC-14824 - Add case priority values for DirectIME
+USE [IMECentricDirectIME]
+INSERT INTO tblPriority (PriorityCode, Description, DateAdded, UserIDAdded, Rank)
+VALUES ('24Hr', '24 Hour Rush', GETDATE(), 'Admin', 30),
+       ('5Day', '5 Day Rush', GETDATE(), 'Admin', 60),
+       ('10Day', '10 Day Rush', GETDATE(), 'Admin', 70),
+       ('RushVerbal', 'Rush Verbal', GETDATE(), 'Admin', 90)
+
+
+
